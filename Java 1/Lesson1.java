e035d2831be5e5f512e0839ca36d92f323642040
Sergey Iryupin <biblelamp@yandex.ru>
2016-06-29 11:08:01 +0300

    tidying indents

--- Java 1/Lesson1.java
+++ Java 1/Lesson1.java
@@ -5,7 +5,7 @@
  *		and returns the result, and, b, c, d - input parameters of this method.
  *	+	Write a method that takes the input of the two numbers,
  *		and checking sum is in the range 10 to 20, and if so - to return true, otherwise - false.
- * + * Write a method that determines whether a leap year.
+ *	+	* Write a method that determines whether a leap year.
  *		Every 4th year is leap year but every 100th, each 400th - leap.
  *
  * @author Sergey Iryupin
@@ -13,8 +13,8 @@
  */
 public class Lesson1 {
 
-	public static void main(String[] args) {
-		byte b = 64;
+    public static void main(String[] args) {
+        byte b = 64;
         short s = 1024;
         int i = 200000;
         long l = 15000L;
@@ -22,21 +22,22 @@ public class Lesson1 {
         double d = 15.72d;
         boolean flag = true;
         char c = 'a';
+
         System.out.println("" + b + " " + s + " " + i + " " + l + " " + f + " " + d + " " + flag + " " + c);
 
-		System.out.println("Calculating a * (b + (c / d))");
-		System.out.println(calcExpr(2, 10, 6, 2));
-		System.out.println(calcExpr(1, 8, 5, 0));
+        System.out.println("Calculating a * (b + (c / d))");
+        System.out.println(calcExpr(2, 10, 6, 2));
+        System.out.println(calcExpr(1, 8, 5, 0));
 
-		System.out.println("Cheking the summ (a + b) is in the range [10..20]");
-		System.out.println(checkSumm(1, 6));
-		System.out.println(checkSumm(10, 2));
-		System.out.println(checkSumm(10, 11));
+        System.out.println("Cheking the summ (a + b) is in the range [10..20]");
+        System.out.println(checkSumm(1, 6));
+        System.out.println(checkSumm(10, 2));
+        System.out.println(checkSumm(10, 11));
 
-		System.out.println("Cheking - is the year leap?");
-		System.out.println(isYearLeap(1600));
-		System.out.println(isYearLeap(1604));
-		System.out.println(isYearLeap(1800));
+        System.out.println("Cheking - is the year leap?");
+        System.out.println(isYearLeap(1600));
+        System.out.println(isYearLeap(1604));
+        System.out.println(isYearLeap(1800));
     }
 
     /** calculating a * (b + (c / d))
