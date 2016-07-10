1553e2ce2f0fbef0925a7b8b582d73f5bc5f61fd
biblelamp <biblelamp@yandex.ru>
2016-07-10 07:15:26 +0400

    chapter 9 find and fix the problem

--- Head First Java/SimUnit.java
+++ Head First Java/SimUnit.java
@@ -2,6 +2,7 @@ class SimUnit {
     String botType;
     SimUnit(String type) {
         botType = type;
+        System.out.println(type + " created."); // to search for a problem
     }
     int powerUse() {
         if ("Retention".equals(botType)) {
--- Head First Java/V3Radiator.java
+++ Head First Java/V3Radiator.java
@@ -2,7 +2,7 @@ import java.util.*;
 class V3Radiator extends V2Radiator {
 	V3Radiator(ArrayList lglist) {
         super(lglist);
-        for(int g=0; g<10; g++) {
+        for(int g=0; g<5; g++) { // fix the problem in using energie
             lglist.add(new SimUnit("V3Radiator"));
         }
     }
