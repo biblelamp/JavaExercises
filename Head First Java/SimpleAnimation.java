e9f171c8c3cc2e9025c97348b43c033ef171a1a8
Sergey Iryupin <biblelamp@yandex.ru>
2016-07-12 15:43:04 +0300

    chapter 12 simple animation with colors

--- Head First Java/SimpleAnimation.java
+++ Head First Java/SimpleAnimation.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class SimpleAnimation {
 
@@ -21,26 +22,28 @@ public class SimpleAnimation {
         frame.setSize(300, 300);
         frame.setVisible(true);
 
-        for (int i = 0; i < 120; i++) {
+        for (int i = 0; i < 130; i++) {
 
             x++;
             y++;
             drawPanel.repaint();
 
             try {
-                Thread.sleep(30);
+                Thread.sleep(20);
             } catch(Exception ex) { }
-            
         }
-
     }
 
     class MyDrawPanel extends JPanel {
 
         public void paintComponent(Graphics g) {
+            Color[] color = new Color[]{ Color.green, Color.blue, Color.red, Color.yellow };
+            Random rand = new Random();
+
             g.setColor(Color.white);
             g.fillRect(0, 0, this.getWidth(), this.getHeight());
-            g.setColor(Color.green);
+
+            g.setColor(color[rand.nextInt(color.length)]);
             g.fillOval(x, y, 40, 40);
         }
     }
