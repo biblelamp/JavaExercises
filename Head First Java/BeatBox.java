9136cce64feed2933b99c0361ca6c96385223ab1
biblelamp <biblelamp@yandex.ru>
2016-07-17 15:02:14 +0400

    chapter 14 added save/open dialogs

--- Head First Java/BeatBox.java
+++ Head First Java/BeatBox.java
@@ -176,43 +176,51 @@ public class BeatBox {
     
     public class MySendListener implements ActionListener {
         public void actionPerformed(ActionEvent a) {
-            boolean [] checkboxState = new boolean [256];
-            for (int i = 0; i < 256; i++) {
-                JCheckBox check = (JCheckBox) checkboxList.get(i);
-                if (check.isSelected()) {
-                    checkboxState[i] = true;
+            JFileChooser save = new JFileChooser();
+            int result = save.showSaveDialog(theFrame);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                boolean[] checkboxState = new boolean[256];
+                for (int i = 0; i < 256; i++) {
+                    JCheckBox check = (JCheckBox) checkboxList.get(i);
+                    if (check.isSelected()) {
+                        checkboxState[i] = true;
+                    }
                 }
-            }
-            try {
-                FileOutputStream fileStream = new FileOutputStream(new File("Checkbox.ser"));
-                ObjectOutputStream os = new ObjectOutputStream(fileStream);
-                os.writeObject(checkboxState);
-            } catch(Exception e) {
-                e.printStackTrace();
+                try {
+                    FileOutputStream fileStream = new FileOutputStream(new File(save.getSelectedFile().getPath()));
+                    ObjectOutputStream os = new ObjectOutputStream(fileStream);
+                    os.writeObject(checkboxState);
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
     public class MyReadInListener implements ActionListener {
         public void actionPerformed(ActionEvent a) {
-            boolean[] checkboxState = null;
-            try {
-                FileInputStream fileIn = new FileInputStream(new File("Checkbox.ser"));
-                ObjectInputStream is = new ObjectInputStream(fileIn);
-                checkboxState = (boolean[]) is.readObject();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-            for (int i = 0; i < 256; i++) {
-                JCheckBox check = (JCheckBox) checkboxList.get(i);
-                if (checkboxState[i]) {
-                    check.setSelected(true);
-                } else {
-                    check.setSelected(false);
+            JFileChooser open = new JFileChooser();
+            int result = open.showOpenDialog(theFrame);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                boolean[] checkboxState = null;
+                try {
+                    FileInputStream fileIn = new FileInputStream(new File(open.getSelectedFile().getPath()));
+                    ObjectInputStream is = new ObjectInputStream(fileIn);
+                    checkboxState = (boolean[]) is.readObject();
+                } catch(Exception e) {
+                    e.printStackTrace();
                 }
+                for (int i = 0; i < 256; i++) {
+                    JCheckBox check = (JCheckBox) checkboxList.get(i);
+                    if (checkboxState[i]) {
+                        check.setSelected(true);
+                    } else {
+                        check.setSelected(false);
+                    }
+                }
+                sequencer.stop();
+                buildTrackAndStart();
             }
-            sequencer.stop();
-            buildTrackAndStart();
         }
     }
 }
