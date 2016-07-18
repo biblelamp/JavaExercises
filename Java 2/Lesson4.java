8fa1998ba089b1815a893a8ac9f29f6c48bfa801
Sergey Iryupin <biblelamp@yandex.ru>
2016-07-18 11:32:37 +0300

    lesson 4. homework stage 1

---
+++ Java 2/Lesson4.java
@@ -0,0 +1,56 @@
+/**
+ * Java. Level 2. Lesson 4. Homework
+ *  1. Create a window for client-side chat (large text field to display
+ *  the correspondence, one-line text box for writing messages, to send messages
+ *  button). The message should be sent either by pressing a button on the form,
+ *  or by pressing the "Enter".
+ *
+ * @author Sergey Iryupin
+ * @version 18 July 2016
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Lesson4 {
+
+    JTextArea incoming;
+    JTextField outgoing;
+
+    public static void main(String[] args) {
+        Lesson4 chat = new Lesson4();
+        chat.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame("Simple Chat Client");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainPanel = new JPanel();
+        incoming = new JTextArea(25, 40);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        outgoing = new JTextField(33);
+        outgoing.addActionListener(new SendButtonListener());
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.add(qScroller);
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setBounds(200, 200, 470, 475);
+        frame.setVisible(true);
+    }
+
+    public class SendButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            if (!outgoing.getText().trim().isEmpty()) {
+                incoming.append(outgoing.getText() + "\n");
+            }
+            outgoing.setText("");
+        }
+    }
+}
