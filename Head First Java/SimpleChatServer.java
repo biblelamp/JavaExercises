6914daf2315aea9d8032b40b8286babbfe199c57
Sergey Iryupin <biblelamp@yandex.ru>
2016-07-18 15:46:21 +0300

    chapter 15 simple chat server

---
+++ Head First Java/SimpleChatServer.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class SimpleChatServer {
+    ArrayList clientOutputStreams;
+
+    public static void main(String[] args) {
+        new SimpleChatServer().go();
+    }
+
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket sock;
+
+        public ClientHandler(Socket clientSocket) {
+            try {
+                sock = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(isReader);
+            } catch(Exception ex) { ex.printStackTrace(); }
+        }
+
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    tellEveryone(message);
+                }
+            } catch(Exception ex) { ex.printStackTrace(); }
+        }
+    }
+
+    public void go() {
+        ArrayList<PrintWriter> clientOutputStreams = new ArrayList<PrintWriter>();
+        try {
+            ServerSocket serverSock = new ServerSocket(5000);
+            while (true) {
+                Socket clientSocket = serverSock.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+                Thread thr = new Thread(new ClientHandler(clientSocket));
+                thr.start();
+                System.out.println("got a connection");
+            }
+        } catch(Exception ex) { ex.printStackTrace(); }
+    }
+
+    public void tellEveryone(String message) {
+        Iterator it = clientOutputStreams.iterator();
+        while (it.hasNext()) {
+            try {
+                PrintWriter writer = (PrintWriter) it.next();
+                writer.println(message);
+                writer.flush();
+            } catch(Exception ex) { ex.printStackTrace(); }
+        }
+    }
+}
