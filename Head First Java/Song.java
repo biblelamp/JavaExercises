5da039ad871910f29f660a6ab1cd3f5d9f9b69bc
Sergey Iryupin <biblelamp@yandex.ru>
2016-07-27 17:45:38 +0300

    chapter 16 fixed class Song

---
+++ Head First Java/Song.java
@@ -0,0 +1,46 @@
+class Song implements Comparable <Song> {
+    String title;
+    String artist;
+    String rating;
+    String bpm;
+
+    public int compareTo(Song s) {
+        return title.compareTo(s.getTitle());
+    }
+
+    public Song(String t, String a, String r, String b) {
+        title = t;
+        artist = a;
+        rating = r;
+        bpm = b;
+    }
+
+    public boolean equals(Object aSong) {
+        Song s = (Song) aSong;
+        return getTitle().equals(s.getTitle());
+    }
+
+    public int hashCode() {
+        return title.hashCode();
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String toString() {
+        return title;
+    }
+}
