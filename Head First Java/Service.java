fb09437890e66b9490c416617d49d4d31455a653
Sergey Iryupin <biblelamp@yandex.ru>
2016-08-07 22:35:43 +0300

    chapter 18 simple service browser

---
+++ Head First Java/Service.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+import java.io.*;
+
+public interface Service extends Serializable { 
+    public JPanel getGuiPanel();
+}
---
+++ Head First Java/ServiceServer.java
@@ -0,0 +1,6 @@
+import java.rmi.*;
+
+public interface ServiceServer extends Remote {
+	Object[] getServiceList() throws RemoteException;
+    Service getService(Object serviceKey) throws RemoteException;
+}
