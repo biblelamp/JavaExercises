83b88c37b1d4eaf9062d284cb42154362426f540
Sergey Iryupin <biblelamp@yandex.ru>
2016-09-13 16:48:32 +0300

    lesson 7. example code for students stage 3

--- Java 1/lesson7/GUIExamples.java
+++ Java 1/lesson7/GUIExamples.java
@@ -6,12 +6,14 @@
  */
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class GUIExamples {
+class GUIExamples {
 
     public static void main(String[] args) {
         Form w = new Form();
         FormBL wbl = new FormBL();
+        FormWithListeners wwl = new FormWithListeners();
     }
 
 }
@@ -20,7 +22,7 @@ class Form extends JFrame { // necessary javax.swing.*
     Form() {
         setTitle("Test Window");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(200, 200, 500, 300); // start x, start y, width, height
+        setBounds(200, 200, 400, 300); // start x, start y, width, height
         setVisible(true);
     }
 }
@@ -29,17 +31,70 @@ class FormBL extends JFrame { // necessary java.awt.*
     FormBL() {
         setTitle("BorderLayout Demo");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(250, 250, 400, 400);
+        setBounds(250, 250, 400, 300);
+        
+        // create buttons
         JButton[] jbs = new JButton[5];
         for (int i = 0; i < 5; i++) {
             jbs[i] = new JButton("#" + i);
         }
+        
+        // set the layout elements
         setLayout(new BorderLayout());
-        add(jbs[0], BorderLayout.EAST); // adding button no the form
+        
+        // adding buttons no the form
+        add(jbs[0], BorderLayout.EAST);
         add(jbs[1], BorderLayout.WEST);
         add(jbs[2], BorderLayout.SOUTH);
         add(jbs[3], BorderLayout.NORTH);
         add(jbs[4], BorderLayout.CENTER);
+        setVisible(true);
+    }
+}
+
+class FormWithListeners extends JFrame { // necessary java.awt.event.*
+    FormWithListeners() {
+        setTitle("BorderLayout Demo");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300, 400, 300);
+        
+        // create buttons
+        JButton btnFist = new JButton("First");
+        JButton btnSecond = new JButton("Second");
+
+        // add buttons listeners
+        btnFist.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button first");
+            }
+        });
+        btnSecond.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button second");
+            }
+        });
+
+        // create canvas with MouseAdapter
+        Canvas canvasPanel = new Canvas();
+        canvasPanel.setBackground(Color.white);
+        canvasPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                int x = e.getX();
+                int y = e.getY();
+                System.out.println(x + ":" + y);
+            }
+        });
+
+        // panel with button
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(btnFist);
+        btnPanel.add(btnSecond);
+
+        // add btnPanel and canvas
+        getContentPane().add(BorderLayout.CENTER, canvasPanel);
+        getContentPane().add(BorderLayout.SOUTH, btnPanel);
         setVisible(true);
     }
 }
