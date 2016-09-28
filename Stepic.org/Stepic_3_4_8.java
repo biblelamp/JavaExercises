6e40bd9a850bf6eaa1c0142e7d29c05436e634c0
Sergey Iryupin <biblelamp@yandex.ru>
2016-09-28 11:36:47 +0300

    task 3.4.8 where it can be used final modifier?

---
+++ Stepic.org/Stepic_3_4_8.java
@@ -0,0 +1,20 @@
+class Stepic_3_4_8 {
+
+    final static int x = 0;
+    final int y = 1;
+
+    public static void main(String[] args) {
+        final int a = 1;
+        new Stepic_3_4_8().test(a);
+    }
+
+    //final Stepic_3_4_8() { // error
+    Stepic_3_4_8() {
+    }
+
+    final void test(final int a) {
+    }
+
+    final class Test {
+    }
+}
