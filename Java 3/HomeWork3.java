88fafcc1eccf2477060b7764fa17e8936d18f39f
Sergey Iryupin <biblelamp@yandex.ru>
2016-10-11 16:09:05 +0300

    lesson 3. homework stage 3

--- Java 3/HomeWork3.java
+++ Java 3/HomeWork3.java
@@ -2,14 +2,17 @@
  * Java. Level 3. Lesson 3. Homework
  * 1. Read the small file (about 1 kb) in a byte array
  * 2. Sew together 10 files one by one
+ * 3. Read file (a small book of 20-50 kb) and count the number of unique words
  *
  */
 import java.io.*;
+import java.util.*;
 
 class HomeWork3 {
 
     final String IN_FILE_NAME = "HomeWork3.java";
     final String OUT_FILE_NAME = "file_x10.txt";
+    final String BOOK_FILE_NAME = "Dead_Mans_Island-John_Escott.txt";
 
     public static void main(String[] args) {
         new HomeWork3().go();
@@ -18,7 +21,7 @@ class HomeWork3 {
     void go() {
         // task 1
         getFileToByteArray(IN_FILE_NAME);
-        
+
         // task 2
         try (FileOutputStream fos = new FileOutputStream(OUT_FILE_NAME, true)) {
             for (int i = 0; i < 10; i++) {
@@ -28,6 +31,31 @@ class HomeWork3 {
         } catch(IOException e) {
             e.printStackTrace();
         }
+
+        // task 3
+        TreeMap<String, Integer> hm = new TreeMap<String, Integer>();
+        Set<Map.Entry<String, Integer>> set = hm.entrySet();
+        try (BufferedReader  reader = new BufferedReader (new InputStreamReader(new FileInputStream(BOOK_FILE_NAME)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                //System.out.println(line);
+                String[] words = line.toLowerCase().split("[\t -\"\'{,|.|!|?}?]+");
+                for (String word : words) {
+                    int value = 0;
+                    try {
+                        value = hm.get(word);
+                    } catch(NullPointerException e) { }
+                    hm.put(word, value + 1);
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        for (Map.Entry<String, Integer> o : set) {
+            System.out.println(o.getKey() + ": " + o.getValue());
+        }
+        System.out.println("The book has " + hm.size() + " unique words.");
+
     }
 
     byte[] getFileToByteArray(String fileName) {
