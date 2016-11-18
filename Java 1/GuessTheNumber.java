cd22c155288c934f6af2dfbc8b142447ac1b6966
Sergey Iryupin <biblelamp@yandex.ru>
2016-11-18 14:56:36 +0300

    Guess The Number - simple game version 0.2

--- Java 1/GuessTheNumber.java
+++ Java 1/GuessTheNumber.java
@@ -1,20 +1,27 @@
 /**
  * Java. Game Guess The Number
  * Simple game for beginners in console
+ *
+ * @version 0.2
  */
 import java.util.*;
 
 class GuessTheNumber {
 
+    static Scanner scanner = new Scanner(System.in);
+    static int counter = 0;
+
     public static void main(String[] args) {
         int range = 10;
-        int number = (int) (Math.random() * range);
-        int counter = 0;
-        Scanner scanner = new Scanner(System.in);
-
+        int number = (int) (Math.random() * range) + 1;
         System.out.println("Your task is to guess the conceived number.");
+        playLevel(range, number);
+        scanner.close();
+    }
+
+    static void playLevel(int range, int number) {
         while (true) {
-            System.out.println("Guess the number from 0 to " + range + ":");
+            System.out.println("Guess the number from 1 to " + range + ":");
             int input_number = scanner.nextInt();
             counter++;
             if (input_number == number) {
@@ -26,6 +33,5 @@ class GuessTheNumber {
                 System.out.println("Conceived number greater.");
             }
         }
-        scanner.close();
     }
 }
