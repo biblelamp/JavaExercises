2cf927dccbd6d4a80ce0e0381117ada8fc0564ae
Sergey Iryupin <biblelamp@yandex.ru>
2016-11-20 16:01:02 +0300

    TradingView Client version 0.2

--- Test Tasks/tradingview.com/Client.java
+++ Test Tasks/tradingview.com/Client.java
@@ -1,13 +1,23 @@
+/**
+ * Java. Client - test task from TradingView
+ *
+ * @author Sergey Iryupin
+ * @version 0.2 dated November 20, 2016
+ */
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
 class Client {
 
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in); // for keyboard input
     final String IP_ADDRESS = "127.0.0.1";
     final int PORT = 2048;
-    final String EXIT_COMMAND = "exit";
+    final String EXIT_COMMAND = "exit"; // command for exit
+
+    Socket sock;
+    PrintWriter writer;
+    BufferedReader reader;
 
     public static void main(String[] args) {
         new Client().go();
@@ -16,21 +26,38 @@ class Client {
     void go() {
         String message;
         try {
-            Socket sock = new Socket(IP_ADDRESS, PORT);
-            PrintWriter writer = new PrintWriter(sock.getOutputStream());
+            sock = new Socket(IP_ADDRESS, PORT);
+            writer = new PrintWriter(sock.getOutputStream());
+            reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            System.out.println("Connection with the server established:");
+            Thread getSrvMessage = new Thread(new ServerListener());
+            getSrvMessage.start();
             try {
                 do {
-                    System.out.print(">");
                     message = scanner.nextLine();
                     writer.println(message);
                     writer.flush();
                 } while (!message.toLowerCase().equals(EXIT_COMMAND));
                 writer.close();
+                System.exit(0);
             } catch(Exception ex) {
                 ex.printStackTrace();
             }
         } catch(IOException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    class ServerListener implements Runnable { // get messages from Server
+        public void run() {
+        String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("get: " + message);
+                }
+            } catch(Exception ex) { 
+                ex.printStackTrace();
+            }
         }
     }
 }
