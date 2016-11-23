ee30cc8b8301d866f54e7c58a39ba0ca529090e0
Sergey Iryupin <biblelamp@yandex.ru>
2016-11-23 14:47:37 +0300

    TradingView Server version 0.3.1

--- Test Tasks/tradingview.com/Server.java
+++ Test Tasks/tradingview.com/Server.java
@@ -2,7 +2,7 @@
  * Java. Server - test task from TradingView
  *
  * @author Sergey Iryupin
- * @version 0.3 dated November 22, 2016
+ * @version 0.3.1 dated November 23, 2016
  */
 import java.io.*;
 import java.net.*;
@@ -13,6 +13,7 @@ class Server {
     final String LS_COMMAND = "ls"; // get list of files
     final String GET_COMMAND = "get"; // get file
     final String EXIT_COMMAND = "exit"; // logoff
+    final String UNKNOWN_COMMAND = "Unknown command. Use only: ls/get <filename>/exit";
 
     final String SERVER_STARTED = "Server started.";
     final String CLIENT_JOINED = "Client joined.";
@@ -32,28 +33,45 @@ class Server {
                 sock = clientSocket;
                 reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
                 writer = new PrintWriter(sock.getOutputStream());
-            } catch(Exception ex) { 
+            } catch(Exception ex) {
                 ex.printStackTrace(); 
             }
         }
 
         public void run() {
             String message;
+            String[] command;
             try {
                 while ((message = reader.readLine()) != null) {
                     System.out.println("get: " + message);
-                    if (message.toLowerCase().equals(EXIT_COMMAND)) // exit
+                    command = message.split(" ");
+                    if (command[0].equals(LS_COMMAND)) { // list of files
+                        message = getListOfFiles();
+                    } else if (command[0].equals(GET_COMMAND)) { // get file
+                        message = getFile();
+                    } else if (message.toLowerCase().equals(EXIT_COMMAND)) { // logoff
                         break;
-                    else {
-                        writer.println(message);
-                        writer.flush();
+                    } else {
+                        message = UNKNOWN_COMMAND;
                     }
+                    writer.println(message);
+                    writer.flush();
                 }
             } catch(Exception ex) { 
                 ex.printStackTrace();
             }
             System.out.println(CLIENT_DISCONNECTED);
         }
+    }
+
+    String getListOfFiles() {
+        String message;
+        return "list of file(s)";
+    }
+
+    String getFile() {
+        String message;
+        return "get <filename>";
     }
 
     void go() {
@@ -68,7 +86,7 @@ class Server {
                 System.out.println(CLIENT_JOINED);
             }
         } catch(IOException ex) {
-            ex.printStackTrace();  
+            ex.printStackTrace();
         }
     }
 }
