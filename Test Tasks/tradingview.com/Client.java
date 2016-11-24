67ca3f43d670da282f40bad5939a7cbbab79a0a0
Sergey Iryupin <biblelamp@yandex.ru>
2016-11-24 15:46:10 +0300

    TradingView Client version 0.2.3

--- Test Tasks/tradingview.com/Client.java
+++ Test Tasks/tradingview.com/Client.java
@@ -2,7 +2,7 @@
  * Java. Client - test task from TradingView
  *
  * @author Sergey Iryupin
- * @version 0.2.2 dated November 23, 2016
+ * @version 0.2.3 dated November 24, 2016
  */
 import java.io.*;
 import java.net.*;
@@ -12,13 +12,14 @@ class Client {
 
     final String SERVER_IP = "127.0.0.1";
     final int SERVER_PORT = 2048;
-    final String PROMPT = "\n$";
+    final String PROMPT = "$";
+    final String GET_COMMAND = "get"; // get file
     final String EXIT_COMMAND = "exit"; // command for exit
     final String CONNECTION_START = "Connection to server established.";
     final String CONNECTION_CLOSED = "Connection closed.";
     Scanner scanner = new Scanner(System.in); // for keyboard input
 
-    Socket sock;
+    Socket socket;
     PrintWriter writer;
     BufferedReader reader;
 
@@ -29,10 +30,10 @@ class Client {
     void go() {
         String message;
         try {
-            sock = new Socket(SERVER_IP, SERVER_PORT);
-            System.out.print(CONNECTION_START + PROMPT);
-            writer = new PrintWriter(sock.getOutputStream());
-            reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            socket = new Socket(SERVER_IP, SERVER_PORT);
+            System.out.print(CONNECTION_START + "\n" + PROMPT);
+            writer = new PrintWriter(socket.getOutputStream());
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Thread getSrvMessage = new Thread(new ServerListener());
             getSrvMessage.start();
             try {
@@ -40,8 +41,9 @@ class Client {
                     message = scanner.nextLine();
                     writer.println(message);
                     writer.flush();
-                } while (!message.toLowerCase().equals(EXIT_COMMAND));
+                } while (!message.equals(EXIT_COMMAND));
                 writer.close();
+                socket.close();
                 System.exit(0);
             } catch(Exception ex) {
                 ex.printStackTrace();
@@ -55,10 +57,28 @@ class Client {
     class ServerListener implements Runnable { // get messages from Server
         public void run() {
             String message;
+            String[] command;
+            boolean toFile = false;
+            FileWriter fw = null;
             try {
                 while ((message = reader.readLine()) != null) {
-                    System.out.print(message);
-                    System.out.print(PROMPT);
+                    if (message.startsWith(GET_COMMAND)) {
+                        System.out.println(message);
+                        command = message.split(" ");
+                        toFile = true;
+                        fw = new FileWriter(command[1]);
+                    } else if (message.equals("\0")) {
+                        System.out.print(PROMPT);
+                        if (toFile) {
+                            toFile = false;
+                            fw.flush();
+                            fw.close();
+                        }
+                    } else if (toFile) {
+                        fw.write(message + "\n");
+                    } else {
+                        System.out.println(message);
+                    }
                 }
             } catch(Exception ex) { 
                 ex.printStackTrace();
