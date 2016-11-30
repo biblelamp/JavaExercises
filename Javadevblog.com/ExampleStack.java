f977fcd6c119afc70a083e167dcd0ef7cb0b13e5
Sergey Iryupin <biblelamp@yandex.ru>
2016-11-30 16:20:52 +0300

    example with stack

--- Javadevblog.com/ExampleStack.java
+++ Javadevblog.com/ExampleStack.java
@@ -18,35 +18,3 @@ class ExampleStack {
         System.out.println();
     }
 }
-
-class Stack {
-    private int size;
-    private int[] stackArray;
-    private int sp;
-
-    Stack(int size) {
-        this.size = size;
-        stackArray = new int[size];
-        sp = -1;
-    }
-
-    void push(int element) {
-        stackArray[++sp] = element;
-    }
-
-    int pop() {
-        return stackArray[sp--];
-    }
- 
-    int peek() {
-        return stackArray[sp];
-    }
- 
-    boolean isEmpty() {
-        return (sp == -1);
-    }
- 
-    boolean isFull() {
-        return (sp == size - 1);
-    }
-}
