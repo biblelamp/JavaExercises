a92a21e2fbedf9ac99bc45f45ab5a6f0478b46ca
Sergey Iryupin <biblelamp@yandex.ru>
2016-11-30 16:47:31 +0300

    using stack to check brackets

---
+++ Javadevblog.com/UsingStack.java
@@ -0,0 +1,51 @@
+/*
+ * Using a stack to check the brackets in expressions
+ */
+
+class UsingStack {
+
+    public static void main(String[] args) {
+        String[] str_tests = {"a[b {c} d] e", "a {b (c] d} e", "{a [b {c} d] e}"};
+        for (String str : str_tests)
+            System.out.println(str + " - " + (new CheckExpressions(str).check()));
+    }
+}
+
+class CheckExpressions {
+
+    private String str;
+    private Stack stack;
+
+    public CheckExpressions(String str) {
+        this.str = str;
+        stack = new Stack(str.length());
+    }
+
+    boolean check() {
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            switch (ch) {
+                case '{':
+                case '[':
+                case '(':
+                    stack.push(ch);
+                    break;
+                case '}':
+                case ']':
+                case ')':
+                    if (!stack.isEmpty()) {
+                        char chClosed = (char) stack.pop();
+                        if ((ch == '}' && chClosed != '{')
+                            || (ch == ']' && chClosed != '[')
+                            || (ch == ')' && chClosed != '('))
+                        return false;
+                    } else 
+                        return false;
+                    break;
+            }
+        }
+        if (!stack.isEmpty())
+            return false;
+        return true;
+    }
+}
