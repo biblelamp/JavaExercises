4cffafe9cf11cb77ecb8b254dae9f75e3b59b971
Sergey Iryupin <biblelamp@yandex.ru>
2016-12-01 16:24:23 +0300

    example with dynamic stack

--- Javadevblog.com/ExampleStack.java
+++ Javadevblog.com/ExampleStack.java
@@ -1,20 +1,23 @@
 class ExampleStack {
 
     public static void main(String[] args) {
-        Stack stack = new Stack(10);
+        //Stack stack = new Stack(10);
+        DimamicStack stack = new DimamicStack();
 
         stack.push(79);
         stack.push(56);
         stack.push(32);
         stack.push(24);
+        stack.push(11);
  
         stack.pop();
  
-        System.out.print("Stack: ");
-        while (!stack.isEmpty()) {
+        System.out.println("Stack: " + stack);
+        /*while (!stack.isEmpty()) {
             int value = stack.pop();
             System.out.print(value + " ");
         }
         System.out.println();
+        */
     }
 }
