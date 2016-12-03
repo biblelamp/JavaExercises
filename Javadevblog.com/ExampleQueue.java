2444c4c70ad69704d574a00b52f1b2d72d895f35
Sergey Iryupin <biblelamp@yandex.ru>
2016-12-03 15:46:30 +0300

    example with queue based on ArrayList

--- Javadevblog.com/ExampleQueue.java
+++ Javadevblog.com/ExampleQueue.java
@@ -1,23 +1,23 @@
 public class ExampleQueue {
 
     public static void main(String[] args) {
-        Queue queue = new Queue(5);
+        //Queue queue = new Queue(5);
+        DimamicQueue<Integer> queue = new DimamicQueue<Integer>();
 
         queue.add(10);
         queue.add(20);
         queue.add(30);
-        System.out.println("Queue: " + queue);
+        System.out.println("Added 10, 20, 30: " + queue);
 
         queue.add(40);
         queue.add(50);
-        System.out.println("Queue: " + queue);
+        System.out.println("Added 40, 50: " + queue);
 
-        queue.extract();
-        queue.extract();
-        queue.extract();
-        System.out.println("Queue: " + queue);
+        System.out.println("Extract: " + queue.extract() + " " + queue);
+        System.out.println("Extract: " + queue.extract() + " " + queue);
+        System.out.println("Extract: " + queue.extract() + " " + queue);
 
         queue.add(60);
-        System.out.println("Queue: " + queue);
+        System.out.println("Added 60: " + queue);
     }
 }
