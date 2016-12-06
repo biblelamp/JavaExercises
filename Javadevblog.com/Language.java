09f613b79d9dbcdab9b39bbedcdca672753216ed
Sergey Iryupin <biblelamp@yandex.ru>
2016-12-06 16:27:02 +0300

    class Language for example xml reader

---
+++ Javadevblog.com/Language.java
@@ -0,0 +1,25 @@
+class Language {
+    private String name; 
+    private int age; 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    } 
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Language { Name = " + name + ", Age = " + age + " }";
+    }
+}
