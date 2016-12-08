f913132742db29849df3801ce5aee4c1f6d545d3
Sergey Iryupin <biblelamp@yandex.ru>
2016-12-08 17:06:22 +0300

    example of xml editor based on DOM

---
+++ Javadevblog.com/DOMxmlEdit.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+import org.w3c.dom.*;
+ 
+public class DOMxmlEdit {
+
+    public static void main(String[] args) {
+        File xmlFile = new File("languages.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        try {
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+
+            // update the value of the elements name
+            updateElementValueName(doc);
+
+            // adding a new element
+            addElement(doc);
+
+            // output to a file and in console
+            doc.getDocumentElement().normalize();
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult file = new StreamResult(new File("languagesUpdated.xml"));
+            StreamResult console = new StreamResult(System.out);
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(source, file);
+            transformer.transform(source, console);
+            System.out.println("XML was successfully changed.");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // adding element paradigm
+    private static void addElement(Document doc) {
+        NodeList languages = doc.getElementsByTagName("Language");
+        Element lang = null;
+
+        // проходим по каждому элементу Language
+        for(int i = 0; i < languages.getLength(); i++){
+            lang = (Element) languages.item(i);
+            System.out.println(lang.getElementsByTagName("name").item(0).getFirstChild());
+            //if ((String)lang.getElementsByTagName("name").item(0).getFirstChild() != "C") {
+            //    Element paradigmElement = doc.createElement("paradigm");
+            //    paradigmElement.appendChild(doc.createTextNode("oop"));
+            //    lang.appendChild(paradigmElement);
+            //}
+        }
+    }
+
+    // change the name element
+    private static void updateElementValueName(Document doc) {
+        NodeList languages = doc.getElementsByTagName("Language");
+        Element lang = null;
+        // update the value of the elements name
+        for(int i = 0; i < languages.getLength(); i++){
+            lang = (Element) languages.item(i);
+            Node name = lang.getElementsByTagName("name").item(0).getFirstChild();
+            name.setNodeValue(name.getNodeValue().toUpperCase());
+        }
+    } 
+}
