4767e3f9c4503d7a9fe807ac5c88967800b867cb
Sergey Iryupin <biblelamp@yandex.ru>
2016-12-13 16:37:46 +0300

    example of interface

--- Javadevblog.com/ExampleInterface.java
+++ Javadevblog.com/ExampleInterface.java
@@ -1,30 +1,55 @@
-class ExampleInterface implements InterfaceA, InterfaceB, InterfaceC {
-
-    @Override
-    public void doSomething() {
-        System.out.println("doSomething is implemented in a particular class");
-    }
+class ExampleInterface {
 
     public static void main(String[] args) {
-        InterfaceA objA = new ExampleInterface();
-        InterfaceB objB = new ExampleInterface();
-        InterfaceC objC = new ExampleInterface();
+        Shape shape = new Circle(10);
+        System.out.println(shape.draw());
+        System.out.println("Area = " + shape.getArea());
 
-        // call methods with specific implementation
-        objA.doSomething();
-        objB.doSomething();
-        objC.doSomething();
+        shape = new Rectangle(10, 10);
+        System.out.println(shape.draw());
+        System.out.println("Area = " + shape.getArea());
     }
 }
 
-interface InterfaceA {
-    public void doSomething();
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String draw() {
+        return "Draw a circle";
+    }
+
+    @Override
+    public double getArea(){
+        return Math.PI * radius*radius;
+    }
 }
 
-interface InterfaceB {
-    public void doSomething();
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String draw() {
+        return "Draw a rectangle";
+    }
+
+    @Override
+    public double getArea() {
+        return height * width;
+    }
 }
 
-interface InterfaceC extends InterfaceA, InterfaceB {
-    public void doSomething();
+interface Shape { // define interface
+    String draw();
+    double getArea();
 }
