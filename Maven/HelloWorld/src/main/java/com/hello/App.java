2cb59e5dd2e3c1546060880ea40d4b083ef1a7f7
Sergey Iryupin <biblelamp@yandex.ru>
2017-01-25 14:47:59 +0300

    first example in maven

---
+++ Maven/HelloWorld/src/main/java/com/hello/App.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
