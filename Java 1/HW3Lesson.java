5803086c5c9e3f0260d6394e715d206b48e83c29
Sergey Iryupin <biblelamp@yandex.ru>
2017-02-22 15:37:12 +0300

    sample of hw 3 for students, added task of high complexity

--- Java 1/HW3Lesson.java
+++ Java 1/HW3Lesson.java
@@ -7,9 +7,20 @@
 import java.util.*;
 
 public class HW3Lesson {
+    static Random random = new Random();
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        guessTheNumber();
+        System.out.print("Choise game [1 - Guess the Number, 2 - Guess the Word]: ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                guessTheNumber();
+                break;
+            case 2:
+                guessTheWord();
+                break;
+        }
     }
 
     /**
@@ -20,8 +31,6 @@ public class HW3Lesson {
      *    «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
      */
     static void guessTheNumber() {
-        Random random = new Random();
-        Scanner sc = new Scanner(System.in);
         int repeat = 0;
         do {
             int count = 0;
@@ -33,14 +42,51 @@ public class HW3Lesson {
                 if (number == guess) {
                     System.out.println("You won!");
                 } else {
-                    System.out.println("Your number is " + ((guess > number)? "more" : "less."));
+                    System.out.println("Your number is " + ((guess > number)? "greater" : "less."));
                     count++;
                 }
             }
             if (count == 3)
                 System.out.println("You lost!");
             System.out.print("Repeat the game?\n[1-yes/0-no]: ");
-            again = sc.nextInt();
+            repeat = sc.nextInt();
         } while (repeat == 1);
+    }
+
+    /**
+     * 2. * Создать массив из слов
+     *      String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+     *      "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi",
+     *      "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper",
+     *      "pineapple", "pumpkin", "potato"};
+     *    При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
+     *    сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь.
+     *    Если слово не угадано, компьютер показывает буквы которые стоят на своих местах.
+     *    apple – загаданное
+     *    apricot - ответ игрока
+     *    ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
+     *    Для сравнения двух слов посимвольно, можно пользоваться:
+     *    String str = "apple";
+     *    str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции,
+     *    играем до тех пор, пока игрок не отгадает слово, используем только маленькие буквы
+     */
+    static void guessTheWord() {
+        String[] words = {
+            "apple", "orange", "lemon", "banana", "apricot",
+            "avocado", "broccoli", "carrot", "cherry", "garlic",
+            "grape", "melon", "leak", "kiwi", "mango",
+            "mushroom", "nut", "olive", "pea", "peanut",
+            "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        String guess = null;
+        String word = words[random.nextInt(words.length)];
+        System.out.println(Arrays.toString(words));
+        do {
+            System.out.print("Guess the word: ");
+            guess = sc.next();
+            for (int i = 0; i < 15; i++)
+                System.out.print((i < word.length() && i < guess.length() && word.charAt(i) == guess.charAt(i))?
+                    word.charAt(i) : ((word.equals(guess))? "" : "#"));
+            System.out.println();
+        } while (!word.equals(guess));
     }
 }
