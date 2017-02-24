e837ce38dc3a2f704d3a46f5c37ed0c2cf91e6c7
Sergey Iryupin <biblelamp@yandex.ru>
2017-02-24 16:24:45 +0300

    hello world for webinar second version

--- Ant/Webinar/src/HelloWorld.java
+++ Ant/Webinar/src/HelloWorld.java
@@ -1,6 +1,23 @@
-public class HelloWorld {
+import javax.swing.*;
+import java.awt.*;
+
+public class HelloWorld extends JFrame {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        new HelloWorld();
+    }
+
+    HelloWorld() {
+        setTitle(sayHello());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(200, 200, 300, 200);
+        JLabel label = new JLabel(sayHello(), SwingConstants.CENTER);
+        label.setFont(new Font("", Font.BOLD, 24));
+        add(label);
+        setVisible(true);
+    }
+
+    String sayHello() {
+        return "Hello, World!";
     }
 }
