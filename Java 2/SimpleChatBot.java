ece1bed8993238d687fabfc0d27bfddd228a5e24
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-03 11:10:44 +0300

    simple chat bot: main class v0.2

--- Java 2/SimpleChatBot.java
+++ Java 2/SimpleChatBot.java
@@ -2,50 +2,50 @@
  * Java. Simple Chat-Bot
  *
  * @author Sergey Iryupin
- * @version 0.1 dated Apr 2, 2016
+ * @version 0.2 dated Apr 3, 2016
  */
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import bot.*;
 
-class SimpleChatBot extends JFrame {
+class SimpleChatBot extends JFrame implements ActionListener {
 
     final String TITLE_OF_PROGRAM = "Simple Chat-Bot";
     final int START_LOCATION = 200;
     final int WINDOW_WIDTH = 350;
     final int WINDOW_HEIGHT = 450;
 
-    SimpleBot sbot = new SimpleBot();
+    JTextArea dialogue; // area for dialog
+    JCheckBox ai;       // enable/disable AI
+    JTextField message; // field for entering messages
+    SimpleBot sbot;     // chat-bot class (in bot package)
 
     public static void main(String[] args) {
         new SimpleChatBot();
     }
 
+    /**
+     * Constructor:
+     * Creating a window and all the necessary elements on it
+     */
     SimpleChatBot() {
         setTitle(TITLE_OF_PROGRAM);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(START_LOCATION, START_LOCATION, WINDOW_WIDTH, WINDOW_HEIGHT);
         setVisible(true);
-        // panel for dialog
-        JTextArea dialogue = new JTextArea();
+        // area for dialog
+        dialogue = new JTextArea();
         dialogue.setLineWrap(true);
         JScrollPane scrollBar = new JScrollPane(dialogue);
         // panel for checkbox, message field and button
         JPanel bp = new JPanel();
         bp.setLayout(new BoxLayout(bp, BoxLayout.X_AXIS));
-        JCheckBox ai = new JCheckBox("AI");
-        JTextField message = new JTextField();
+        ai = new JCheckBox("AI");
+        message = new JTextField();
+        message.addActionListener(this);
         JButton enter = new JButton("Enter");
-        enter.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dialogue.append(message.getText() + "\n");
-                dialogue.append(sbot.sayInReturn(message.getText()) + "\n");
-                message.setText("");
-                message.requestFocusInWindow();
-            }
-        });
+        enter.addActionListener(this);
         // adding all elements to the window
         bp.add(ai);
         bp.add(message);
@@ -53,5 +53,19 @@ class SimpleChatBot extends JFrame {
         add(BorderLayout.CENTER, scrollBar);
         add(BorderLayout.SOUTH, bp);
         setVisible(true);
+        sbot = new SimpleBot(); // creating bot object
+    }
+
+    /**
+     * Listener of events from message field and enter button
+     */
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (message.getText().trim().length() > 0) {
+            dialogue.append(message.getText() + "\n");
+            dialogue.append(sbot.sayInReturn(message.getText(), ai.isSelected()) + "\n");
+        }
+        message.setText("");
+        message.requestFocusInWindow();
     }
 }
