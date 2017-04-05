c6c7c01a70926e65033f3f0008239144bf1a31ef
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-05 16:15:22 +0300

    simple chatbot version 0.2.1

--- Java 2/SimpleChatBot.java
+++ Java 2/SimpleChatBot.java
@@ -2,11 +2,12 @@
  * Java. Chatter: simple chatbot
  *
  * @author Sergey Iryupin
- * @version 0.2 dated Apr 3, 2016
+ * @version 0.2.1 dated Apr 5, 2016
  */
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.text.*; // for StyledDocument
 import bot.*;
 
 class SimpleChatBot extends JFrame implements ActionListener {
@@ -16,10 +17,11 @@ class SimpleChatBot extends JFrame implements ActionListener {
     final int WINDOW_WIDTH = 350;
     final int WINDOW_HEIGHT = 450;
 
-    JTextArea dialogue; // area for dialog
+    JTextPane dialogue; // area for dialog
     JCheckBox ai;       // enable/disable AI
     JTextField message; // field for entering messages
     SimpleBot sbot;     // chat-bot class (in bot package)
+    SimpleAttributeSet botStyle; // style bot text
 
     public static void main(String[] args) {
         new SimpleChatBot();
@@ -33,11 +35,16 @@ class SimpleChatBot extends JFrame implements ActionListener {
         setTitle(TITLE_OF_PROGRAM);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBounds(START_LOCATION, START_LOCATION, WINDOW_WIDTH, WINDOW_HEIGHT);
-        setVisible(true);
         // area for dialog
-        dialogue = new JTextArea();
-        dialogue.setLineWrap(true);
+        dialogue = new JTextPane();
+        dialogue.setEditable(false);
+        dialogue.setContentType("text/html");
         JScrollPane scrollBar = new JScrollPane(dialogue);
+        // style for bot messages
+        botStyle = new SimpleAttributeSet();
+        StyleConstants.setItalic(botStyle, true);
+        StyleConstants.setForeground(botStyle, Color.blue);
+        //StyleConstants.setAlignment(botStyle, StyleConstants.ALIGN_RIGHT);
         // panel for checkbox, message field and button
         JPanel bp = new JPanel();
         bp.setLayout(new BoxLayout(bp, BoxLayout.X_AXIS));
@@ -63,8 +70,14 @@ class SimpleChatBot extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent event) {
         if (message.getText().trim().length() > 0) {
-            dialogue.append(message.getText() + "\n");
-            dialogue.append(sbot.sayInReturn(message.getText(), ai.isSelected()) + "\n");
+            try {
+                StyledDocument doc = dialogue.getStyledDocument();
+                doc.insertString(doc.getLength(), message.getText() + "\n", 
+                    new SimpleAttributeSet());
+                doc.insertString(doc.getLength(),
+                    sbot.sayInReturn(message.getText(), ai.isSelected()) + "\n",
+                    botStyle);
+            } catch(Exception e) { }
         }
         message.setText("");
         message.requestFocusInWindow();
