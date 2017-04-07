d53afa0d420e922d579c776ef3355f839de1ee92
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-07 16:44:07 +0300

    added volatile for multithreading

--- Head First Java/BankAccount.java
+++ Head First Java/BankAccount.java
@@ -1,10 +1,11 @@
 class BankAccount {
-	private int balance = 100;
+    private volatile int balance = 100; // volatile is neccesary
 
-	public int getBalance() {
-		return balance;
-	}
-	public void withdraw(int amount) {
-		balance = balance - amount;
-	}
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdraw(int amount) {
+        balance = balance - amount;
+    }
 }
