02a4640c4ec8a61745571cc9a56edd36813a44f4
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-07 10:14:30 +0300

    example thread with imp Runnable added threads

--- Java 2/lesson5/ImpRunnable.java
+++ Java 2/lesson5/ImpRunnable.java
@@ -2,7 +2,11 @@ class ImpRunnable {
 
     public static void main(String args[]) {
         System.out.println("Main thread is starting...");
-        MyThread mt = new MyThread("Child #1");
+
+        MyThread mt1 = new MyThread("Child #1");
+        //MyThread mt2 = new MyThread("Child #2");
+        //MyThread mt3 = new MyThread("Child #3");
+
         for (int i = 0; i < 50; i++) {
             System.out.print(".");
             try {
