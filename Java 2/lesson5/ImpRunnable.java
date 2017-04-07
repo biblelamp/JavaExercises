984384ac4cac48a196e70f856aed3eaa23a41630
Sergey Iryupin <biblelamp@yandex.ru>
2017-04-07 09:56:07 +0300

    example thread with imp Runnable, improved

--- Java 2/lesson5/ImpRunnable.java
+++ Java 2/lesson5/ImpRunnable.java
@@ -1,34 +1,40 @@
 class ImpRunnable {
+
     public static void main(String args[]) {
-        System.out.println("Starting main thread...");
-        TestThreadClass mpObj = new TestThreadClass();
-        Thread t1 = new Thread(mpObj); 
-        t1.setName("t1");
-        t1.start();
-        for (int i=0; i < 10; i++) {
-            System.out.println("main: " + i);
+        System.out.println("Main thread is starting...");
+        MyThread mt = new MyThread("Child #1");
+        for (int i = 0; i < 50; i++) {
+            System.out.print(".");
             try {
                 Thread.sleep(100);
             } catch (InterruptedException ex) {
-                System.out.println("Exception in main thread.");
+                System.out.println("Мain thread is interrupted.");
             }
         }
-        System.out.println("Completion main thread.");
+        System.out.println("Main thread is complete.");
     }
 }
 
-class TestThreadClass implements Runnable {
+class MyThread implements Runnable {
+    Thread thread;
+
+    // creating new thread
+    MyThread(String name) {
+        thread = new Thread(this, name);
+        thread.start();
+    }
+
+    // starting new thread
     public void run() {
-        String name = Thread.currentThread().getName();
-        System.out.println("Starting " + name + " thread...");
-        for (int i=0; i < 10; i++) {
+        System.out.println(thread.getName() + " is starting...");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(thread.getName() + ", counter " + i);
             try {
-                Thread.sleep(100);
+                Thread.sleep(400);
             } catch (InterruptedException ex) {
-                System.out.println("Exception in " + name + " thread.");
+                System.out.println(thread.getName() + " is interrupted.");
             }
-            System.out.println(name + ": " + i);
         }
-        System.out.println("Completion " + name + " thread.");
+        System.out.println(thread.getName() + " is complete.");
     }
 }
