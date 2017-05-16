d92244501749635cadb51c501f1a1f2b6d79717d
Sergey Iryupin <biblelamp@yandex.ru>
2017-05-16 05:43:03 +0300

    sample of hw 3 for students improved a bit

--- Java 1/HW3Lesson.java
+++ Java 1/HW3Lesson.java
@@ -2,7 +2,7 @@
  * Java. Level 1. Lesson 3. Example of homework
  *
  * @author Sergey Iryupin
- * @version dated 22 Feb 2017
+ * @version dated May 16, 2017
  */
 import java.util.*;
 
@@ -11,15 +11,20 @@ public class HW3Lesson {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.print("Choise game [1 - Guess the Number, 2 - Guess the Word]: ");
-        int choice = sc.nextInt();
-        switch (choice) {
-            case 1:
-                guessTheNumber();
-                break;
-            case 2:
-                guessTheWord();
-                break;
+        while (true) {
+            System.out.print("Make a choice:\n"+
+                "1. Guess the Number\n2. Guess the Word\n3. Exit\n: ");
+            String choice = sc.next();
+            switch (choice) {
+                case "1":
+                    guessTheNumber();
+                    break;
+                case "2":
+                    guessTheWord();
+                    break;
+                default:
+                    return;
+            }
         }
     }
 
@@ -28,47 +33,57 @@ public class HW3Lesson {
      *    и пользователю дается 3 попытки угадать это число. При каждой попытке
      *    компьютер должен сообщить больше ли указанное пользователем число чем
      *    загаданное, или меньше. После победы или проигрыша выводится запрос
-     *    «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
+     *    «Повторить игру еще раз? 1 – да / 0 – нет» (1 – повторить, 0 – нет).
      */
     static void guessTheNumber() {
-        int repeat = 0;
+        String repeat = "0";
         do {
             int count = 0;
             int guess = -1;
             int number = random.nextInt(10);
             while (count < 3 && guess != number) {
-                System.out.print("Guess [" + (count + 1) + "] the number (0..9): ");
-                guess = sc.nextInt();
-                if (number == guess) {
-                    System.out.println("You won!");
-                } else {
-                    System.out.println("Your number is " + ((guess > number)? "greater" : "less."));
-                    count++;
+                System.out.print(
+                    "Guess [" + (count + 1) + "] the number (0..9): ");
+                try {
+                    guess = sc.nextInt();
+                    if (number == guess) {
+                        System.out.println("You won!");
+                    } else {
+                        System.out.println("Your number is " + ((guess < number)?
+                            "greater" : "less."));
+                        count++;
+                    }
+                } catch (InputMismatchException ex) {
+                    System.out.println("Input Mismatch Exception!");
+                    sc.next();
                 }
             }
             if (count == 3)
                 System.out.println("You lost!");
-            System.out.print("Repeat the game?\n[1-yes/0-no]: ");
-            repeat = sc.nextInt();
-        } while (repeat == 1);
+            System.out.print("Repeat the game?\n[1 - yes / 0 - no]: ");
+            repeat = sc.next();
+        } while (repeat.equals("1"));
     }
 
     /**
      * 2. * Создать массив из слов
-     *      String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
-     *      "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi",
-     *      "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper",
-     *      "pineapple", "pumpkin", "potato"};
-     *    При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя,
-     *    сравнивает его с загаданным словом и сообщает правильно ли ответил пользователь.
-     *    Если слово не угадано, компьютер показывает буквы которые стоят на своих местах.
-     *    apple – загаданное
-     *    apricot - ответ игрока
-     *    ap############# (15 символов, чтобы пользователь не мог узнать длину слова)
+     *      String[] words = {"apple", "orange", "lemon", "banana", "apricot",
+     *      "avocado", "broccoli", "carrot", "cherry", "garlic", "grape",
+     *      "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive",
+     *      "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin",
+     *      "potato"};
+     *    При запуске программы компьютер загадывает слово, запрашивает ответ
+     *    у пользователя, сравнивает его с загаданным словом и сообщает
+     *    правильно ли ответил пользователь. Если слово не угадано, компьютер
+     *    показывает буквы которые стоят на своих местах.
+     *      apple – загаданное
+     *      apricot - ответ игрока
+     *      ap#############
+     *    (15 символов, чтобы пользователь не мог узнать длину слова)
      *    Для сравнения двух слов посимвольно, можно пользоваться:
-     *    String str = "apple";
-     *    str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции,
-     *    играем до тех пор, пока игрок не отгадает слово, используем только маленькие буквы
+     *    str.charAt(0) - метод вернет char, который стоит в слове str на
+     *    первой позиции, играем до тех пор, пока игрок не отгадает слово,
+     *    используем только маленькие буквы
      */
     static void guessTheWord() {
         String[] words = {
@@ -84,7 +99,9 @@ public class HW3Lesson {
             System.out.print("Guess the word: ");
             guess = sc.next();
             for (int i = 0; i < 15; i++)
-                System.out.print((i < word.length() && i < guess.length() && word.charAt(i) == guess.charAt(i))?
+                System.out.print(
+                    (i < word.length() && i < guess.length() 
+                        && word.charAt(i) == guess.charAt(i))?
                     word.charAt(i) : ((word.equals(guess))? "" : "#"));
             System.out.println();
         } while (!word.equals(guess));
