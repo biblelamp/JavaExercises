ce09ba0c72dc657da21ccd640aa72a65b91e21ba
Sergey Iryupin <biblelamp@yandex.ru>
2017-06-20 20:11:35 +0300

    improved a bit

--- Java 2/lesson5/MoreThreads.java
+++ Java 2/lesson5/MoreThreads.java
@@ -3,7 +3,7 @@ class MoreThreads { // using isAlive() method
     public static void main(String args[]) {
         System.out.println("Main thread is starting...");
 
-        MyThread mt1 = new MyThread("Child #1");
+        MyThread mt1 = new MyThread("Child #1"); // based on MyThread extends Thread
         MyThread mt2 = new MyThread("Child #2");
         MyThread mt3 = new MyThread("Child #3");
 
@@ -14,9 +14,9 @@ class MoreThreads { // using isAlive() method
             } catch (InterruptedException ex) {
                 System.out.println("Мain thread is interrupted.");
             }
-        } while (mt1.thread.isAlive() || // waiting for completion all threads
-                 mt2.thread.isAlive() ||
-                 mt3.thread.isAlive());
+        } while (mt1.isAlive() || // waiting for completion all threads
+                 mt2.isAlive() ||
+                 mt3.isAlive());
         System.out.println("Main thread is complete.");
     }
 }
