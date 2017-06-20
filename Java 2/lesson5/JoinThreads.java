58a75ee6902f4cf86b3d953d0ab054d7d82cb3ad
Sergey Iryupin <biblelamp@yandex.ru>
2017-06-20 20:22:59 +0300

    improved a bit

--- Java 2/lesson5/JoinThreads.java
+++ Java 2/lesson5/JoinThreads.java
@@ -3,17 +3,17 @@ class JoinThreads { // using join() method
     public static void main(String args[]) {
         System.out.println("Main thread is starting...");
 
-        MyThread mt1 = new MyThread("Child #1");
+        MyThread mt1 = new MyThread("Child #1"); // based on MyThread extends Thread
         MyThread mt2 = new MyThread("Child #2");
         MyThread mt3 = new MyThread("Child #3");
 
         try {
-            mt1.thread.join();
-            mt2.thread.join();
-            mt3.thread.join();
-            System.out.println(mt1.thread.getName() + " is joined.");
-            System.out.println(mt2.thread.getName() + " is joined.");
-            System.out.println(mt3.thread.getName() + " is joined.");
+            mt1.join();
+            mt2.join();
+            mt3.join();
+            System.out.println(mt1.getName() + " is joined.");
+            System.out.println(mt2.getName() + " is joined.");
+            System.out.println(mt3.getName() + " is joined.");
         } catch (InterruptedException ex) {
             System.out.println("Мain thread is interrupted.");
         }
