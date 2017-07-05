c276f1b06296204a5d7bc033b61f46f663b84664
Sergey Iryupin <biblelamp@yandex.ru>
2017-07-05 17:39:57 +0300

    making db file for lesson 7 ver 0.2

--- Java 2/lesson7/MakeDBFile.java
+++ Java 2/lesson7/MakeDBFile.java
@@ -3,16 +3,23 @@
  * Making db file with users
  *
  * @author Sergey Iryupin
- * @version 0.1.1 dated Jul 01, 2017
+ * @version 0.2 dated Jul 05, 2017
  */
 import java.sql.*;
 
-class MakeDBFile {
+class MakeDBFile implements IConstants {
 
-    final String DRIVER_NAME = "org.sqlite.JDBC";
-    final String PREFIX_NAME = "jdbc:sqlite:";
-    final String NAME_DB = "chat.db";
     final String NAME_TABLE = "users";
+    final String SQL_CREATE_TABLE = "CREATE TABLE " + NAME_TABLE +
+        "(login  CHAR(6) PRIMARY KEY NOT NULL," +
+        " passwd CHAR(6) NOT NULL);";
+    final String SQL_INSERT_MIKE = "INSERT INTO " + NAME_TABLE +
+        " (login, passwd) " +
+        "VALUES ('mike', 'qwerty');";
+    final String SQL_INSERT_JONH = "INSERT INTO " + NAME_TABLE +
+        " (login, passwd) " +
+        "VALUES ('john', '12345');";
+    final String SQL_SELECT = "SELECT * FROM " + NAME_TABLE + ";";
 
     Connection connect;
     Statement stmt;
@@ -27,34 +34,28 @@ class MakeDBFile {
         // open db file
         try {
             Class.forName(DRIVER_NAME);
-            connect = DriverManager.getConnection(PREFIX_NAME + NAME_DB);
+            connect = DriverManager.getConnection(SQLITE_DB);
         } catch (Exception e) { }
 
         // create table
         try {
             stmt = connect.createStatement();
-            stmt.executeUpdate("CREATE TABLE " + NAME_TABLE +
-                "(login  CHAR(6) PRIMARY KEY NOT NULL," +
-                " passwd CHAR(6) NOT NULL);");
+            stmt.executeUpdate(SQL_CREATE_TABLE);
         } catch (Exception e) { }
 
         // insert record(s)
         try {
-            stmt.executeUpdate("INSERT INTO " + NAME_TABLE +
-                " (login, passwd) " +
-                "VALUES ('mike', 'qwerty');");
-            stmt.executeUpdate("INSERT INTO " + NAME_TABLE +
-                " (login, passwd) " +
-                "VALUES ('john', '12345');");
+            stmt.executeUpdate(SQL_INSERT_MIKE);
+            stmt.executeUpdate(SQL_INSERT_JONH);
         } catch (Exception e) { }
 
         // print records
         try {
-            rs = stmt.executeQuery("SELECT * FROM " + NAME_TABLE + ";" );
+            rs = stmt.executeQuery(SQL_SELECT);
             System.out.println("LOGIN\tPASSWD");
             while (rs.next()) {
                 System.out.println(rs.getString("login") + "\t" +
-                    rs.getString("passwd"));
+                    rs.getString(PASSWD_COL));
             }
         } catch (Exception e) { }
     }
