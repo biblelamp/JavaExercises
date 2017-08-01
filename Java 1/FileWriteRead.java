8f1facf75bf17661b92901e7ce71f76bccffef55
Sergey Iryupin <biblelamp@yandex.ru>
2017-08-01 17:03:51 +0300

    simple writing/reading file example

---
+++ Java 1/FileWriteRead.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+class FileWriteRead {
+
+    public static void main(String[] args) {
+
+        // writing to file
+        try {
+            FileWriter file = new FileWriter("text.txt");
+            file.write("Hello\nJava");
+            file.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        // reading from file
+        try {
+            int b;
+            FileReader file = new FileReader("text.txt");
+            while ((int b = file.read()) != -1)
+                System.out.print((char)b);
+            file.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
