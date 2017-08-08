20b738fed27de95d6c37f2b0ef7e2e60a8407a85
Sergey Iryupin <biblelamp@yandex.ru>
2017-08-08 16:39:44 +0300

    example hw 4 with simple AI

--- Java 1/HW4Lesson.java
+++ Java 1/HW4Lesson.java
@@ -1,6 +1,6 @@
 /**
  * Java. Level 1. Lesson 4. Example of homework
- *  Tic-tac-toe in console with some optimisation
+ *  Tic-tac-toe in console with simple AI
  *
  * @author Sergey Iryupin
  * @version dated Aug 08, 2017
@@ -42,7 +42,7 @@ class HW4Lesson {
             }
             /*if (isMapFull()) {                    // this code doesn't matter
                 System.out.println("Sorry, DRAW!"); // because a human always
-                break;                              //  makes a move last
+                break;                              //  makes a move last in 3x3
             }*/
         }
         System.out.println("GAME OVER.");
@@ -76,6 +76,16 @@ class HW4Lesson {
 
     void turnAI() {                                 // AI action
         int x, y;
+        for (x = 0; x < SIZE; x++)                  // simple blocking
+            for (y = 0; y < SIZE; y++)
+                if (isCellValid(x, y)) {            // if cell empty
+                    map[y][x] = DOT_X;              // try to use like human
+                    if (checkWin(DOT_X)) {          // if win
+                        map[y][x] = DOT_O;          // block
+                        return;                     // and exit
+                    }
+                    map[y][x] = DOT_EMPTY;          // restore cell
+                }
         do {
             x = rand.nextInt(SIZE);
             y = rand.nextInt(SIZE);
