012b5b7f05366e595af92916279bbbabfb4e4892
Sergey Iryupin <biblelamp@yandex.ru>
2017-08-11 13:12:57 +0300

    evaluates triangular numbers

---
+++ Algorithms/recursion/triangle.java
@@ -0,0 +1,41 @@
+/**
+ * Book: Data Structures and Algorithms in Java, by Robert LaFore
+ * Chapter 6:
+ *  triangle.java
+ *  evaluates triangular numbers
+ *  to compile this code: javac triangle.java
+ *  to run this program:  java TriangleApp
+ */
+import java.io.*;
+
+class TriangleApp {
+    static int theNumber;
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Enter a number: ");
+        theNumber = getInt();
+        int theAnswer = triangle(theNumber);
+        // we can check result using formula:
+        //  result = (n^2 + n)/2
+        System.out.println("Triangle = " + theAnswer);
+    }
+
+    public static int triangle(int n) {
+        if (n==1)
+            return 1;
+        else
+            return (n + triangle(n-1));
+        }
+
+    public static String getString() throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
+    }
+
+    public static int getInt() throws IOException {
+        String s = getString();
+        return Integer.parseInt(s);
+    }
+}
