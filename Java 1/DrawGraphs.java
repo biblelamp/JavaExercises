1ed2ccdd181f1ec70ff1e02a5a792469e3e0d312
Sergey Iryupin <biblelamp@yandex.ru>
2017-08-14 13:25:16 +0300

    added 3 keys for choosing function

--- Java 1/DrawGraphs.java
+++ Java 1/DrawGraphs.java
@@ -2,7 +2,7 @@
  * Java. Level 1. Drawing graphs of simple functions
  *
  * @author Sergey Iryupin
- * @version 0.2 dated Aug 13, 2017
+ * @version 0.3 dated Aug 14, 2017
  */
 import javax.swing.*;
 import java.awt.*;
@@ -18,10 +18,14 @@ class DrawGraphs extends JFrame implements KeyListener {
     final int UP = 38;
     final int RIGHT = 39;
     final int DOWN = 40;
+    final int SIN = 83;     // for choosing function
+    final int COS = 67;
+    final int TAN = 84;
 
     Canvas canvas; // JPanel for painting
     float stretchX = 1.2f;
     int stretchY = 75;
+    int typeGraph = SIN;    // default type of function
 
     public static void main(String[] args) {
         new DrawGraphs();
@@ -60,9 +64,16 @@ class DrawGraphs extends JFrame implements KeyListener {
             case DOWN: stretchY -= 5;
                 canvas.repaint();
                 break;
+            case SIN:
+            case COS:
+            case TAN: typeGraph = event.getKeyCode();
+                canvas.repaint();
+                break;
+            default:
+                System.out.println(event.getKeyCode());
         }
     }
-    
+
     class Canvas extends JPanel { // for painting
         @Override
         public void paint(Graphics g) {
@@ -76,15 +87,39 @@ class DrawGraphs extends JFrame implements KeyListener {
             // drawing coordinate lines
             g.drawLine(5, height / 2, width - 6, height / 2);
             g.drawLine(width / 2, 5, width / 2, height - 6);
-            g.drawString("X", width - 15, height / 2 - 10);
+
+            // draving arrows Х
+            g.drawLine(width - 6, height / 2, width - 6 - 7, height / 2 - 3);
+            g.drawLine(width - 6, height / 2, width - 6 - 7, height / 2 + 3);
+
+            // draving arrows Y
+            g.drawLine(width / 2, 5, width / 2 - 3, 5 + 7);
+            g.drawLine(width / 2, 5, width / 2 + 3, 5 + 7);
+
+            // drawing name of coordinates
+            g.drawString("X", width - 15, height / 2 - 8);
             g.drawString("Y", width / 2 - 15, 20);
+            g.drawString("Sin | Cos | Tan", 10, height - 15);
 
             // converting degrees to radians
             // radians = degrees * Math.PI/180
 
             g.setColor(Color.blue);
             for (int x = -(width / 2 - 10), y = 0; x < width /2 - 10; x++) {
-                y = (int) (Math.sin(x * Math.PI/180 * stretchX) * stretchY);
+                double radian = x * Math.PI/180 * stretchX;
+                switch (typeGraph) {
+                    case SIN:
+                        g.drawString("SIN", 10, 20);
+                        y = (int) (Math.sin(radian) * stretchY);
+                        break;
+                    case COS:
+                        g.drawString("COS", 10, 20);
+                        y = (int) (Math.cos(radian) * stretchY);
+                        break;
+                    case TAN:
+                        g.drawString("TAN", 10, 20);
+                        y = (int) (Math.tan(radian) * stretchY);
+                }
                 g.drawRect(x + width / 2, y + height / 2, 0, 0);
             }
         }
