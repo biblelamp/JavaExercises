2637131b67a618ab144d7f090074e011ec698b6e
Sergey Iryupin <biblelamp@yandex.ru>
2017-08-26 16:16:10 +0300

    shooting ball class ball v0.2

--- Java 2/ShootingBall/src/Ball.java
+++ Java 2/ShootingBall/src/Ball.java
@@ -3,31 +3,30 @@
  *  Class Ball
  *
  * @author Sergey Iryupin
- * @version 0.1 dated Aug 24, 2017
+ * @version 0.2 dated Aug 26, 2017
  */
 import java.awt.Graphics;
 
 class Ball {
-    private int x, y, radius;
-    private double angle;
+    private int x, y, radius, angle;
 
     Ball(int x, int y, int radius) {
         this.x = x;
         this.y = y;
-        this.radius = radius / 2;
+        this.radius = radius;
         angle = 270;
     }
 
     void forward(int points) {
-        double dx = (points) * Math.cos(angle * Math.PI/180);
-        double dy = (points) * Math.sin(angle * Math.PI/180);
+        double dx = points * Math.cos(Math.toRadians(angle));
+        double dy = points * Math.sin(Math.toRadians(angle));
         x += (int) dx;
         y += (int) dy;
     }
 
     void backward(int points) {
-        double dx = (points) * Math.cos(angle * Math.PI/180);
-        double dy = (points) * Math.sin(angle * Math.PI/180);
+        double dx = points * Math.cos(Math.toRadians(angle));
+        double dy = points * Math.sin(Math.toRadians(angle));
         x -= (int) dx;
         y -= (int) dy;
     }
@@ -40,10 +39,24 @@ class Ball {
         this.angle += angle;
     }
 
+    int getXPill() {
+        return (int)(x + (radius + 4) * Math.cos(Math.toRadians(angle)));
+    }
+
+    int getYPill() {
+        return (int)(y + (radius + 4) * Math.sin(Math.toRadians(angle)));
+    }
+
+    int getAngle() {
+        return angle;
+    }
+
     void paint(Graphics g) {
-        double dx = (radius + 5) * Math.cos(angle * Math.PI/180);
-        double dy = (radius + 5) * Math.sin(angle * Math.PI/180);
+        double x1 = (radius - 4) * Math.cos(Math.toRadians(angle));
+        double y1 = (radius - 4) * Math.sin(Math.toRadians(angle));
+        double dx = (radius + 4) * Math.cos(Math.toRadians(angle));
+        double dy = (radius + 4) * Math.sin(Math.toRadians(angle));
         g.drawOval(x - radius, y - radius, radius * 2, radius * 2);
-        g.drawLine(x, y, x + (int)dx, y + (int)dy);
+        g.drawLine(x + (int)x1, y + (int)y1, x + (int)dx, y + (int)dy);
     }
 }
