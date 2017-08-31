71825cc0570a643a1026157c25fab1ceb1c83a4d
Sergey Iryupin <biblelamp@yandex.ru>
2017-08-31 16:10:04 +0300

    shooting ball class ballbot v0.2

--- Java 2/ShootingBall/src/BallBot.java
+++ Java 2/ShootingBall/src/BallBot.java
@@ -3,8 +3,9 @@
  *  Class BallBot
  *
  * @author Sergey Iryupin
- * @version 0.1 dated Aug 29, 2017
+ * @version 0.2 dated Aug 31, 2017
  */
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
@@ -13,40 +14,51 @@ class BallBot {
     private Pill pill;
     private int typeOfAction, steps;
     private Random random;
+    private final int WIDTH, HEIGHT;
 
     BallBot(int width, int height) {
-        ball = new Ball(100, 100, 10, 0, width, height);
+        WIDTH = width;
+        HEIGHT = height;
+        ball = new Ball(WIDTH / 2, HEIGHT / 2, 10, 0, WIDTH, HEIGHT);
         random = new Random();
-        typeOfAction = -1;
         steps = 0;
     }
 
     void action() {
         if (steps == 0) {
             typeOfAction = random.nextInt(5);
-            if (typeOfAction == 4)
-                pill = new Pill(
-                    ball.getXPill(), ball.getYPill(), ball.getAngle());
-            else
+            if (typeOfAction == 0) {
+                if (pill == null) {
+                    pill = new Pill(
+                        ball.getXPill(), ball.getYPill(), 
+                        ball.getAngle(), Color.red);
+                    typeOfAction = 4;
+                    steps = 6;
+                }
+            } else
                 steps = random.nextInt(10) + 1;
         }
         switch (typeOfAction) {
-            case 0: ball.turnLeft(steps--);
+            case 1: ball.turn(-(steps*5));
+                steps--;
                 break;
-            case 1: ball.turnRight(steps--);
+            case 2: ball.turn(steps*5);
+                steps--;
                 break;
-            case 2: ball.forward(steps--);
+            case 3: ball.move(steps--);
                 break;
-            case 3: ball.backward(steps--);
+            case 4: ball.move(-(steps--));
                 break;
         }
         if (pill != null)
-            pill.move(5);
+            if (pill.getX() < 0 || pill.getY() < 0 ||
+                pill.getX() > WIDTH || pill.getY() > HEIGHT)
+                pill = null;
+            else pill.move(5);
     }
 
     void paint(Graphics g) {
         ball.paint(g);
-        if (pill != null)
-            pill.paint(g);
+        if (pill != null) pill.paint(g);
     }
 }
