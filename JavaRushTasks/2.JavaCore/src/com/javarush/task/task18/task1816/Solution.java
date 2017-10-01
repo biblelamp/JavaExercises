40fcfe0436be958b10ea2e88071ecc0f31641454
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-01 15:26:23 +0300

    solved JavaCore tasks 1816 lvl 08

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -1,10 +1,22 @@
 package com.javarush.task.task18.task1816;
 
-/* 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/*
 Английские буквы
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int countEL = 0;
+        FileInputStream file = new FileInputStream(args[0]);
+        while (file.available() > 0) {
+            byte b = (byte) file.read();
+            if ((b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z'))
+                countEL++;
+        }
+        file.close();
+        System.out.println(countEL);
     }
 }
