2e142fb6bddda5c42d6ff1ade05b47494a345080
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-01 14:52:07 +0300

    solved JavaCore tasks lvl 08 16, 17

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1817/Solution.java
@@ -1,11 +1,22 @@
 package com.javarush.task.task18.task1817;
 
-/* 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/*
 Пробелы
 */
 
 public class Solution {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        int countSpaces = 0;
+        FileInputStream file = new FileInputStream(args[0]);
+        int countChars = file.available();
+        while (file.available() > 0) {
+            if ((byte) file.read() == ' ')
+                countSpaces++;
+        }
+        file.close();
+        System.out.printf("%.2f", (double) countSpaces/countChars*100);
     }
 }
