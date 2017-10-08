a1d785d887db07d863434b7614a770f523b50368
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-08 14:10:56 +0300

    changed to work with sqlile db

--- Test Tasks/mts.ru/DBConnector.java
+++ Test Tasks/mts.ru/DBConnector.java
@@ -1,19 +1,17 @@
 /**
  * Class DBConnector
- * The class simulates the work with the database:
- *  - the product table is simulated by several HashMap
- *  - the table storing requests is emulated by a text file
+ * The class provides work with the SQLite database:
+ *  - table PRODUCT stores information about products
+ *  - table LOG stores requests history
  *
  * @author Sergey Iryupin
- * @version 0.2 dated Oct 07, 2017
+ * @version 0.2.1 dated Oct 08, 2017
  */
 import java.util.Date;
-import java.util.*;
-import java.io.*;
+import java.text.SimpleDateFormat;
 import java.sql.*;
 
 class DBConnector {
-    static final String LOG_FILE_NAME = "journal.txt";
     static final String DRIVER_NAME = "org.sqlite.JDBC";
     static final String DB_FILE = "product.db";
     static Connection connect = null;
@@ -40,34 +38,26 @@ class DBConnector {
                 e.printStackTrace();
             }
             writeLog(
-                name + "\tsell\t" + Integer.toString(quantity) + "\tsuccess");
-            // change price
-            if (quantity / batch > 0)
-                try (Statement stmt = connect.createStatement()) {
-                    int newPrice = price + quantity / batch * change;
-                    stmt.executeUpdate(
-                        "UPDATE product SET price=" + newPrice +
-                        " WHERE name='" + name + "';");
-                writeLog(
-                    name + "\tprice changed\t" + newPrice + "\tsuccess");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                name, "sell " + Integer.toString(quantity), "success");
+            // change price if it need
+            int rest = sales % batch; // get the rest of saled batches
+            if ((rest + quantity) / batch > 0)
+                setPrice(name, price + (rest + quantity) / batch * change);
         } else
             writeLog(
-                name + "\tsell\t" + Integer.toString(quantity) + "\tfailed");
+                name, "sell " + quantity, "failed");
     }
 
     public static void setPrice(String name, int price) { // set price by name
         openDB();
         try (Statement stmt = connect.createStatement()) {
-            stmt.executeUpdate(
+            int i = stmt.executeUpdate(
                 "UPDATE product SET price=" + price + " WHERE name='" + name + "';");
         } catch (Exception e) {
             e.printStackTrace();
         }
         writeLog(
-            name + "\tset price\t" + Integer.toString(price) + "\tsuccess");
+            name, "set price " + price, "success");
     }
 
     private static Integer getAmount(String name) { // get amount by name
@@ -97,34 +87,41 @@ class DBConnector {
             e.printStackTrace();
         }
         writeLog(
-            name + "\tadd\t" + Integer.toString(quantity) + "\tsuccess");
+            name, "add " + quantity, "success");
     }
 
     private static void openDB() { // open and init DB if it's needed
         try {
-            // open db
-            Class.forName(DRIVER_NAME);
-            connect = DriverManager.getConnection("jdbc:sqlite:" + DB_FILE);
-            // create tables
-            Statement stmt = connect.createStatement();
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS product" +
-                "(name   CHAR(12) PRIMARY KEY NOT NULL," +
-                " price  INT NOT NULL," +
-                " amount INT," +
-                " sales  INT," +  // sold quantity
-                " batch  INT," +  // batch size for price change
-                " change INT);"); // price change
-            // add two records if table is empty
-            ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM product;");
-            if (rs.getInt(1) == 0) {
-                stmt.executeUpdate("INSERT INTO product" +
-                    "(name, price, amount, batch, change) " +
-                    "VALUES ('apple', 15, 1000, 100, -1);");
-                stmt.executeUpdate("INSERT INTO product" +
-                    "(name, price, amount, batch, change) " +
-                    "VALUES ('wine', 250, 50, 20, 10);");
+            if (connect == null) {
+                // open db
+                Class.forName(DRIVER_NAME);
+                connect = DriverManager.getConnection("jdbc:sqlite:" + DB_FILE);
+                // create tables
+                Statement stmt = connect.createStatement();
+                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS product" +
+                    "(name   CHAR(12) PRIMARY KEY NOT NULL," +
+                    " price  INT NOT NULL," +
+                    " amount INT," +
+                    " sales  INT," +  // sold quantity
+                    " batch  INT," +  // batch size for price change
+                    " change INT);"); // price change
+                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS log" +
+                    "(date   TEXT NOT NULL," +
+                    " name   CHAR(12)," +
+                    " action TEXT," +
+                    " result CHAR(8));");
+                // add two records if table is empty
+                ResultSet rs = stmt.executeQuery("SELECT COUNT(*) FROM product;");
+                if (rs.getInt(1) == 0) {
+                    stmt.executeUpdate("INSERT INTO product" +
+                        "(name, price, amount, batch, change) " +
+                        "VALUES ('apple', 15, 1000, 100, -1);");
+                    stmt.executeUpdate("INSERT INTO product" +
+                        "(name, price, amount, batch, change) " +
+                        "VALUES ('wine', 250, 50, 20, 10);");
+                }
+                stmt.close();
             }
-            stmt.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -144,13 +141,16 @@ class DBConnector {
         return result;
     }
 
-    private static void writeLog(String str) { // write to log file
-        try (PrintWriter log = 
-            new PrintWriter(
-                new FileWriter(LOG_FILE_NAME, true))) {
-            log.write((new Date()) + "\t" + str + "\n");
-        } catch (IOException ex) {
-            ex.printStackTrace();
+    private static void writeLog(String name, String action, String result) {
+        openDB();
+        try (Statement stmt = connect.createStatement()) {
+            stmt.executeUpdate("INSERT INTO log" +
+                "(date, name, action, result) " +
+                "VALUES ('" + (new SimpleDateFormat(
+                    "yyyy-MM-dd HH:mm:ss:SSS").format(new Date())) + "', '" +
+                name + "', '" + action + "', '" + result + "');");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
