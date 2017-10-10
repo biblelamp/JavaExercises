816034b58845b991ff647b51bd2a0e10992692cb
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-10 14:40:53 +0300

    class orders

---
+++ Test Tasks/epam.com/Orders.java
@@ -0,0 +1,51 @@
+/**
+ * Class Orders
+ * The class provides work with the orders list
+ *
+ * @author Sergey Iryupin
+ * @version 0.1 dated Oct 10, 2017
+ */
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.util.*;
+
+class Orders {
+    private Map<String, String[]> orders;
+
+    Orders(String xmlFile) {
+        orders = new HashMap<>();
+        try {
+            DocumentBuilderFactory documentBuilderFactory =
+                DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder =
+                documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(xmlFile);
+            NodeList nodeList =
+                document.getElementsByTagName(
+                    document.getDocumentElement().getChildNodes().item(1).
+                    getNodeName());
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                Element element = (Element) node;
+                String name =
+                    element.getElementsByTagName("client").item(0).getTextContent();
+                NodeList list = element.getElementsByTagName("item");
+                String[] items = new String[list.getLength()];
+                for (int j = 0; j < list.getLength(); j++)
+                    items[j] = list.item(j).getTextContent();
+                orders.put(name, items);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = "{";
+        for (Map.Entry<String, String[]> item : orders.entrySet())
+            result += item.getKey() + "=" +
+                Arrays.toString(item.getValue()) + ", ";
+        return result.substring(0, result.length() - 2) + "}";
+    }
+}
