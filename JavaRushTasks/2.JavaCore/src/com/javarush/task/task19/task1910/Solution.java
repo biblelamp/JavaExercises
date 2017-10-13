07c27a692b48d5fc68a35fe02f72bc51934833be
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-13 23:06:28 +0300

    solved JavaCore task lvl 09 1910

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -1,10 +1,32 @@
 package com.javarush.task.task19.task1910;
 
-/* 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
 Пунктуация
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileIn = reader.readLine();
+        String fileOut = reader.readLine();
+        reader.close();
+
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileIn));
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileOut));
+
+        while (fileReader.ready()) {
+            String str = fileReader.readLine().replaceAll("[\\p{Punct}\\n]", "");
+            fileWriter.write(str);
+        }
+
+        fileReader.close();
+        fileWriter.close();
     }
 }
