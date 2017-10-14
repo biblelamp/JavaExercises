a7bb5807afcb03d5c6fafba89f7dbff7caeb9a77
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-14 17:00:05 +0300

    solved JavaCore task lvl 09 1911

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task19.task1911;
 
-/* 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/*
 Ридер обертка
 */
 
@@ -8,7 +11,28 @@ public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        // save real PrintStream in special variable
+        PrintStream consoleStream = System.out;
 
+        // create dynamic array
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        // create adapter for class PrintStream...
+        PrintStream stream = new PrintStream(outputStream);
+        // ...and set it as System.out
+        System.setOut(stream);
+
+        // call the function
+        testString.printSomething();
+
+        // convert writing in our ByteArray data to string + up case all
+        String result = outputStream.toString().toUpperCase();
+
+        // restore the real PrintStream
+        System.setOut(consoleStream);
+
+        // output result string
+        System.out.println(result);
     }
 
     public static class TestString {
