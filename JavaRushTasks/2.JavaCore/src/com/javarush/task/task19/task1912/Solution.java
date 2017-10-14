39ccf3b3fc7ada27f9d23e81a7051a349ac7d17f
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-14 17:08:37 +0300

    solved JavaCore task lvl 09 1912

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task19.task1912;
 
-/* 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/*
 Ридер обертка 2
 */
 
@@ -8,6 +11,28 @@ public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        // save real PrintStream in special variable
+        PrintStream consoleStream = System.out;
+
+        // create dynamic array as output stream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        // create adapter for class PrintStream...
+        PrintStream stream = new PrintStream(outputStream);
+        // ...and set it as System.out
+        System.setOut(stream);
+
+        // call the function
+        testString.printSomething();
+
+        // restore the real PrintStream
+        System.setOut(consoleStream);
+
+        // convert writing in our ByteArray data to string + replace something
+        String result = outputStream.toString().replaceAll("te", "\\?\\?");
+
+        // output result string
+        System.out.println(result);
     }
 
     public static class TestString {
