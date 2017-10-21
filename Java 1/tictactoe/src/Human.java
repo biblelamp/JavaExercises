a13b1d20381c8ec33be0b357618858f58632fbe0
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-21 13:53:55 +0300

    tic-tac-toe field class ver 0.1.1 for lesson 8

--- Java 1/tictactoe/src/Human.java
+++ Java 1/tictactoe/src/Human.java
@@ -3,7 +3,7 @@
  * Class: Human
  *
  * @author Sergey Iryupin
- * @version 0.1 dated March 11, 2017
+ * @version 0.1.1 dated Oct 21, 2017
  */
 class Human {
     private final char DOT;
@@ -11,7 +11,7 @@ class Human {
     Human(char ch) { DOT = ch; }
 
     void turn(int x, int y, Field field, AI ai) {
-        if (field.isCellEmpty(x, y)) {
+        if (field.isCellValid(x, y)) {
             if (!field.isGameOver()) field.setDot(x, y, DOT);
             if (!field.isGameOver()) ai.turn(field);
         }
