82925c4f5242f33ef6c9af53bde06a00bca53878
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-22 09:50:47 +0300

    solved JavaCore task lvl 09 1917

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -4,9 +4,64 @@ package com.javarush.task.task19.task1917;
 Свой FileWriter
 */
 
-public class FileConsoleWriter {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class FileConsoleWriter {
+    private FileWriter fileWriter;
+
+    public FileConsoleWriter(String fileName) throws IOException {
+        fileWriter = new FileWriter(fileName);
     }
 
+    public FileConsoleWriter(String fileName, boolean append) throws IOException {
+        fileWriter = new FileWriter(fileName, append);
+    }
+
+    public FileConsoleWriter(File file) throws IOException {
+        fileWriter = new FileWriter(file);
+    }
+
+    public FileConsoleWriter(File file, boolean append) throws IOException {
+        fileWriter = new FileWriter(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd) {
+        fileWriter = new FileWriter(fd);
+    }
+
+    public void write(int c) throws IOException {
+        fileWriter.write(c);
+        System.out.print(c);
+    }
+
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        fileWriter.write(cbuf, off, len);
+        for (int i = off; i < off + len; i++)
+            System.out.print(cbuf[i]);
+    }
+
+    public void write(String str, int off, int len) throws IOException {
+        fileWriter.write(str, off, len);
+        System.out.print(str.substring(off, off + len));
+    }
+
+    public void write(char[] cbuf) throws IOException {
+        fileWriter.write(cbuf);
+        System.out.print(cbuf);
+    }
+
+    public void write(String str) throws IOException {
+        fileWriter.write(str);
+        System.out.print(str);
+    }
+
+    public void close() throws IOException {
+        fileWriter.close();
+    }
+
+    public static void main(String[] args) {
+    }
 }
