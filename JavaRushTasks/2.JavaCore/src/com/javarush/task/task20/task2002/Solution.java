97a90eb8ec71f777b3819007ab776230fbc5ee4c
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-25 12:25:33 +0300

    solved JavaCore task lvl 10 2002

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task20.task2002;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /* 
@@ -17,13 +18,29 @@ public class Solution {
             InputStream inputStream = new FileInputStream(your_file_name);
 
             JavaRush javaRush = new JavaRush();
-            //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
+            //initialize users field for the javaRush object here
+            User user = new User();
+            user.setFirstName("First Name");
+            user.setLastName("Last Name");
+            user.setBirthDate(new Date());
+            user.setMale(true);
+            user.setCountry(User.Country.RUSSIA);
+            javaRush.users.add(user);
+
             javaRush.save(outputStream);
             outputStream.flush();
 
             JavaRush loadedObject = new JavaRush();
             loadedObject.load(inputStream);
-            //check here that javaRush object equals to loadedObject object - проверьте тут, что javaRush и loadedObject равны
+            //check here that javaRush object equals to loadedObject object
+            System.out.println(javaRush.equals(loadedObject));
+            for (User usr : loadedObject.users) {
+                System.out.println(usr.getFirstName());
+                System.out.println(usr.getLastName());
+                System.out.println(usr.getBirthDate());
+                System.out.println(usr.isMale());
+                System.out.println(usr.getCountry());
+            }
 
             outputStream.close();
             inputStream.close();
@@ -41,11 +58,36 @@ public class Solution {
         public List<User> users = new ArrayList<>();
 
         public void save(OutputStream outputStream) throws Exception {
-            //implement this method - реализуйте этот метод
+            PrintStream writer = new PrintStream(outputStream);
+            for (User user : users) {
+                writer.println(user.getFirstName());
+                writer.println(user.getLastName());
+                writer.println(user.getBirthDate().getTime());
+                writer.println(user.isMale());
+                writer.println(user.getCountry());
+            }
+            writer.flush();
         }
 
         public void load(InputStream inputStream) throws Exception {
-            //implement this method - реализуйте этот метод
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            while (reader.ready()) {
+                String firstName = reader.readLine();
+                String lastName = reader.readLine();
+                String birthDate = reader.readLine();
+                String male = reader.readLine();
+                String country = reader.readLine();
+                User user = new User();
+                user.setFirstName(firstName);
+                user.setLastName(lastName);
+                Date dt = new Date();
+                dt.setTime(Long.parseLong(birthDate));
+                user.setBirthDate(dt);
+                user.setMale(male.equals("true"));
+                user.setCountry(User.Country.valueOf(country));
+                users.add(user);
+            }
+            reader.close();
         }
 
         @Override
