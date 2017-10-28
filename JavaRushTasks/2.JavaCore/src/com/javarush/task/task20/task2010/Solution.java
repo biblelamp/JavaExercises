0d4b92d7763b344e67509b6518dd646e3e432460
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-28 16:32:26 +0300

    solved JavaCore task lvl 10 2010

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2010/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2010/Solution.java
@@ -1,17 +1,19 @@
 package com.javarush.task.task20.task2010;
 
-/* 
+import java.io.Serializable;
+
+/*
 Как сериализовать что-то свое?
 */
 public class Solution {
-    public static class Object {
+    public static class Object implements Serializable {
         public String string1;
         public String string2;
     }
 
     public static int countStrings;
 
-    public static class String {
+    public static class String implements Serializable {
         private final int number;
 
         public String() {
@@ -24,6 +26,5 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-
     }
 }
