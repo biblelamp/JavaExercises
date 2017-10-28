1ad24815a0c893a9dc06242c6635766ca39e8a08
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-28 14:29:25 +0300

    sample of hw 1 for students

--- Java 2/HW1Lesson.java
+++ Java 2/HW1Lesson.java
@@ -20,7 +20,9 @@
  *    > java HW1Lesson
  *
  * @author Sergey Iryupin
- * @version 0.3.2 dated Sep 02, 2017
+ * @version 0.3.3 dated Oct 28, 2017
+ * @task 1
+ * @mark
  */
 import hw1.animals.*;
 import hw1.obstances.*;
@@ -29,18 +31,18 @@ import hw1.*;
 class HW1Lesson {
 
     public static void main(String[] args) {
-        Course c = new Course(new Doable[] { // creating an obstacle course
+        Course cource = new Course(new Doable[] { // creating an obstacle course
             new Track(50),
             new Water(50),
             new Wall(2)});
-        Team t1 = new Team("Animals", new Animal[] { // creating a team
+        Team team = new Team("Animals", new Animal[] { // creating a team
             new Cat("Murzik"),
             new Cat("Barsik"),
             new Hen("Izzy"),
             new Hippo("Hippopo")});
 
-        System.out.println(t1); // to show team list
-        c.doIt(t1);             // ask the team to pass the obstacle course
-        t1.showResults();       // to show results
+        System.out.println(team); // to show team list
+        cource.doIt(team);        // ask the team to pass the obstacle course
+        team.showResults();       // to show results
     }
 }
