1ec6d1cbc2b75e6d179bae5bb8c19059733b4482
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-28 16:27:50 +0300

    solved JavaCore task lvl 10 2009

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2009/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2009/Solution.java
@@ -1,16 +1,17 @@
 package com.javarush.task.task20.task2009;
 
-/* 
+import java.io.Serializable;
+
+/*
 Как сериализовать static?
 */
 public class Solution {
-    public static class ClassWithStatic {
+    public static class ClassWithStatic implements Serializable {
         public static String staticString = "it's test static string";
         public int i;
         public int j;
     }
 
     public static void main(String[] args) {
-
     }
 }
