0150d276f1d9dca3dd0c07059e236c18f352da5f
Sergey Iryupin <biblelamp@yandex.ru>
2017-10-29 22:03:19 +0300

    solved JavaCore task lvl 10 2011

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
@@ -1,12 +1,16 @@
 package com.javarush.task.task20.task2011;
 
-/* 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+/*
 Externalizable для апартаментов
 */
 public class Solution {
 
-    public static class Apartment {
-
+    public static class Apartment implements Externalizable {
         private String address;
         private int year;
 
@@ -26,9 +30,22 @@ public class Solution {
         public String toString() {
             return("Address: " + address + "\n" + "Year: " + year);
         }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(this.address);
+            out.writeInt(this.year);
+            out.close();
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            address = (String)in.readObject();
+            year = in.readInt();
+            in.close();
+        }
     }
 
     public static void main(String[] args) {
-
     }
 }
