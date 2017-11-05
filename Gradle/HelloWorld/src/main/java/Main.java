02f45469018c5e5fcb2467224659f9e5e7dc7c38
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-05 22:03:22 +0300

    simple code for testing gradle

---
+++ Gradle/HelloWorld/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static String doSomething() {
+        return "Hello World!";
+    }
+}
---
+++ Gradle/HelloWorld/src/test/java/MainTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import org.junit.Assert;
+ 
+public class MainTest {
+    @Test
+    public void doTest() {
+        Assert.assertEquals("Hello World!", Main.doSomething());
+    }
+}
