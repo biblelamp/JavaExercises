7ac909ccd627581d44bc0031590d606a18aa3272
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-06 14:37:53 +0300

    changed java code a bit

--- Gradle/HelloWorld/src/main/java/Main.java
+++ Gradle/HelloWorld/src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static String doSomething() {
+
+    public static void main(String[] args) {
+        System.out.println(getHello());
+    }
+
+    public static String getHello() {
         return "Hello World!";
     }
 }
