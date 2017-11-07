f4c837dc4da68fae3994ad5e53474ef68899a600
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-07 16:41:00 +0300

    solved JavaCore task lvl 10 2022

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
@@ -6,10 +6,12 @@ import java.io.*;
 Переопределение сериализации в потоке
 */
 public class Solution implements Serializable, AutoCloseable {
-    private FileOutputStream stream;
+    transient private FileOutputStream stream;
+    private String fileName;
 
     public Solution(String fileName) throws FileNotFoundException {
         this.stream = new FileOutputStream(fileName);
+        this.fileName = fileName;
     }
 
     public void writeObject(String string) throws IOException {
@@ -20,12 +22,13 @@ public class Solution implements Serializable, AutoCloseable {
 
     private void writeObject(ObjectOutputStream out) throws IOException {
         out.defaultWriteObject();
-        out.close();
+        //out.close();
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
-        in.close();
+        stream = new FileOutputStream(fileName, true);
+        //in.close();
     }
 
     @Override
@@ -34,7 +37,15 @@ public class Solution implements Serializable, AutoCloseable {
         stream.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Solution solution = new Solution("hello.txt");
+        solution.writeObject("Hello!\n");
+        ObjectOutputStream oos = new ObjectOutputStream(
+                new FileOutputStream("solution.obj"));
+        oos.writeObject(solution);
 
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("solution.obj"));
+        Solution solution2 = (Solution) ois.readObject();
+        solution2.writeObject("Hello again!");
     }
 }
