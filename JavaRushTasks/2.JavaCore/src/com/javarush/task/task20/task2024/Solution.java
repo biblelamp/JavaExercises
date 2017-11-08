d6874b8bf7d07743a29ca0143598d8e88a9dd416
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-08 15:27:54 +0300

    solved JavaCore task lvl 10 2024

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2024/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2024/Solution.java
@@ -1,16 +1,16 @@
 package com.javarush.task.task20.task2024;
 
+import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 
 /* 
 Знакомство с графами
 */
-public class Solution {
+public class Solution implements Serializable {
     int node;
     List<Solution> edges = new LinkedList<>();
 
     public static void main(String[] args) {
-
     }
 }
