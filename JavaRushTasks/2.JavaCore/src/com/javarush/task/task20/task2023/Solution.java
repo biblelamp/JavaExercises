a7e16f3fc73765213d353c937f8da803f05872c6
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-08 15:24:59 +0300

    solved JavaCore task lvl 10 2023

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2023/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2023/Solution.java
@@ -10,17 +10,19 @@ public class Solution {
     }
 
     public static class A {
-        public void method1() {
+        private void method1() {
             System.out.println("A class, method1");
         }
 
         public void method2() {
             System.out.println("A class, method2");
+            method1();
         }
     }
 
     public static class B extends A {
         public void method1() {
+            super.method2();
             System.out.println("B class, method1");
         }
 
@@ -36,6 +38,7 @@ public class Solution {
 
         public void method2() {
             System.out.println("C class, method2");
+            super.method1();
         }
     }
 }
