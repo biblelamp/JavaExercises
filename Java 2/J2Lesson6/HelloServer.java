776d3e0fef39511a0e712ff0e7785d9c9d2ccdb1
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-10 18:02:41 +0300

    simple HelloServer for 6 lesson Java 2

--- Java 2/J2Lesson6/HelloServer.java
+++ Java 2/J2Lesson6/HelloServer.java
@@ -1,3 +1,10 @@
+/**
+ * Java. Level 2. Lesson 6. Networking
+ * Class HelloServer: send message to client
+ *
+ * @author Sergey Iryupin
+ * @version dated Nov 10, 2017
+ */
 import java.net.*;
 import java.io.*;
 
@@ -9,13 +16,13 @@ class HelloServer {
 
     HelloServer() {
         try {
-            ServerSocket server = new ServerSocket(1024);
+            ServerSocket server = new ServerSocket(1024); // more than 1023
             System.out.println("Server started.");
             while (true) {
                 Socket socket = server.accept();
-                System.out.println("Client connected.");
                 PrintWriter writer = new PrintWriter(socket.getOutputStream());
                 writer.println("Server says: Hello");
+                System.out.println("Message was sent to the client.");
                 writer.close();
             }
         } catch (IOException ex) {
