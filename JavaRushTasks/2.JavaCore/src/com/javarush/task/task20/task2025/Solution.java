09febacde83ee5ea36e1edce949778dea68c3bf6
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-10 14:39:41 +0300

    solved JavaCore task lvl 10 2025

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -4,27 +4,32 @@ import java.util.Arrays;
 
 /*
 Алгоритмы-числа
+Поиск самовлюблённых чисел или чисел Армстронга
+@see https://github.com/shamily/ArmstrongNumbers
 */
 public class Solution {
     public static long[] getNumbers(long N) {
         long[] result = null;
-        long[] armstrNum = {
-                1, 2, 3, 4, 5, 6, 7, 8, 9,
-                153, 370, 371, 407, 1634, 8208, 9474, 54748, 92727, 93084,
-                548834, 1741725, 4210818, 9800817, 9926315, 24678050, 24678051,
-                88593477, 146511208, 472335975, 534494836, 912985153, 4679307774L
+        long[] armstrongNum = {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407, 1634, 8208, 9474,
+                54748, 92727, 93084, 548834, 1741725, 4210818, 9800817, 9926315,
+                24678050, 24678051, 88593477, 146511208, 472335975, 534494836,
+                912985153, 4679307774L, 32164049650L, 32164049651L, 40028394225L,
+                42678290603L, 44708635679L, 49388550606L, 82693916578L,
+                94204591914L, 28116440335967L, 4338281769391370L,
+                4338281769391371L, 21897142587612075L, 35641594208964132L,
+                35875699062250035L, 1517841543307505039L, 3289582984443187032L,
+                4498128791164624869L, 4929273885928088826L
         };
         int idx = 0;
-        while (armstrNum[idx] < N) idx++;
-        if (idx > 0) {
-            result = new long[idx];
-            for (int i = 0; i < result.length; i++)
-                result[i] = armstrNum[i];
-        }
+        while (idx < armstrongNum.length && armstrongNum[idx] < N)
+            idx++;
+        result = new long[idx];
+        System.arraycopy(armstrongNum, 0, result, 0, idx);
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(getNumbers(8208)));
+        //System.out.println(Arrays.toString(getNumbers(1)));
     }
 }
