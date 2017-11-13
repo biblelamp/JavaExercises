e9cbcf0f0fc8a0b173ce7d8d3d5a9072338c24a3
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-13 14:46:28 +0300

    sample of extended hw 6 for students

--- Java 2/J2Lesson6/AttackHelloSrv.java
+++ Java 2/J2Lesson6/AttackHelloSrv.java
@@ -1,3 +1,12 @@
+/**
+ * Java. Level 2. Lesson 6. Example of homework
+ * Extended task: write code attacking Hello server
+ * Note: we must see in console this message
+ *  java.net.BindException: Address already in use: connect
+ *
+ * @author Sergey Iryupin
+ * @version 0.1 dated Nov 13, 2017
+ */
 import java.util.*;
 
 class AttackHelloSrv {
@@ -5,7 +14,7 @@ class AttackHelloSrv {
     public static void main(String[] args) throws InterruptedException {
         List<AttackThread> list = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 6; i++)
             list.add(new AttackThread());
 
         for (AttackThread thread : list)
