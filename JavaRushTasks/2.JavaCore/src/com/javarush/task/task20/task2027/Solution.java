481b0011920e3931a05320b37b9007a6ec39e8e4
Sergey Iryupin <biblelamp@yandex.ru>
2017-11-15 11:26:55 +0300

    solved JavaCore task lvl 10 2027

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task20.task2027;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /* 
 Кроссворд
@@ -14,7 +15,8 @@ public class Solution {
                 {'m', 'l', 'p', 'r', 'r', 'h'},
                 {'p', 'o', 'e', 'e', 'j', 'j'}
         };
-        detectAllWords(crossword, "home", "same");
+        for (Word word : detectAllWords(crossword, "home", "same"))
+            System.out.println(word);
         /*
 Ожидаемый результат
 home - (5, 3) - (2, 0)
@@ -23,8 +25,43 @@ same - (1, 1) - (4, 1)
     }
 
     public static List<Word> detectAllWords(int[][] crossword, String... words) {
+        int[] endXY;
+        List<Word> list = new ArrayList<>();
+        for (int y = 0; y < crossword.length; y++)
+            for (int x = 0; x < crossword[y].length; x++)
+                for (String wd : words)
+                    if ((char)crossword[y][x] == wd.charAt(0))
+                        if ((endXY = getEndXY(crossword, wd, x, y)) != null) {
+                            Word word = new Word(wd);
+                            word.setStartPoint(x, y);
+                            word.setEndPoint(endXY[0], endXY[1]);
+                            list.add(word);
+                        }
+        return list;
+    }
 
+    private static int[] getEndXY(int[][] crossword, String word, int x, int y) {
+        for (int dy = -1; dy < 2; dy++)
+            for (int dx = -1; dx < 2; dx++)
+                if (getWord(crossword, x, y, dx, dy, word.length()).equals(word))
+                    return (new int[]
+                        {x + dx*(word.length()-1), y + dy*(word.length()-1)});
         return null;
+    }
+
+    private static String getWord(int[][] crossword, int x, int y,
+            int dx, int dy, int len) {
+        String word = "";
+        for (int i = 0; i < len; i++, x += dx, y += dy)
+            word += getChar(crossword, x, y);
+        return word;
+    }
+
+    private static char getChar(int[][] crossword, int x, int y) {
+        if (y < 0 || x < 0 ||
+            y > crossword.length - 1 || x > crossword[y].length - 1)
+            return (char) 0;
+        return (char)crossword[y][x];
     }
 
     public static class Word {
