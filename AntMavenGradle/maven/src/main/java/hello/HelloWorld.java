2dc040d3e56a029228b7da9440ea9f0abca0dfa4
Sergey Iryupin <biblelamp@yandex.ru>
2017-12-08 16:55:19 +0300

    init files for maven

---
+++ AntMavenGradle/maven/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        System.out.println(hello.sayHello());
+    }
+
+    String sayHello() {
+        return "Hello, World!";
+    }
+}
---
+++ AntMavenGradle/maven/src/test/java/hello/TestHello.java
@@ -0,0 +1,13 @@
+package hello;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestHello {
+
+    @Test
+    public void testHello() {
+        HelloWorld hello = new HelloWorld();
+        assertEquals("Hello, World!", hello.sayHello());
+    }
+}
