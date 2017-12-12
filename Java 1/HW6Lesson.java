572928a48de7c75920f8a868e176c1d20083fe86
Sergey Iryupin <biblelamp@yandex.ru>
2017-12-12 09:45:22 +0300

    sample of hw 6 for students

--- Java 1/HW6Lesson.java
+++ Java 1/HW6Lesson.java
@@ -2,7 +2,7 @@
  * Java. Level 1. Lesson 6. Example of homework
  *
  * @author Sergey Iryupin
- * @version dated May 26, 2017
+ * @version dated Dec 12, 2017
  */
 class HW6Lesson {
 
@@ -17,41 +17,45 @@ class HW6Lesson {
     }
 }
 
-class Cat extends Animal {
+class Cat extends AnimalImpl {
     Cat(int run_limit, float jump_limit, int swim_limit) {
         super(run_limit, jump_limit, swim_limit);
     }
-
     @Override
-    protected boolean swim(int distance) {
+    public boolean swim(int distance) {
         return false; // cats cannot swim (by the condition of task)
     }
 }
 
-class Dog extends Animal {
+class Dog extends AnimalImpl {
     Dog(int run_limit, float jump_limit, int swim_limit) {
         super(run_limit, jump_limit, swim_limit);
     }
 }
 
-abstract class Animal {
+interface Animal {
+    boolean run(int distance);
+    boolean jump(float height);
+    boolean swim(int distance);
+}
+
+abstract class AnimalImpl implements Animal {
     private int run_limit;
     private float jump_limit;
     private int swim_limit;
 
-    Animal(int run_limit, float jump_limit, int swim_limit) {
+    AnimalImpl(int run_limit, float jump_limit, int swim_limit) {
         this.run_limit = run_limit;
         this.jump_limit = jump_limit;
         this.swim_limit = swim_limit;
     }
-
-    protected boolean run(int distance) {
+    public boolean run(int distance) {
         return distance <= run_limit;
     }
-    protected boolean jump(float height)  {
+    public boolean jump(float height)  {
         return height <= jump_limit;
     }
-    protected boolean swim(int distance) {
+    public boolean swim(int distance) {
         return distance <= swim_limit;
     }
 }
