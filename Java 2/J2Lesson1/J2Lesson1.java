3b4f69ed43e75ec2b852a44cb38ca705859d0a01
Sergey Iryupin <biblelamp@yandex.ru>
2017-12-21 11:51:00 +0300

    renamed main class

---
+++ Java 2/J2Lesson1/J2Lesson1.java
@@ -0,0 +1,25 @@
+import animals.*;
+import obstacles.*;
+/**
+ * Write a description of class J2Lesson1 here.
+ *
+ * @author Sergey Iryupin
+ * @version dated Dec 19, 2017
+ * @link https://github.com/
+ */
+public class J2Lesson1 {
+
+    public static void main(String[] args) {
+        Animal[] zoo =
+            {new Cat("Murzik"), new Hen("Izzy"), new Hippo("Hippopo")};
+        Track track = new Track(80);
+        Wall wall = new Wall(3);
+        Water water = new Water(10);
+
+        for (Animal animal : zoo)
+            System.out.println(animal + " say: " + animal.voice() +
+                "\n run: " + track.doIt(animal) +
+                "\n jump: " + wall.doIt(animal) +
+                "\n swim: " + water.doIt(animal));
+    }
+}
