ed6e1c904b0721346f935869c0cfbf51789319d9
Sergey Iryupin <biblelamp@yandex.ru>
2017-12-22 12:15:27 +0300

    added interface runable

--- Java 2/hw1/animals/Animal.java
+++ Java 2/hw1/animals/Animal.java
@@ -3,9 +3,9 @@ package hw1.animals;
  * Class Animal is parent for all animals
  *
  * @author  Sergey Iryupin
- * @version 0.3 dated Mar 24, 2017
+ * @version 0.3.1 dated Dec 21, 2017
  */
-public abstract class Animal {
+public abstract class Animal implements Runable {
     protected String name;
     protected int run_limit;
 
---
+++ Java 2/hw1/animals/Runable.java
@@ -0,0 +1,10 @@
+package hw1.animals;
+/**
+ * Interface Runable
+ * 
+ * @author  Sergey Iryupin
+ * @version 0.1 dated Dec 21, 2017
+ */
+public interface Runable {
+    boolean run(int length);
+}
