7f52ae79ec5a8407be17c825b659fc0310690c61
Sergey Iryupin <biblelamp@yandex.ru>
2017-12-29 15:06:14 +0300

    deleted old files

--- Java 2/animals/Jumpable.java
+++
@@ -1,9 +0,0 @@
-/**
- * Interface Jumpable
- * 
- * @author  Sergey Iryupin
- * @version 0.1 dated Mar 19, 2017
- */
-interface Jumpable {
-    boolean jump(float height);
-}
--- Java 2/animals/Swimable.java
+++
@@ -1,9 +0,0 @@
-/**
- * Interface Swimable
- * 
- * @author  Sergey Iryupin
- * @version 0.1 dated Mar 18, 2017
- */
-interface Swimable {
-    boolean swim(int length);
-}
