332475994354c7f5d64e6542d80db5530c0b4a5d
Sergey Iryupin <biblelamp@yandex.ru>
2018-01-11 10:02:03 +0300

    hello client for lesson 6

--- Java 2/J2Lesson6/HelloClient.java
+++ Java 2/J2Lesson6/HelloClient.java
@@ -1,9 +1,9 @@
 /**
  * Java. Level 2. Lesson 6. Networking
- * Class HelloClient: connect and ge hello from server
+ * Class HelloClient: connect and get hello from server
  *
  * @author Sergey Iryupin
- * @version dated Nov 21, 2017
+ * @version dated Jan 11, 2018
  */
 import java.net.*;
 import java.io.*;
@@ -15,11 +15,14 @@ class HelloClient {
     }
 
     HelloClient() {
-        try (Socket socket = new Socket("127.0.0.1", 1024)) {
-            BufferedReader reader = new BufferedReader(
-                new InputStreamReader(socket.getInputStream()));
-            System.out.println(reader.readLine());
-            reader.close();
+        try (Socket socket = new Socket("127.0.0.1", 1024);
+            BufferedReader reader =
+                new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()))) {
+
+            System.out.println(
+                reader.readLine());
+
         } catch (IOException ex) {
             System.out.println(ex);
         }
