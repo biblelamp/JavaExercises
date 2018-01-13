314b8cbf28cd2ed75a2c938f4082f8071d1a3429
Sergey Iryupin <biblelamp@yandex.ru>
2018-01-13 13:45:28 +0300

    sample of hw 5 for students

--- Java 2/HW5Lesson.java
+++ Java 2/HW5Lesson.java
@@ -2,11 +2,11 @@
  * Java. Level 2. Lesson 5. Example of homework
  *
  * @author Sergey Iryupin
- * @version 0.2.1 dated Nov 11, 2017
+ * @version 0.2.2 dated Jan 13, 2018
  */
 import java.util.Arrays;
 
-class HW5Lesson implements Runnable {
+class HW5Lesson {
 
     final int SIZE = 10000000;
     final int HALF_SIZE = SIZE / 2;
@@ -21,44 +21,58 @@ class HW5Lesson implements Runnable {
         hw.doWithThreads();
     }
 
-    void doWithoutThreads() { // without threads
-        Arrays.fill(arr, 1);
-        long a = System.currentTimeMillis(); // note the time
+    void doWithoutThreads() {
+        Arrays.fill(arr, 1); // filling the array
+        long a = System.currentTimeMillis();
         for (int i = 0; i < SIZE; i++)
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) *
-                Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        System.out.println("Way 1 took " + (System.currentTimeMillis() - a) + " mc");
-        testA65 = arr[HALF_SIZE + 65];
+            arr[i] = (float)(arr[i] * 
+                Math.sin(0.2f + i / 5) *
+                Math.cos(0.2f + i / 5) *
+                Math.cos(0.4f + i / 2));
+        System.out.println("Way 1 took " + (System.currentTimeMillis() - a));
+        testA65 = arr[HALF_SIZE + 65]; // getting control value
     }
 
-    void doWithThreads() { // using two threads
+    void doWithThreads() {
         Arrays.fill(arr, 1);
-        long a = System.currentTimeMillis(); // note the time
-        System.arraycopy(arr, 0, a1, 0, HALF_SIZE); // split array into two parts
+        long a = System.currentTimeMillis();
+        System.arraycopy(arr, 0, a1, 0, HALF_SIZE);
         System.arraycopy(arr, HALF_SIZE, a2, 0, HALF_SIZE);
-        Thread one = new Thread(this, "one"); // create and
-        Thread two = new Thread(this, "two"); // run two threads
+        Thread one = new Thread(new CalcArray(a1, 0));
+        Thread two = new Thread(new CalcArray(a2, HALF_SIZE));
         one.start();
         two.start();
         try {
             one.join();
             two.join();
-        } catch (InterruptedException ex) { } // waiting
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
         System.arraycopy(a1, 0, arr, 0, HALF_SIZE);
         System.arraycopy(a2, 0, arr, HALF_SIZE, HALF_SIZE);
-        System.out.println("Way 2 took " + (System.currentTimeMillis() - a) + " mc");
-        System.out.println(testA65 == arr[HALF_SIZE + 65]);
+        System.out.println("Way 2 took " + (System.currentTimeMillis() - a));
+        System.out.println(
+            testA65 == arr[HALF_SIZE + 65]); // checking control value
+    }
+}
+
+class CalcArray implements Runnable {
+    private float[] array;
+    private int shift;
+
+    CalcArray(float[] array, int shift) {
+        this.array = array;
+        this.shift = shift;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < HALF_SIZE; i++)
-            if (Thread.currentThread().getName() == "one")
-                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) *
-                    Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-            else
-                a2[i] = (float)(a2[i] * Math.sin(0.2f + (i + HALF_SIZE) / 5) *
-                    Math.cos(0.2f + (i + HALF_SIZE) / 5) *
-                    Math.cos(0.4f + (i + HALF_SIZE) / 2));
+        for (int i = 0; i < array.length; i++) {
+            int idx = i + shift;
+            array[i] = (float)(array[i] * 
+                Math.sin(0.2f + idx / 5) *
+                Math.cos(0.2f + idx / 5) *
+                Math.cos(0.4f + idx / 2));
+        }
     }
 }
