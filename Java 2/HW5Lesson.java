1c3c9d04b90f20e2f4daec2ed01a1d3c47243573
Sergey Iryupin <biblelamp@yandex.ru>
2018-01-14 16:38:17 +0300

    sample of hw 5 for students

--- Java 2/HW5Lesson.java
+++ Java 2/HW5Lesson.java
@@ -2,7 +2,7 @@
  * Java. Level 2. Lesson 5. Example of homework
  *
  * @author Sergey Iryupin
- * @version 0.2.2 dated Jan 13, 2018
+ * @version 0.2.3 dated Jan 14, 2018
  */
 import java.util.Arrays;
 
@@ -10,9 +10,6 @@ class HW5Lesson {
 
     final int SIZE = 10000000;
     final int HALF_SIZE = SIZE / 2;
-    float[] arr = new float[SIZE];
-    float[] a1 = new float[HALF_SIZE];
-    float[] a2 = new float[HALF_SIZE];
     float testA65;
 
     public static void main(String[] args) {
@@ -22,6 +19,8 @@ class HW5Lesson {
     }
 
     void doWithoutThreads() {
+        float[] arr = new float[SIZE];
+
         Arrays.fill(arr, 1); // filling the array
         long a = System.currentTimeMillis();
         for (int i = 0; i < SIZE; i++)
@@ -34,6 +33,10 @@ class HW5Lesson {
     }
 
     void doWithThreads() {
+        float[] arr = new float[SIZE];
+        float[] a1 = new float[HALF_SIZE];
+        float[] a2 = new float[HALF_SIZE];
+
         Arrays.fill(arr, 1);
         long a = System.currentTimeMillis();
         System.arraycopy(arr, 0, a1, 0, HALF_SIZE);
@@ -52,27 +55,25 @@ class HW5Lesson {
         System.arraycopy(a2, 0, arr, HALF_SIZE, HALF_SIZE);
         System.out.println("Way 2 took " + (System.currentTimeMillis() - a));
         System.out.println(
-            testA65 == arr[HALF_SIZE + 65]); // checking control value
-    }
-}
-
-class CalcArray implements Runnable {
-    private float[] array;
-    private int shift;
-
-    CalcArray(float[] array, int shift) {
-        this.array = array;
-        this.shift = shift;
+            Float.compare(testA65, arr[HALF_SIZE + 65]) == 0); // checking value
     }
 
-    @Override
-    public void run() {
-        for (int i = 0; i < array.length; i++) {
-            int idx = i + shift;
-            array[i] = (float)(array[i] * 
-                Math.sin(0.2f + idx / 5) *
-                Math.cos(0.2f + idx / 5) *
-                Math.cos(0.4f + idx / 2));
+    class CalcArray implements Runnable { // for threads
+        private float[] array;
+        private int shift;
+
+        CalcArray(float[] array, int shift) {
+            this.array = array;
+            this.shift = shift;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < array.length; i++)
+                array[i] = (float)(array[i] * 
+                    Math.sin(0.2f + (i + shift) / 5) *
+                    Math.cos(0.2f + (i + shift) / 5) *
+                    Math.cos(0.4f + (i + shift) / 2));
         }
     }
 }
