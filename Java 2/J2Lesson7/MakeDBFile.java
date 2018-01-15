03ccbcfc16fbea7498aef1325b5372723351ff00
Sergey Iryupin <biblelamp@yandex.ru>
2018-01-15 12:01:22 +0300

    making db file for lesson 7 ver 0.3

--- Java 2/J2Lesson7/MakeDBFile.java
+++ Java 2/J2Lesson7/MakeDBFile.java
@@ -1,62 +1,62 @@
 /**
  * Java. Level 2. Lesson 7
- * Making db file with users
+ * Making SQLite db file with users
  *
  * @author Sergey Iryupin
- * @version 0.2 dated Jul 05, 2017
+ * @version 0.3 dated Jan 15, 2018
  */
 import java.sql.*;
 
 class MakeDBFile implements IConstants {
 
     final String NAME_TABLE = "users";
-    final String SQL_CREATE_TABLE = "CREATE TABLE " + NAME_TABLE +
+    final String SQL_CREATE_TABLE =
+        "DROP TABLE IF EXISTS " + NAME_TABLE + ";" +
+        "CREATE TABLE " + NAME_TABLE +
         "(login  CHAR(6) PRIMARY KEY NOT NULL," +
         " passwd CHAR(6) NOT NULL);";
-    final String SQL_INSERT_MIKE = "INSERT INTO " + NAME_TABLE +
+    final String SQL_INSERT_MIKE =
+        "INSERT INTO " + NAME_TABLE +
         " (login, passwd) " +
-        "VALUES ('mike', 'qwerty');";
-    final String SQL_INSERT_JONH = "INSERT INTO " + NAME_TABLE +
+        "VALUES ('mike', 'qwe');";
+    final String SQL_INSERT_JONH =
+        "INSERT INTO " + NAME_TABLE +
         " (login, passwd) " +
-        "VALUES ('john', '12345');";
+        "VALUES ('john', 'rty');";
     final String SQL_SELECT = "SELECT * FROM " + NAME_TABLE + ";";
-
-    Connection connect;
-    Statement stmt;
-    ResultSet rs;
-    String sql;
 
     public static void main(String[] args) {
         new MakeDBFile();
     }
 
     MakeDBFile() {
-        // open db file
         try {
+            // loads a class, including running its static initializers
             Class.forName(DRIVER_NAME);
-            connect = DriverManager.getConnection(SQLITE_DB);
-        } catch (Exception e) { }
+            // attempts to establish a connection to the given database URL
+            Connection connect = DriverManager.getConnection(SQLITE_DB);
+            // сreates an object for sending SQL statements to the database
+            Statement stmt = connect.createStatement();
 
-        // create table
-        try {
-            stmt = connect.createStatement();
+            // create table
             stmt.executeUpdate(SQL_CREATE_TABLE);
-        } catch (Exception e) { }
 
-        // insert record(s)
-        try {
+            // insert record(s)
             stmt.executeUpdate(SQL_INSERT_MIKE);
             stmt.executeUpdate(SQL_INSERT_JONH);
-        } catch (Exception e) { }
 
-        // print records
-        try {
-            rs = stmt.executeQuery(SQL_SELECT);
+            // print records
+            ResultSet rs = stmt.executeQuery(SQL_SELECT);
             System.out.println("LOGIN\tPASSWD");
-            while (rs.next()) {
-                System.out.println(rs.getString("login") + "\t" +
+            while (rs.next())
+                System.out.println(
+                    rs.getString("login") + "\t" +
                     rs.getString(PASSWD_COL));
-            }
-        } catch (Exception e) { }
+            rs.close();
+            stmt.close();
+            connect.close();
+        } catch (ClassNotFoundException | SQLException ex) {
+                ex.printStackTrace();
+        }
     }
 }
