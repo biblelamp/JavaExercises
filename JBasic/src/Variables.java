0494b5b67525a5dd2976b7e06288a525c7ac8c5f
Sergey Iryupin <biblelamp@yandex.ru>
2018-01-25 11:46:59 +0300

    simple work with variables

--- JBasic/src/Variables.java
+++ JBasic/src/Variables.java
@@ -8,9 +8,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Variables {
-    Map<Integer, String> variables;
+    Map<String, Float> variables;
 
     Variables() {
         variables = new HashMap<>();
+    }
+
+    void put(String name, String value) {
+        try {
+            variables.put(name, Float.parseFloat(value));
+        } catch (NumberFormatException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    float get(String name) {
+        try {
+            return variables.get(name);
+        } catch (NullPointerException ex) {
+            return 0;
+        }
+    }
+
+    void print() {
+        System.out.println(variables);
     }
 }
