d962b5443a8f974f10e0dda939382782960d68b3
Sergey Iryupin <biblelamp@yandex.ru>
2018-02-13 10:06:57 +0300

    sample of hw 5 for students

--- Java 1/HW5Lesson.java
+++ Java 1/HW5Lesson.java
@@ -2,27 +2,64 @@
  * Java. Level 1. Lesson 5. Example of homework
  *
  * @author Sergey Iryupin
- * @version dated May 23, 2017
+ * @version dated Feb 13, 2018
+ * @link https://github.com/<your_nik> || null
  */
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+
 class HW5Lesson {
 
     public static void main(String[] args) {
-        // array of staff objects
-        Employee[] arrEmployee = new Employee[5];
-        arrEmployee[0] = new Employee("Ivanov Ivan", "Engineer",
+        // define and fill array of employees
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Ivanov Ivan", "Engineer",
             "iivanov@mail.com", "2312312", 30000, 30);
-        arrEmployee[1] = new Employee("Petrov Petr", "Lead Engineer",
+        employees[1] = new Employee("Petrov Petr", "Lead Engineer",
             "ppenrov@mail.com", "2365113", 42000, 39);
-        arrEmployee[2] = new Employee("Vasilev Vasil", "Head of Department",
+        employees[2] = new Employee("Vasilev Vasil", "Head of Department",
             "vvasilev@mail.com", "2365001", 55000, 55);
-        arrEmployee[3] = new Employee("Sidorov Sidor", "Assistant",
+        employees[3] = new Employee("Sidorov Sidor", "Assistant",
             "ssidorov@mail.com", "2365223", 25000, 42);
-        arrEmployee[4] = new Employee("Sergeev Sergey", "Trainee",
+        employees[4] = new Employee("Sergeev Sergey", "Trainee",
             "ssergeev@mail.com", "2365113", 15000, 25);
-        // view all employees looking for over 40 year
-        for (Employee employee : arrEmployee)
+
+        // show all employees older than 40 year
+        for (Employee employee : employees)
             if (employee.getAge() > 40)
                 System.out.println(employee);
+
+        // write array to the CSV file
+        try (FileWriter file = new FileWriter("employee.txt")) {
+            for (Employee employee : employees)
+                file.write(employee.getDataInCSV() + "\n");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        // read all lines from file
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(Paths.get("employee.txt"),
+                StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        // parsing CSV lines
+        int index = 0;
+        employees = new Employee[lines.size()];
+        for(String line: lines){
+            String[] field = line.split(",");
+            employees[index] = new Employee(
+                field[0], field[1], field[2], field[3],
+                Integer.parseInt(field[4]), Integer.parseInt(field[5]));
+        }
     }
 }
 
@@ -37,7 +74,7 @@ class Employee {
     private int salary;
     private int age;
 
-    Employee(String name, String position, String email, String phone,
+    public Employee(String name, String position, String email, String phone,
             int salary, int age) {
         this.name = name;
         this.position = position;
@@ -47,8 +84,13 @@ class Employee {
         this.age = age;
     }
 
-    int getAge() { // getter for age
+    public int getAge() { // getter for age
         return age;
+    }
+
+    public String getDataInCSV() {
+        return name + "," + position + "," + email + "," + phone + "," +
+            salary + "," + age;
     }
 
     @Override
