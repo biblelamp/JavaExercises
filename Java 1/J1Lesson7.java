ae2bab0c5e11db925113ac463d0d148f0977d49d
Sergey Iryupin <biblelamp@yandex.ru>
2018-02-17 13:35:05 +0300

    java 1 code for lesson 7

--- Java 1/J1Lesson7.java
+++ Java 1/J1Lesson7.java
@@ -2,23 +2,23 @@
  * Java. Level 1. Lesson 7
  *
  * @author Sergey Iryupin
- * @version dated Oct 17, 2017
+ * @version dated Feb 17, 2018
  */
-import java.io.*; 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 class J1Lesson7 {
-
     public static void main(String[] args) {
-        Cat cat = new Cat("Barsik", 5);
-        Plate plate = new Plate(50);
-        System.out.println(plate);
-        cat.eat(plate);
-        System.out.println(plate);
-
         //Test.testString(120000);
         //Test.testStringBuilder(120000);
         //Test.testBufferedReader("E:\\Audacity\\audacity.exe");
         //Test.testFileReader("E:\\Audacity\\audacity.exe");
+        Cat cat = new Cat("Barsik", 10);
+        Plate plate = new Plate(5);
+        System.out.println(plate);
+        cat.eat(plate);
+        System.out.println(plate);
     }
 }
 
@@ -32,7 +32,7 @@ class Cat {
     }
 
     void eat(Plate plate) {
-        plate.decreaseFood(appetite);
+        plate.dicreaseFood(appetite);
     }
 }
 
@@ -43,13 +43,13 @@ class Plate {
         this.food = food;
     }
 
-    void decreaseFood(int food) {
+    void dicreaseFood(int food) {
         this.food -= food;
     }
 
     @Override
     public String toString() {
-        return "Plate: " + food;
+        return "Food: " + food;
     }
 }
 
@@ -58,54 +58,44 @@ class Test {
      * Testing class String (immutable)
      */
     static void testString(int cycles) {
-        System.out.print("Testing String...");
+        System.out.print("Testing String... ");
         long t1 = System.currentTimeMillis();
-        long m1 = Runtime.getRuntime().freeMemory();
-        System.out.print(m1 + ":");
 
         String a = "";
         for (int i = 0; i < cycles; i++)
             a += "w";
 
         long t2 = System.currentTimeMillis();
-        long m2 = Runtime.getRuntime().freeMemory();
-        System.out.print(m2);
-        System.out.print("It took " + (t2 - t1) + " mc ");
-        System.out.println("and " + (m1 - m2)/1024 + " Kb");
+        System.out.println("It took " + (t2 - t1) + " mc");
     }
 
     /**
      * Testing class StringBuilder (mutable)
      */
     static void testStringBuilder(int cycles) {
-        System.out.print("Testing StringBuilder...");
+        System.out.print("Testing StringBuilder... ");
         long t1 = System.currentTimeMillis();
-        long m1 = Runtime.getRuntime().freeMemory();
 
         StringBuilder a = new StringBuilder("");
         for (int i = 0; i < cycles; i++)
             a.append("w");
 
         long t2 = System.currentTimeMillis();
-        long m2 = Runtime.getRuntime().freeMemory();
-        System.out.print("It took " + (t2 - t1) + " mc ");
-        System.out.println("and " + (m1 - m2)/1024 + " Kb");
+        System.out.println("It took " + (t2 - t1) + " mc");
     }
 
     /**
      * Testing class FileReader
      */
     static void testFileReader(String fileName) {
-        System.out.print("Testing FileReader...");
+        System.out.print("Testing FileReader... ");
         long t1 = System.currentTimeMillis();
 
-        try {
+        try (FileReader file = new FileReader(fileName)) {
             int x = -1;
-            FileReader file = new FileReader(fileName);
             do
                 x = file.read();
             while (x != -1);
-            file.close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -118,16 +108,15 @@ class Test {
      * Testing class BufferedReader
      */
     static void testBufferedReader(String fileName) {
-        System.out.print("Testing BufferedReader...");
+        System.out.print("Testing BufferedReader... ");
         long t1 = System.currentTimeMillis();
 
-        try {
+        try (BufferedReader file = new BufferedReader(
+                new FileReader(fileName))) {
             int x = -1;
-            BufferedReader file = new BufferedReader(new FileReader(fileName));
             do
                 x = file.read();
             while (x != -1);
-            file.close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
