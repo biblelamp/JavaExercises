34273504057b89b19c0ded71aed57c605903a462
Sergey Iryupin <biblelamp@yandex.ru>
2018-02-20 23:13:00 +0300

    semaphore demo for lesson 5

---
+++ Java 3/J3Lesson5/SemaphoreDemo.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.Semaphore;
+
+class SemaphoreDemo {
+    public static void main(String[] args) {
+        Semaphore smp = new Semaphore(2);
+        for (int i = 0; i < 5; i++) {
+            final int w = i;
+            new Thread(() -> {
+                try {
+                    System.out.println("Thread " + w + " before the semaphore");
+                    smp.acquire();
+                    System.out.println("Thread " + w + " accessed the resource");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("Thread " + w + " freed the resource");
+                    smp.release();
+                }
+            }).start();
+        }
+    }
+}
