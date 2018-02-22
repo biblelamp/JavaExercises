390c3824a8cfc812df8515a29a279c411e87ad4f
Sergey Iryupin <biblelamp@yandex.ru>
2018-02-22 15:11:33 +0300

    sample of hw 4 init

---
+++ Java 3/HW4Lesson.java
@@ -0,0 +1,22 @@
+/**
+ * Java. Level 3. Lesson 4. Homework
+ *
+ * 1. Create 3 threads, each of which prints the letter (A, B and C) 5 times,
+ *    the order should be exactly ABCABCABS. Use wait/notify/notifyAll
+ * 2. Write a method in which 3 threads write data to a file line by line
+ *    (for 10 records, with a period of 20 ms)
+ * 3. Write a MFD class, where you can print and scan documents at the same
+ *    time, but you can not print two documents or scan at the same time
+ *    (when printing, messages are output to the console "1, 2, 3, ... pages"
+ *    printed, when scanning the same, only "scanned ...", output
+ *    to the console with a period of 50 ms)
+ *
+ * @author Sergey Iryupin
+ * @version Feb 22, 2018
+ * @link https://github.com/biblelamp
+ */
+public class HW4Lesson {
+
+    public static void main(String[] args) {
+    }
+}
