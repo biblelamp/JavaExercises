7ce8ba6307e72cf5d07a69623c916eb6166bc999
Sergey Iryupin <biblelamp@yandex.ru>
2018-02-25 16:22:35 +0300

    CyclicBarrier demo

---
+++ Java 3/J3Lesson5/CyclicBarrierDemo.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier cb = new CyclicBarrier(3);
+        for (int i = 0; i < 3; i++) {
+            final int w = i;
+            new Thread(() -> {
+                try {
+                    System.out.println("Thread " + w + " is preparing");
+                    Thread.sleep(100 + (int) (3000 * Math.random()));
+                    System.out.println("Thread " + w + " is ready");
+                    cb.await();
+                    System.out.println("Thread " + w + " is started");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
---
+++ Java 3/J3Lesson5/Ferry.java
@@ -0,0 +1,55 @@
+/* Паромная переправа. Паром может переправлять одновременно
+ * по 3 автомобиля. Чтобы не гонять паром лишний раз, нужно отправлять его,
+ * когда у переправы соберется минимум 3 автомобиля.
+ */
+import java.util.concurrent.CyclicBarrier;
+
+class Ferry {
+    static final int NUMBER_OF_CARS = 3;
+    // инициализируем барьер на 3 потока + task, который будет выполняться,
+    // когда у барьера соберется 3 потока, после чего они будут освобождены
+    static final CyclicBarrier BARRIER =
+        new CyclicBarrier(NUMBER_OF_CARS, new FerryBoat());
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 9; i++) {
+            new Thread(new Car(i)).start();
+            Thread.sleep(400);
+        }
+    }
+
+    // task, который будет выполняться при достижении барьера
+    static class FerryBoat implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(500);
+                System.out.println("Ferry ferrying cars!");
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    // cтороны, которые будут достигать барьера
+    static class Car implements Runnable {
+        private int carNumber;
+
+        public Car(int carNumber) {
+            this.carNumber = carNumber;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.printf("Car №%d drove up to the ferry.\n", carNumber);
+                // для указания потоку о том что он достиг барьера, нужно вызвать
+                // метод await(), после этого данный поток блокируется, и ждет
+                // пока остальные достигнут барьера
+                BARRIER.await();
+                System.out.printf("Car №%d continued to move.\n", carNumber);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
