844217838bc60f9b1c3ee4cdc96877f5088b32cb
Sergey Iryupin <biblelamp@yandex.ru>
2018-02-25 14:46:42 +0300

    sample of hw 4 all stages - 1,2,3

--- Java 3/HW4Lesson.java
+++ Java 3/HW4Lesson.java
@@ -7,12 +7,12 @@
  *    (for 10 records, with a period of 20 ms)
  * 3. Write a MFD class, where you can print and scan documents at the same
  *    time, but you can not print two documents or scan at the same time
- *    (when printing, messages are output to the console "1, 2, 3, ... pages"
- *    printed, when scanning the same, only "scanned ...", output
- *    to the console with a period of 50 ms)
+ *    (when printing, write to the console "printed 1,2,3,... pages",
+ *    when scanning the same, only "scanned ...", output to the console
+ *    with a period of 50 ms)
  *
  * @author Sergey Iryupin
- * @version Feb 22, 2018
+ * @version Feb 25, 2018
  * @link https://github.com/biblelamp
  */
 import java.io.IOException;
@@ -24,9 +24,10 @@ public class HW4Lesson {
     final Object monitor = new Object();
     volatile char currentLetter = 'A';
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         HW4Lesson hw = new HW4Lesson();
 
+        /*
         // 1 stage
         new Thread(() -> hw.printLetter('A', 'B', 5)).start();
         new Thread(() -> hw.printLetter('B', 'C', 5)).start();
@@ -36,6 +37,24 @@ public class HW4Lesson {
         new Thread(() -> hw.printFile(new File("hw4lesson.txt"), 10)).start();
         new Thread(() -> hw.printFile(new File("hw4lesson.txt"), 10)).start();
         new Thread(() -> hw.printFile(new File("hw4lesson.txt"), 10)).start();
+        */
+
+        // 3 stage
+        MFD mfd = new MFD();
+        new Thread(()->mfd.print("doc1", 5)).start();
+        new Thread(()->mfd.print("doc2", 5)).start();
+
+        Thread.sleep(1000);
+        System.out.println();
+
+        new Thread(()->mfd.scan("doc3", 5)).start();
+        new Thread(()->mfd.scan("doc4", 5)).start();
+
+        Thread.sleep(1000);
+        System.out.println();
+
+        new Thread(()->mfd.scan("doc5", 5)).start();
+        new Thread(()->mfd.print("doc6", 5)).start();
     }
 
     void printLetter(char mainLetter, char nextLetter, int times) {
@@ -65,5 +84,32 @@ public class HW4Lesson {
                     ex.printStackTrace();
                 }
         }
+    }
+}
+
+class MFD {
+    private final Object printer = new Object();
+    private final Object scanner = new Object();
+
+    private void doIt(Object device, String docName, int pages) {
+        synchronized (device) {
+            for (int i = 0; i < pages; i++)
+                try {
+                    System.out.printf("%s page %d:%d of '%s'\n",
+                        (device.equals(printer)? "Printed" : "Scanned"),
+                        i + 1, pages, docName);
+                    Thread.sleep(50);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+        }
+    }
+
+    public void print(String docName, int pages) {
+        doIt(printer, docName, pages);
+    }
+
+    public void scan(String docName, int pages) {
+        doIt(scanner, docName, pages);
     }
 }
