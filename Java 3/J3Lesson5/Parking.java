771e0490802621908e85435f1944a663ac987505
Sergey Iryupin <biblelamp@yandex.ru>
2018-02-25 15:36:39 +0300

    Semaphore demo

--- Java 3/J3Lesson5/Parking.java
+++ Java 3/J3Lesson5/Parking.java
@@ -1,14 +1,19 @@
+/* Существует парковка, которая одновременно может вмещать не более
+ * 5 автомобилей. Если парковка заполнена полностью, то вновь прибывший
+ * автомобиль должен ждать пока не освободится хотя бы одно место.
+ */
 import java.util.concurrent.Semaphore;
 
 class Parking {
-    // парковочное место занято - true, свободно - false
-    static boolean[] PARKING_PLACES = new boolean[5];
-    // устанавливаем флаг "справедливый", в таком случае метод
-    // aсquire() будет раздавать разрешения в порядке очереди
-    static final Semaphore SEMAPHORE = new Semaphore(5, true);
+    static final int PARKING_SIZE = 5;
+    static final boolean[] PARKING_PLACES = new boolean[PARKING_SIZE];
+    // создаём Семафор с количеством разрешений == PARKING_SIZE и флагом
+    // "справедливость", в этом случае метод aсquire() будет
+    // раздавать разрешения в порядке очереди
+    static final Semaphore SEMAPHORE = new Semaphore(PARKING_SIZE, true);
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i < 11; i++) {
             new Thread(new Car(i)).start();
             Thread.sleep(400);
         }
@@ -23,11 +28,12 @@ class Parking {
 
         @Override
         public void run() {
-            System.out.printf("Car №%d drove up to the parking lot.\n", carNumber);
+            System.out.printf(
+                "Car №%d drove up to the parking lot.\n", carNumber);
             try {
-                // acquire() запрашивает доступ к следующему за вызовом этого метода
-                // блоку кода, если доступ не разрешен, поток вызвавший этот метод
-                // блокируется до тех пор, пока семафор не разрешит доступ
+                // acquire() запрашивает доступ к следующему за вызовом этого
+                // метода блоку кода, если доступ не разрешен, поток вызвавший
+                // этот метод блокируется, пока семафор не разрешит доступ
                 SEMAPHORE.acquire();
 
                 int parkingNumber = -1;
@@ -44,15 +50,20 @@ class Parking {
                         }
                 }
 
-                Thread.sleep(5000);       // уходим за покупками, к примеру
+                if (parkingNumber == -1)
+                    System.out.printf(
+                        "Car %d didn't find an empty parking lot.\n", carNumber);
+                else {
+                    Thread.sleep(5000); // уходим за покупками, приходим, уезжаем
 
-                synchronized (PARKING_PLACES) {
-                    PARKING_PLACES[parkingNumber] = false; // освобождаем место
+                    synchronized (PARKING_PLACES) {
+                        PARKING_PLACES[parkingNumber] = false; // освобождаем место
+                    }
+
+                    SEMAPHORE.release(); // освобождаем ресурс
+                    System.out.printf(
+                        "Car №%d left the parking lot.\n", carNumber);
                 }
-
-                // release() освобождает ресурс
-                SEMAPHORE.release();
-                System.out.printf("Car №%d left the parking lot.\n", carNumber);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- Java 3/J3Lesson5/SemaphoreDemo.java
+++ Java 3/J3Lesson5/SemaphoreDemo.java
@@ -2,20 +2,20 @@ import java.util.concurrent.Semaphore;
 
 class SemaphoreDemo {
     public static void main(String[] args) {
-        Semaphore smp = new Semaphore(2);
+        Semaphore semaphore = new Semaphore(2);
         for (int i = 0; i < 5; i++) {
             final int w = i;
             new Thread(() -> {
                 try {
                     System.out.println("Thread " + w + " before the semaphore");
-                    smp.acquire();
+                    semaphore.acquire();
                     System.out.println("Thread " + w + " accessed the resource");
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
                     System.out.println("Thread " + w + " freed the resource");
-                    smp.release();
+                    semaphore.release();
                 }
             }).start();
         }
