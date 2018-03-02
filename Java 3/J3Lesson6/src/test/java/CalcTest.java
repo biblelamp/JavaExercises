cd9d8e1d994c434957c8673f319ef309daf2a0ea
Sergey Iryupin <biblelamp@yandex.ru>
2018-03-02 21:56:20 +0300

    added @Before

--- Java 3/J3Lesson6/src/test/java/CalcTest.java
+++ Java 3/J3Lesson6/src/test/java/CalcTest.java
@@ -1,30 +1,32 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalcTest {
     private Calculator calculator;
 
+    @Before
+    public void startTest() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void testAdd() {
-        calculator = new Calculator();
         Assert.assertEquals(4, calculator.add(2, 2));
     }
 
     @Test
     public void testSub() {
-        calculator = new Calculator();
         Assert.assertEquals(3, calculator.sub(5, 2));
     }
 
     @Test
     public void testMul() {
-        calculator = new Calculator();
         Assert.assertEquals(9, calculator.mul(3, 3));
-        }
+    }
 
     @Test
     public void testDiv() {
-        calculator = new Calculator();
         Assert.assertEquals(1, calculator.div(2, 2));
     }
 }
