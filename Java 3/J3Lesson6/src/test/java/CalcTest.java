250d8323904ae903bfbc53c351fd2ad8fe8c141e
Sergey Iryupin <biblelamp@yandex.ru>
2018-03-02 21:53:36 +0300

    added tests

--- Java 3/J3Lesson6/src/test/java/CalcTest.java
+++ Java 3/J3Lesson6/src/test/java/CalcTest.java
@@ -3,10 +3,28 @@ import org.junit.Test;
 
 public class CalcTest {
     private Calculator calculator;
-    
+
     @Test
     public void testAdd() {
         calculator = new Calculator();
         Assert.assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    public void testSub() {
+        calculator = new Calculator();
+        Assert.assertEquals(3, calculator.sub(5, 2));
+    }
+
+    @Test
+    public void testMul() {
+        calculator = new Calculator();
+        Assert.assertEquals(9, calculator.mul(3, 3));
+        }
+
+    @Test
+    public void testDiv() {
+        calculator = new Calculator();
+        Assert.assertEquals(1, calculator.div(2, 2));
     }
 }
