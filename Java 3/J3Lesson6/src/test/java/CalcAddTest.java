564cb61c4c0b53b3ecff28542e58631b6942e473
Sergey Iryupin <biblelamp@yandex.ru>
2018-03-04 21:39:13 +0300

    example of more tests for add

--- Java 3/J3Lesson6/src/test/java/CalcAddTest.java
+++ Java 3/J3Lesson6/src/test/java/CalcAddTest.java
@@ -3,7 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
 
-@Ignore
+//@Ignore
 public class CalcAddTest {
     private Calculator calculator;
 
@@ -17,30 +17,37 @@ public class CalcAddTest {
         Assert.assertEquals(0, calculator.add(0, 0));
     }
 
+    @Test
     public void testAdd2() {
         Assert.assertEquals(2, calculator.add(1, 1));
     }
 
+    @Test
     public void testAdd3() {
-        Assert.assertEquals(4, calculator.add(2, 2));
+        Assert.assertEquals(-4, calculator.add(-2, -2));
     }
 
+    @Test
     public void testAdd4() {
-        Assert.assertEquals(10, calculator.add(5, 5));
+        Assert.assertEquals(0, calculator.add(-5, 5));
     }
 
+    @Test
     public void testAdd5() {
-        Assert.assertEquals(6, calculator.add(4, 2));
+        Assert.assertEquals(2, calculator.add(4, -2));
     }
 
+    @Test
     public void testAdd6() {
-        Assert.assertEquals(5, calculator.add(1, 3));
+        Assert.assertEquals(-2, calculator.add(-5, 3));
     }
 
+    @Test
     public void testAdd7() {
         Assert.assertEquals(4, calculator.add(6, -2));
     }
 
+    @Test
     public void testAdd8() {
         Assert.assertEquals(4, calculator.add(-1, 5));
     }
