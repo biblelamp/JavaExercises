f25c48d3914ea0901238e5c363262c9b28cd6d37
Sergey Iryupin <biblelamp@yandex.ru>
2018-03-12 23:29:58 +0300

    added try with resources

--- Java 1/FileWriteRead.java
+++ Java 1/FileWriteRead.java
@@ -2,7 +2,7 @@
  * Java. Level 1. Lesson 3. Example of writing/reading file
  *
  * @author Sergey Iryupin
- * @version dated Feb 02, 2018
+ * @version dated Mar 12, 2018
  */
 import java.io.FileWriter;
 import java.io.FileReader;
@@ -13,21 +13,19 @@ class FileWriteRead {
     public static void main(String[] args) {
 
         // writing to file
-        try {
-            FileWriter file = new FileWriter("text.txt");
+        try (FileWriter file = new FileWriter("text.txt")) {
             file.write("Hello\nJava");
-            file.close();
+            //file.close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
         // reading from file
-        try {
+        try (FileReader file = new FileReader("text.txt")) {
             int b;
-            FileReader file = new FileReader("text.txt");
             while ((b = file.read()) != -1)
                 System.out.print((char)b);
-            file.close();
+            //file.close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
