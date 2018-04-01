9740e087aed3c8ef05e573463cd460dd9bff320c
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-01 21:54:33 +0200

    2nd step in implementation of arrays

--- JBasic/src/model/Dim.java
+++ JBasic/src/model/Dim.java
@@ -4,15 +4,32 @@ package model;
  * model.Dim - working with arrays
  *
  * @author Sergey Iryupin
- * @version 0.1 dated Mar 31, 2018
+ * @version 0.2 dated Apr 01, 2018
  */
+import java.util.HashMap;
+import java.util.Map;
+
+import static tools.IConstants.OPER_DIM;
+
 public class Dim {
     Variables variables;
+    Map<String, String> dim;
 
     public Dim(Variables variables) {
         this.variables = variables;
+        dim = new HashMap<>();
     }
 
     public void init(ProgramLines programLines) {
+        for (Integer line : programLines.keySet()) {
+            String str = programLines.get(line);
+            if (str.startsWith(OPER_DIM)) {
+                String list = str.substring(OPER_DIM.length());
+                String[] items = list.split("\\)[,]?");
+                for (int i = 0; i < items.length; i++)
+                    dim.put(items[i].trim().substring(0, 1),
+                            items[i].trim().substring(2));
+            }
+        }
     }
 }
