2fd42987fbf7f95bab0796ea80dbe812522246f2
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-07 14:56:51 +0200

    2nd step in implementation of arrays

--- JBasic/src/model/Dim.java
+++ JBasic/src/model/Dim.java
@@ -4,7 +4,7 @@ package model;
  * model.Dim - working with arrays
  *
  * @author Sergey Iryupin
- * @version 0.2.1 dated Apr 04, 2018
+ * @version 0.2.2 dated Apr 07, 2018
  */
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +13,7 @@ import static tools.IConstants.OPER_DIM;
 
 public class Dim {
     Variables variables;
-    Map<String, String> dim;
+    Map<String, float[][]> dim;
 
     public Dim(Variables variables) {
         this.variables = variables;
@@ -26,17 +26,30 @@ public class Dim {
             if (str.startsWith(OPER_DIM)) {
                 String list = str.substring(OPER_DIM.length());
                 String[] items = list.split("\\)[,]?");
-                for (int i = 0; i < items.length; i++)
-                    dim.put(items[i].trim().substring(0, 1),
-                            items[i].trim().substring(2));
+                for (int i = 0; i < items.length; i++) {
+                    String name = items[i].trim().substring(0, 1);
+                    String[] idx = items[i].trim().substring(2).split(",");
+                    float[][] array;
+                    int idx1 = Integer.parseInt(idx[0]);
+                    if (idx.length > 1) {
+                        int idx2 = Integer.parseInt(idx[1]);
+                        array = new float[idx2][idx1];
+                    } else
+                        array = new float[1][idx1];
+                    dim.put(name, array);
+                }
             }
         }
     }
 
-    public float get(String name, String idx) {
-        return 0;
+    public float get(String name, int idX, int idY) {
+        float[][] array = dim.get(name);
+        return array[idY][idX];
     }
 
-    public void put(String name, String idx) {
+    public void put(String name, int idX, int idY, float value) {
+        float[][] array = dim.get(name);
+        array[idY][idX] = value;
+        dim.put(name, array);
     }
 }
