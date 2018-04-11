faa52bd3b302d9c8d3566fc5e46ab2b72e9233b6
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-11 22:11:57 +0200

    added a generic value

--- Test Tasks/bcpraha.com/src/cz/parser/Item.java
+++ Test Tasks/bcpraha.com/src/cz/parser/Item.java
@@ -4,15 +4,25 @@ package cz.parser;
  * Item - the command-line element
  *
  * @author Sergey Iryupin
- * @version 0.1 dated Apr 10, 2018
+ * @version 0.2 dated Apr 11, 2018
  */
-public class Item {
+public class Item<E> {
     private String name;
     private boolean mandatory;
     private String description;
     private String[] aliases;
+    private E byDefault;
+    private E value;
 
-    public Item() {
-        //
+    public Item(String name, boolean mandatory, String description, String[] aliases, E byDefault) {
+        this.name = name;
+        this.mandatory = mandatory;
+        this.description = description;
+        this.aliases = aliases;
+        this.byDefault = byDefault;
+    }
+
+    public E getValue() {
+        return value;
     }
 }
