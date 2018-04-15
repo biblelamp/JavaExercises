3733090f8a39da894095bd78755d03c85351658b
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-15 21:39:19 +0200

    next step

--- Test Tasks/bcpraha.com/src/App.java
+++ Test Tasks/bcpraha.com/src/App.java
@@ -2,7 +2,7 @@
  * App - example of using command-parser
  *
  * @author Sergey Iryupin
- * @version 0.2.1 dated Apr 14, 2018
+ * @version 0.2.2 dated Apr 15, 2018
  */
 import cz.parser.Action;
 import cz.parser.CLExecutor;
@@ -18,6 +18,10 @@ public class App {
                 new String[]{"-l", "--left"}, Action.GET));
         cle.add(new Item<Integer>("R", true, "-r/--right <Integer>",
                 new String[]{"-r", "--right"}, Action.GET));
+        cle.add(new Item<Integer>("O", true, "-o/--operation <Action>",
+                new String[]{"-o", "--operation"}, Action.CALCULATE));
+        cle.add(new Item<Boolean>("S", false, "-v/--verbose",
+                new String[]{"-v", "--verbose"}, Action.PRINT));
 
         String cmd = "-l 5 -r 6 -o PLUS -v";
         cle.execute(cmd.split(" "));
--- Test Tasks/bcpraha.com/src/cz/parser/CLExecutor.java
+++ Test Tasks/bcpraha.com/src/cz/parser/CLExecutor.java
@@ -4,17 +4,21 @@ package cz.parser;
  * CLExecutor - the command-line executor
  *
  * @author Sergey Iryupin
- * @version 0.2 dated Apr 13, 2018
+ * @version 0.2.1 dated Apr 15, 2018
  */
+import java.util.HashMap;
 import java.util.Map;
 
 public class CLExecutor {
-    private Map<String, Item> map;
+    private Map<String, Item> map = new HashMap<>();
 
     public void add(Item item) {
         map.put(item.getName(), item);
     }
 
     public void execute(String[] args) {
+        for(String str : args) {
+            System.out.println(str);
+        }
     }
 }
