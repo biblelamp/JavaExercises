ad16f04a15eb55b409ed1f2cc832d501991ff600
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-22 18:56:02 +0200

    finished 1st working version

--- Test Tasks/bsc-ideas.com/src/model/Data.java
+++ Test Tasks/bsc-ideas.com/src/model/Data.java
@@ -4,31 +4,54 @@ package model;
  * Data - data of currency accounts
  *
  * @author Sergey Iryupin
- * @version 0.1 dated Apr 21, 2018
+ * @version 0.2 dated Apr 22, 2018
  */
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.HashMap;
 
 public class Data {
-    private Map<String, Double> data;
+    private Map<String, Integer> data;
 
     public Data() {
         data = new HashMap<>();
     }
 
-    public void addRecord(String record) {
-        //
+    public void processLine(String line) {
+        String[] str = line.split(" ");
+        if (str.length > 1)
+            try {
+                add(str[0], Integer.parseInt(str[1]));
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+    }
+
+    public void add(String key, int value) {
+        data.put(key, data.getOrDefault(key, 0) + value);
     }
 
     public void addFromFile(String fileName) {
-        //
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(fileName)))) {
+            String line;
+            while ((line = reader.readLine()) != null)
+                processLine(line);
+       } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public String toString() {
         String result = "";
-        for (Map.Entry<String, Double> item : data.entrySet())
-            result += item.getKey() + " " +  item.getValue() + "\n";
+        for (Map.Entry<String, Integer> item : data.entrySet())
+            if (item.getValue() != 0)
+                result += item.getKey() + " " +  item.getValue() + "\n";
         return result;
     }
 }
