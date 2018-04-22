475cf83bef0f29b1721330aaa8fa02e561e664fb
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-22 15:56:12 +0200

    changed main class

--- Test Tasks/bsc-ideas.com/src/PayTracker.java
+++ Test Tasks/bsc-ideas.com/src/PayTracker.java
@@ -2,11 +2,14 @@
  * PayTracker - Payment Tracker
  *
  * @author Sergey Iryupin
- * @version 0.1 dated Apr 21, 2018
+ * @version 0.2 dated Apr 22, 2018
  */
+import controller.Controller;
+import model.Data;
+
 public class PayTracker {
 
     public static void main(String[] args) {
-
+        new Controller(new Data(), args);
     }
 }
