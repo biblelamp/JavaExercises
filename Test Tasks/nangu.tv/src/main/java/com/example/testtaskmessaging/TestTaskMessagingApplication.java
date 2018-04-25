f1dad77e179b040fa41b986de3ad02ea9876d715
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-25 21:26:20 +0200

    initial commit

---
+++ Test Tasks/nangu.tv/src/main/java/com/example/testtaskmessaging/TestTaskMessagingApplication.java
@@ -0,0 +1,12 @@
+package com.example.testtaskmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskMessagingApplication.class, args);
+	}
+}
