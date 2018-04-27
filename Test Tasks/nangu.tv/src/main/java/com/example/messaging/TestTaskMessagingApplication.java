d3c952da1ae6ee512f9654cf0f34c9a73bc6db4f
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-27 12:22:24 +0200

    finished changing

--- Test Tasks/nangu.tv/src/main/java/com/example/messaging/TestTaskMessagingApplication.java
+++ Test Tasks/nangu.tv/src/main/java/com/example/messaging/TestTaskMessagingApplication.java
@@ -1,12 +1,89 @@
-package com.example.testtaskmessaging;
+package com.example.messaging;
 
+import com.example.messaging.model.User;
+import com.example.messaging.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Objects;
+
+import static java.lang.String.format;
+
+@Configuration
 @SpringBootApplication
 public class TestTaskMessagingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestTaskMessagingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskMessagingApplication.class, args);
+    }
+}
+
+@Configuration
+class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    private final UserService userService;
+
+    @Autowired
+    public WebSecurityConfiguration(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService()).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    UserDetailsService userDetailService() {
+        return s -> {
+            User foundUser = userService.findByUserName(s);
+            if (Objects.nonNull(foundUser)) {
+                return new org.springframework.security.core.userdetails.User(
+                        foundUser.getUserName(),
+                        foundUser.getPassword(),
+                        true,
+                        true,
+                        true,
+                        true,
+                        AuthorityUtils.createAuthorityList("USER")
+                );
+            }
+
+            throw new UsernameNotFoundException(format("Could not find user %s", s));
+        };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/message/**/*").fullyAuthenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
 }
