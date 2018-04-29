71c69771e078f0adc84b6d86f67f308cccfd5eb7
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-29 15:46:54 +0200

    initial commit

--- StarCommX4/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ StarCommX4/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,8 +7,6 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1280;
-		config.height = 720;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
