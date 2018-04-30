2bd5a2f9ef42b47451a60fc145cf9b34cc97b956
Sergey Iryupin <biblelamp@yandex.ru>
2018-04-30 19:28:10 +0200

    show background with stars

--- StarCommX4/core/src/com/mygdx/game/MyGdxGame.java
+++ StarCommX4/core/src/com/mygdx/game/MyGdxGame.java
@@ -8,26 +8,30 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Background bg;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		bg = new Background();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	    update();
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+        bg.render(batch);
 		batch.end();
 	}
-	
+
+    public void update() {
+        bg.update();
+    }
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
