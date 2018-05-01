dd3848e0c47d5059390073129147209b7a17856f
Sergey Iryupin <biblelamp@yandex.ru>
2018-05-01 10:52:38 +0200

    added changing of speed and flicker

--- StarCommX4/core/src/com/mygdx/game/Background.java
+++ StarCommX4/core/src/com/mygdx/game/Background.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class Background {
@@ -10,27 +11,28 @@ public class Background {
         float speed;
 
         public Star() {
-            position = new Vector2((float)Math.random() * 1024, (float)Math.random() * 576);
-            speed = 1.0f + (float)Math.random() * 5.0f;
+            position = new Vector2(MathUtils.random(0, 1024), MathUtils.random(0, 576));
+            speed = MathUtils.random(0.5f, 2.0f);
         }
 
         public void update() {
             position.x -= speed;
             if(position.x < 0) {
                 position.x = 1024;
-                position.y = (float)Math.random() * 576;
+                position.y = MathUtils.random(0, 576);
+                speed = MathUtils.random(0.5f, 2.0f);
             }
         }
     }
 
-    private Texture texture;
-    private Texture texStar;
+    private Texture imgBg;
+    private Texture imgStar;
     private final int STARS_COUNT = 200;
     private Star[] stars;
 
     public Background() {
-        texture = new Texture("bg.png");
-        texStar = new Texture("star12.tga");
+        imgBg = new Texture("bg.png");
+        imgStar = new Texture("star12.tga");
         stars = new Star[STARS_COUNT];
         for (int i = 0; i < STARS_COUNT; i++) {
             stars[i] = new Star();
@@ -38,9 +40,13 @@ public class Background {
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, 0, 0);
+        batch.draw(imgBg, 0, 0);
         for (int i = 0; i < STARS_COUNT; i++) {
-            batch.draw(texStar, stars[i].position.x, stars[i].position.y);
+            float size = stars[i].speed / 2.0f;
+            if (MathUtils.random(0, 50) < 1)
+                size *= 1.8f;
+            batch.draw(imgStar, stars[i].position.x, stars[i].position.y, 6, 6, 12, 12, size, size, 0, 0, 0, 12, 12, false, false);
+            //batch.draw(imgStar, stars[i].position.x, stars[i].position.y);
         }
     }
 
