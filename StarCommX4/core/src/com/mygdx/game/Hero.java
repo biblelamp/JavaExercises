af4052ce00c5251ca2fd4f256bb1df5950fd0e96
Sergey Iryupin <biblelamp@yandex.ru>
2018-05-02 09:30:01 +0200

    added control of the ship

--- StarCommX4/core/src/com/mygdx/game/Hero.java
+++ StarCommX4/core/src/com/mygdx/game/Hero.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -12,7 +14,7 @@ public class Hero {
     public Hero() {
         imgShip = new Texture("ship80x60.tga");
         position = new Vector2(100, 288);
-        speed = 10.0f;
+        speed = 8.0f;
     }
 
     public void render(SpriteBatch batch) {
@@ -20,6 +22,25 @@ public class Hero {
     }
 
     public void update() {
-
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            position.y -= speed;
+            if (position.y < 0)
+                position.y = 0;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            position.y += speed;
+            if (position.y > 576 - 60)
+                position.y = 576 - 60;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            position.x += speed;
+            if (position.x > 1024 - 80)
+                position.x = 1024 - 80;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            position.x -= speed;
+            if (position.x < 0)
+                position.x = 0;
+        }
     }
 }
