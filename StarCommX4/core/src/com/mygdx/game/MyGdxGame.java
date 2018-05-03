ca0b9eece25215728722a8d5c0d61482c45ff02d
Sergey Iryupin <biblelamp@yandex.ru>
2018-05-03 18:22:08 +0200

    order with indentations

--- StarCommX4/core/src/com/mygdx/game/MyGdxGame.java
+++ StarCommX4/core/src/com/mygdx/game/MyGdxGame.java
@@ -7,42 +7,42 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Background background;
-	Hero hero;
-	Asteroid[] asteroids;
-	static Bullet[] bullets;
+    SpriteBatch batch;
+    Background background;
+    Hero hero;
+    Asteroid[] asteroids;
+    static Bullet[] bullets;
 
-	@Override
-	public void create() {
-		batch = new SpriteBatch();
-		background = new Background();
-		hero = new Hero();
-		Texture imgAsteroid = new Texture("asteroid60.tga");
-		asteroids = new Asteroid[20];
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Background();
+        hero = new Hero();
+        Texture imgAsteroid = new Texture("asteroid60.tga");
+        asteroids = new Asteroid[20];
         for (int i = 0; i < asteroids.length; i++)
             asteroids[i] = new Asteroid(imgAsteroid);
         Texture imgBullet = new Texture("bullet20.tga");
         bullets = new Bullet[100];
-		for (int i = 0; i < bullets.length; i++)
-			bullets[i] = new Bullet(imgBullet);
-	}
+        for (int i = 0; i < bullets.length; i++)
+            bullets[i] = new Bullet(imgBullet);
+    }
 
-	@Override
-	public void render() {
-	    update();
-		Gdx.gl.glClearColor(0, 0, 15/255f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
+    @Override
+    public void render() {
+        update();
+        Gdx.gl.glClearColor(0, 0, 15/255f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
         background.render(batch);
         hero.render(batch);
         for (int i = 0; i < asteroids.length; i++)
             asteroids[i].render(batch);
-		for (int i = 0; i < bullets.length; i++)
-        	if (bullets[i].active)
-				bullets[i].render(batch);
-		batch.end();
-	}
+        for (int i = 0; i < bullets.length; i++)
+            if (bullets[i].active)
+                bullets[i].render(batch);
+        batch.end();
+    }
 
     public void update() {
         background.update();
@@ -58,5 +58,5 @@ public class MyGdxGame extends ApplicationAdapter {
                         bullets[i].deactivate();
                     }
             }
-	}
+    }
 }
