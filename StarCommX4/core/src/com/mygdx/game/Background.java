e29fffd3a2cd4292b432a154bc633d7a844d0151
Sergey Iryupin <biblelamp@yandex.ru>
2018-05-03 22:24:52 +0200

    refactoring a bit

--- StarCommX4/core/src/com/mygdx/game/Background.java
+++ StarCommX4/core/src/com/mygdx/game/Background.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class Background {
+    private Texture imgStar;
+    private Star[] stars;
+
     class Star {
         Vector2 position;
         float speed;
@@ -17,39 +20,32 @@ public class Background {
 
         public void update() {
             position.x -= speed;
-            if(position.x < 0) {
-                position.x = 1024;
-                position.y = MathUtils.random(0, 576);
+            if (position.x < 0) {
+                position.set(1024, MathUtils.random(0, 576));
                 speed = MathUtils.random(0.5f, 2.0f);
             }
         }
     }
 
-    private Texture imgStar;
-    private final int STARS_COUNT = 200;
-    private Star[] stars;
-
     public Background() {
         imgStar = new Texture("star12.tga");
-        stars = new Star[STARS_COUNT];
-        for (int i = 0; i < STARS_COUNT; i++) {
+        stars = new Star[200];
+        for (int i = 0; i < stars.length; i++)
             stars[i] = new Star();
-        }
     }
 
     public void render(SpriteBatch batch) {
-        for (int i = 0; i < STARS_COUNT; i++) {
+        for (int i = 0; i < stars.length; i++) {
             float size = stars[i].speed / 2.0f;
             if (MathUtils.random(0, 50) < 1)
                 size *= 1.8f;
-            batch.draw(imgStar, stars[i].position.x, stars[i].position.y, 6, 6, 12, 12, size, size, 0, 0, 0, 12, 12, false, false);
-            //batch.draw(imgStar, stars[i].position.x, stars[i].position.y);
+            batch.draw(imgStar, stars[i].position.x, stars[i].position.y,
+                    6, 6, 12, 12, size, size, 0, 0, 0, 12, 12, false, false);
         }
     }
 
     public void update() {
-        for (int i = 0; i < STARS_COUNT; i++) {
+        for (int i = 0; i < stars.length; i++)
             stars[i].update();
-        }
     }
 }
