9ad12c9642f00f34fcaa677bc782c621e63eed43
Sergey Iryupin <biblelamp@yandex.ru>
2018-05-18 14:01:02 +0200

    replaced the tabs with spaces

--- StarCommX4/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ StarCommX4/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,10 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1024;
-		config.height = 576; // 1024 * 0,5625
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 1024;
+        config.height = 576; // 1024 * 0,5625
+        new LwjglApplication(new MyGdxGame(), config);
+    }
 }
