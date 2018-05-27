d6d097aa488fa227b65f82c50b36e680b340d53d
Sergey Iryupin <biblelamp@yandex.ru>
2018-05-27 22:05:10 +0200

    init commit

---
+++ Test Tasks/merck.com/jdbc/src/main/java/BookShelf.java
@@ -0,0 +1,11 @@
+public class BookShelf {
+
+    public static void main(String[] args) {
+        BookShelf hello = new BookShelf();
+        System.out.println(hello.sayHello());
+    }
+
+    String sayHello() {
+        return "Hello, BookShelf!";
+    }
+}
