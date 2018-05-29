64dcf2f3037602295655f5df12110d827b7124a7
Sergey Iryupin <biblelamp@yandex.ru>
2018-05-29 08:13:27 +0200

    added drop table

--- Test Tasks/merck.com/jdbc/src/main/java/model/ITable.java
+++ Test Tasks/merck.com/jdbc/src/main/java/model/ITable.java
@@ -8,6 +8,19 @@ package model;
  * @version dated May 29, 2018
  */
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public interface ITable {
     void addLine(String line);
+    default ITable dropTable(Connection connection) {
+        try (Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate(
+                    "DROP TABLE IF EXISTS " +
+                            this.getClass().getSimpleName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return this;
+    }
 }
