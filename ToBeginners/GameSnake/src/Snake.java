51257d64dfe24d15de089095c1144da57f1b656d
Sergey Iryupin <biblelamp@yandex.ru>
2018-06-05 17:23:02 +0200

    refactored a bit

--- ToBeginners/GameSnake/src/Snake.java
+++ ToBeginners/GameSnake/src/Snake.java
@@ -3,44 +3,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
-    List<Point> snake = new ArrayList<>();
-    int direction;
+    private List<Point> snake = new ArrayList<>();
+    private int direction;
 
     public Snake(int x, int y, int length, int direction) {
-        for (int i = 0; i < length; i++) {
-            Point point = new Point(x - i, y);
-            snake.add(point);
-        }
+        for (int i = 0; i < length; i++)
+            snake.add(new Point(x - i, y));
         this.direction = direction;
     }
 
     public void setDirection(int direction) {
-        if ((direction >= GameSnake.LEFT) && (direction <= GameSnake.DOWN)) {
-            if (Math.abs(this.direction - direction) != 2) {
+        if ((direction >= GameSnake.LEFT) && (direction <= GameSnake.DOWN))
+            if (Math.abs(this.direction - direction) != 2)
                 this.direction = direction;
-            }
-        }
     }
 
     public void move() {
         int x = snake.get(0).getX();
         int y = snake.get(0).getY();
-        if (direction == GameSnake.LEFT)
-            x--;
-        if (direction == GameSnake.RIGHT)
-            x++;
-        if (direction == GameSnake.UP)
-            y--;
-        if (direction == GameSnake.DOWN)
-            y++;
-        if (x < 0)
-            x = GameSnake.WIDTH - 1;
-        if (x == GameSnake.WIDTH)
-            x = 0;
-        if (y < 0)
-            y = GameSnake.HEIGHT - 1;
-        if (y == GameSnake.HEIGHT)
-            y = 0;
+        switch (direction) {
+            case GameSnake.LEFT: x--;
+                if (x < 0)
+                    x = GameSnake.WIDTH - 1;
+                break;
+            case GameSnake.RIGHT: x++;
+                if (x == GameSnake.WIDTH)
+                    x = 0;
+                break;
+            case GameSnake.UP: y--;
+                if (y < 0)
+                    y = GameSnake.HEIGHT - 1;
+                break;
+            case GameSnake.DOWN: y++;
+                if (y == GameSnake.HEIGHT)
+                    y = 0;
+                break;
+        }
         snake.add(0, new Point(x, y));
         snake.remove(snake.size() - 1);
     }
