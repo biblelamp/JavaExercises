f65a8a44f2eb349019578940a9bd59e230432f07
Sergey Iryupin <biblelamp@yandex.ru>
2018-06-20 19:53:01 +0200

    added proportional dimensions

--- ToBeginners/SnakeGDX/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ ToBeginners/SnakeGDX/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.SnakeGdxGame;
 
 public class DesktopLauncher {
-	public static void main(String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SnakeGdxGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 32 * 32;
+        config.height = 32 * 18;
+        new LwjglApplication(new SnakeGdxGame(), config);
+    }
 }
