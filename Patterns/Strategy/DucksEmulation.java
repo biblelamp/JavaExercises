353676922760bd1b4fce2ed33c64a291d1bb3fa6
Sergey Iryupin <biblelamp@yandex.ru>
2018-07-03 19:12:43 +0200

    init commit

---
+++ Patterns/Strategy/DucksEmulation.java
@@ -0,0 +1,32 @@
+/**
+ * Java. Head First Design Patterns
+ *  Chapter 1: Strategy, init version
+ *
+ */
+public class DucksEmulation {
+    public static void main(String[] args) {
+    }
+}
+
+abstract class Duck {
+    void quack() { }
+    void swim() { }
+    void display() { }
+}
+
+class MallardDuck extends Duck {
+    @Override
+    void display() { }
+}
+
+class RedheadDuck extends Duck {
+    @Override
+    void display() { }
+}
+
+class RubberDuck extends Duck {
+    @Override
+    void quack() { } // rubber ducks don't quack
+    @Override
+    void display() { }
+}
