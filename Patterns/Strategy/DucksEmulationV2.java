13378bed7ece46ec12f8e54da81c494467afad82
Sergey Iryupin <biblelamp@yandex.ru>
2018-07-03 21:49:49 +0200

    version with interfaces

---
+++ Patterns/Strategy/DucksEmulationV2.java
@@ -0,0 +1,47 @@
+/**
+ * Java. Head First Design Patterns
+ *  Chapter 1: Strategy, version with interfaces
+ *
+ */
+public class DucksEmulationV2 {
+    public static void main(String[] args) {
+    }
+}
+
+abstract class Duck {
+    void swim() { }
+    void display() { }
+}
+
+interface Flyable {
+    void fly();
+}
+
+interface Quackable {
+    void quack();
+}
+
+class MallardDuck extends Duck implements Quackable, Flyable {
+    @Override
+    void display() { }
+    public void fly() { }
+    public void quack() { }
+}
+
+class RedheadDuck extends Duck implements Quackable, Flyable {
+    @Override
+    void display() { }
+    public void fly() { }
+    public void quack() { }
+}
+
+class RubberDuck extends Duck implements Quackable {
+    @Override
+    void display() { }
+    public void quack() { }
+}
+
+class DecoyDuck extends Duck {
+    @Override
+    void display() { }
+}
