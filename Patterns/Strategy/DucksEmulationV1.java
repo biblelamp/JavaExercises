e2bb09b6720328ea2ca0d1daa01016c9823dd721
Sergey Iryupin <biblelamp@yandex.ru>
2018-07-03 20:50:01 +0200

    version with inheritance

---
+++ Patterns/Strategy/DucksEmulationV1.java
@@ -0,0 +1,44 @@
+/**
+ * Java. Head First Design Patterns
+ *  Chapter 1: Strategy, version with inheritance
+ *
+ */
+public class DucksEmulationV1 {
+    public static void main(String[] args) {
+    }
+}
+
+abstract class Duck {
+    void quack() { }
+    void swim() { }
+    void display() { }
+    void fly() { } // added a new feature
+}
+
+class MallardDuck extends Duck {
+    @Override
+    void display() { }
+}
+
+class RedheadDuck extends Duck {
+    @Override
+    void display() { }
+}
+
+class RubberDuck extends Duck {
+    @Override
+    void quack() { } // rubber ducks don't quack
+    @Override
+    void display() { }
+    @Override
+    void fly() { } // rubber ducks don't fly
+}
+
+class DecoyDuck extends Duck { // added new inheritable class
+    @Override
+    void quack() { } // decoy ducks don't quack
+    @Override
+    void display() { }
+    @Override
+    void fly() { } // decoy ducks don't fly
+}
