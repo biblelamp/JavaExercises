c4e3cc8a1b6b140d24ebafa312481d2921c13487
Sergey Iryupin <biblelamp@yandex.ru>
2018-07-04 22:36:02 +0200

    tested mallard duck class

---
+++ Patterns/Strategy/src/DuckEmuation.java
@@ -0,0 +1,8 @@
+public class DuckEmuation {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.display();
+        mallard.performQuack();
+        mallard.performFly();
+    }
+}
---
+++ Patterns/Strategy/src/MallardDuck.java
@@ -0,0 +1,11 @@
+public class MallardDuck extends Duck {
+
+    public MallardDuck() {
+        flyBehavior = new FlyWithWings();
+        quackBehavior = new Quack();
+    }
+
+    public void display() {
+        System.out.println("I'm real Mallard duck!");
+    }
+}
