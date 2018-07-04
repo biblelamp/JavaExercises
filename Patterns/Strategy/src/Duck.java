3e8a5d227ab3d499ef4b011b0dfcbf483550260d
Sergey Iryupin <biblelamp@yandex.ru>
2018-07-04 22:30:59 +0200

    added abstract class

---
+++ Patterns/Strategy/src/Duck.java
@@ -0,0 +1,18 @@
+public abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public abstract void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+}
