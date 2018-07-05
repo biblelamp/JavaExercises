0d408c4aa6f7111172fa9431300b2fff5d192db6
Sergey Iryupin <biblelamp@yandex.ru>
2018-07-05 20:02:53 +0200

    added set methods

--- Patterns/Strategy/src/Duck.java
+++ Patterns/Strategy/src/Duck.java
@@ -12,6 +12,14 @@ public abstract class Duck {
         quackBehavior.quack();
     }
 
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
     public void swim() {
         System.out.println("All ducks float, even decoys!");
     }
