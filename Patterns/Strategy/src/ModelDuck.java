cb976e267edda28e7ed2816144d1168c4c5724d5
Sergey Iryupin <biblelamp@yandex.ru>
2018-07-05 20:03:22 +0200

    added new Duck subclass

---
+++ Patterns/Strategy/src/ModelDuck.java
@@ -0,0 +1,10 @@
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+
+    public void display() {
+        System.out.println("I'm model duck");
+    }
+}
