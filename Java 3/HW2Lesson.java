21404a74d8539003b02389b2a911a6f60747c683
Sergey Iryupin <biblelamp@yandex.ru>
2018-07-14 09:54:35 +0200

    refactored a bit

--- Java 3/HW2Lesson.java
+++ Java 3/HW2Lesson.java
@@ -8,7 +8,7 @@
  * 5. List of products in the given price range // -list <price1> <price2>
  *
  * @author Sergey Iryupin
- * @version Feb 10, 2018
+ * @version Jul 14, 2018
  */
 import java.sql.DriverManager;
 import java.sql.Connection;
@@ -52,8 +52,7 @@ public class HW2Lesson {
     static final String CMD_LIST = "-list";
     static final String MSG_NOTFOUND = "Not found";
 
-    Connection connect;
-    Statement stmt;
+    Connection connection = null;
 
     public static void main(String[] args) {
         HW2Lesson hw = new HW2Lesson(DRIVER_NAME, DB_NAME);
@@ -84,22 +83,29 @@ public class HW2Lesson {
                             System.out.println(item);
                     }
             }
+        hw.close();
     }
 
     HW2Lesson(String driverName, String dbName) { // get connection
-        connect = null;
-        stmt = null;
         try {
             Class.forName(driverName);
-            connect = DriverManager.getConnection(dbName);
-            stmt = connect.createStatement();
+            connection = DriverManager.getConnection(dbName);
         } catch (ClassNotFoundException | SQLException ex) {
             ex.printStackTrace();
         }
     }
 
+    void close() {                                // close connection
+        if (connection != null)
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
     void createTable() {                          // stage 1. create table
-        try {
+        try (Statement stmt = connection.createStatement()) {
             stmt.executeUpdate(SQL_CREATE_TABLE);
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -107,14 +113,16 @@ public class HW2Lesson {
     }
 
     void initTable(int quantity) {                // stage 2. init & fill table
-        try {
+        try (Statement stmt = connection.createStatement();
+                PreparedStatement pstmt =
+                    connection.prepareStatement(SQL_INSERT)) {
             stmt.executeUpdate(SQL_CLEAR_TABLE);
-            PreparedStatement pstmt = connect.prepareStatement(SQL_INSERT);
             for (int i = 1; i <= quantity; i++) {
                 pstmt.setString(1, "product" + i);
                 pstmt.setFloat(2, i*10);
-                pstmt.executeUpdate();
+                pstmt.addBatch();
             }
+            pstmt.executeBatch();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
@@ -122,12 +130,13 @@ public class HW2Lesson {
 
     float getPriceByName(String name) {           // stage 3. get price by name
         float price = -1;
-        try {
-            PreparedStatement pstmt = connect.prepareStatement(SQL_SELECT);
+        try (PreparedStatement pstmt =
+                connection.prepareStatement(SQL_SELECT)) {
             pstmt.setString(1, name);
             ResultSet rs = pstmt.executeQuery();
             while (rs.next())
                 price = rs.getFloat(COL_PRICE);
+            rs.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
@@ -135,8 +144,8 @@ public class HW2Lesson {
     }
                                                   // stage 4. set price by name
     void setPriceByName(String name, float price) {
-        try {
-            PreparedStatement pstmt = connect.prepareStatement(SQL_UPDATE);
+        try (PreparedStatement pstmt =
+                connection.prepareStatement(SQL_UPDATE)) {
             pstmt.setFloat(1, price);
             pstmt.setString(2, name);
             pstmt.executeUpdate();
@@ -147,8 +156,8 @@ public class HW2Lesson {
                                                   // stage 5. list in range
     List<String> getListInRange(float priceFrom, float priceTo) {
         List<String> list = new ArrayList<>();
-        try {
-            PreparedStatement pstmt = connect.prepareStatement(SQL_LIST_IN_RANGE);
+        try (PreparedStatement pstmt =
+                connection.prepareStatement(SQL_LIST_IN_RANGE)) {
             pstmt.setFloat(1, priceFrom);
             pstmt.setFloat(2, priceTo);
             ResultSet rs = pstmt.executeQuery();
@@ -157,6 +166,7 @@ public class HW2Lesson {
                     rs.getInt(COL_ID) + "\t" +
                     rs.getString(COL_TITLE) + "\t" +
                     rs.getFloat(COL_PRICE));
+            rs.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
