a31ea0f6c87e932f45a4a1f33c75f59a085d3551
Sergey Iryupin <biblelamp@yandex.ru>
2018-07-15 08:01:06 +0200

    refactored a bit

--- Java 3/J3Lesson3/SequenceInputStreamEx.java
+++ Java 3/J3Lesson3/SequenceInputStreamEx.java
@@ -11,19 +11,16 @@ import java.util.Collections;
 class SequenceInputStreamEx {
     public static void main(String[] args) {
         List<InputStream> al = new ArrayList<>();
-        FileInputStream in1 = null, in2 = null;
         SequenceInputStream seq = null;
-        FileOutputStream out = null;
-        try {
-            in1 = new FileInputStream("1.txt");
-            in2 = new FileInputStream("2.txt");
+        try (FileInputStream in1 = new FileInputStream("1.txt");
+                FileInputStream in2 = new FileInputStream("2.txt");
+                FileOutputStream out = new FileOutputStream("3.txt")) {
             al.add(in1);
             al.add(in2);
             Enumeration<InputStream> list = Collections.enumeration(al);
-            seq = new SequenceInputStream(list); //in1, in2);
-            out = new FileOutputStream("3.txt");
+            seq = new SequenceInputStream(list); //instead (in1, in2);
             int rb = seq.read();
-            while(rb != -1){
+            while (rb != -1) {
                 out.write(rb);
                 rb = seq.read();
             }
@@ -31,7 +28,6 @@ class SequenceInputStreamEx {
             e.printStackTrace();
         } finally {
             try { seq.close(); } catch ( IOException e ) { };
-            try { out.close(); } catch ( IOException e ) { };
         }
     }
 }
