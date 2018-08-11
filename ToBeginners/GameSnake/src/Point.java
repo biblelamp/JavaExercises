8c49fe5dde8a0c35147748d000bb201e317894ca
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-11 22:50:44 +0200

    added comments

--- ToBeginners/GameSnake/src/Point.java
+++ ToBeginners/GameSnake/src/Point.java
@@ -1,30 +1,40 @@
-import java.awt.*;
+/**
+ * Java. Classic Game Snake
+ *  Class Point: minimal building element
+ *
+ * @author Sergey Iryupin
+ * @version dated Jul 18, 2018
+ */
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Point {
-    protected int x, y;
-    protected Color color;
+    protected int x, y;                       // object coordinates
+    protected Color color;                    // object color
 
-    public Point(int x, int y, Color color) {
-        set(x, y);
-        this.color = color;
+    public Point(int x, int y, Color color) { // constructor
+        set(x, y);                            // init coordinates
+        this.color = color;                   // init color
     }
 
-    public void set(int x, int y) {
+    public void set(int x, int y) {           // set coordinates
         this.x = x;
         this.y = y;
     }
 
-    public int getX() {
+    public int getX() {                       // get the X coordinate
         return x;
     }
 
-    public int getY() {
+    public int getY() {                       // get the Y coordinate
         return y;
     }
 
-    public void paint(Graphics g) {
+    public void paint(Graphics g) {           // object rendering
         g.setColor(color);
-        g.fillOval(x * GameSnake.POINT_SIZE, y * GameSnake.POINT_SIZE,
-                GameSnake.POINT_SIZE, GameSnake.POINT_SIZE);
+        g.fillOval(x * GameSnake.POINT_SIZE,  // coordinates of the 
+                y * GameSnake.POINT_SIZE,     //    upper left corner
+                GameSnake.POINT_SIZE,         // width
+                GameSnake.POINT_SIZE);        // height
     }
 }
