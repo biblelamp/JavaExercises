f602fba85a11320e4e665480f6bda7cfd5efe42c
Sergey Iryupin <biblelamp@yandex.ru>
2018-09-28 22:01:27 +0200

    code for 6 lesson

--- Java 1/J1Lesson6.java
+++ Java 1/J1Lesson6.java
@@ -1,36 +1,17 @@
 class J1Lesson6 {
 
     public static void main(String[] args) {
-        Animal[] animals = {new Cat("Murzik", "white", 5), new Dog("Tuzik", "black", 8)};
-        for (Animal animal : animals)
-            System.out.println(animal + " say " + animal.voice());
-        //Animal animal = new Animal(null, null, -1);
-        //System.out.println(animal + " say " + animal.voice());
-        Singleton s1 = Singleton.getInstance();
-        Singleton s2 = Singleton.getInstance();
-        System.out.println(s1 + "\n" + s2);
+        IAnimal[] animals = {new Cat("Murzik", "white", 3),
+            new Dog("Barbos", "black", 11)};
+        for (IAnimal animal : animals)
+            System.out.println(animal + " " + animal.voice());
     }
-}
-
-class Singleton {
-    private static Singleton singleton;
-    // more fields...
-
-    private Singleton() { }
-
-    static Singleton getInstance() {
-        if (singleton == null)
-            singleton = new Singleton();
-        return singleton;
-    }
-    // more methods...
 }
 
 class Cat extends Animal {
     Cat(String name, String color, int age) {
         super(name, color, age);
     }
-    @Override
     public String voice() {
         return "meow";
     }
@@ -40,25 +21,24 @@ class Dog extends Animal {
     Dog(String name, String color, int age) {
         super(name, color, age);
     }
-    @Override
     public String voice() {
-        return "gaf-gaf";
+        return "wuf-wuf";
     }
 }
 
-abstract class Animal {
-    String name;
-    String color;
-    int age;
+interface IAnimal {
+    String voice();
+}
+
+abstract class Animal implements IAnimal {
+    protected String name;
+    protected String color;
+    protected int age;
 
     Animal(String name, String color, int age) {
         this.name = name;
         this.color = color;
         this.age = age;
-    }
-
-    String voice() {
-        return "gaf-gaf";
     }
 
     @Override
