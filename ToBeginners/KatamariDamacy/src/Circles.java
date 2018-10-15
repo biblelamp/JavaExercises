c0133108006c05117065d1a0b09e2e6f75f52af7
Sergey Iryupin <biblelamp@yandex.ru>
2018-10-15 16:39:43 +0200

    added a bit

--- ToBeginners/KatamariDamacy/src/Circles.java
+++ ToBeginners/KatamariDamacy/src/Circles.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.LinkedList;
 import java.util.Random;
@@ -7,7 +8,7 @@ import java.util.Random;
  *  Class Circles: enemies
  *
  * @author Sergey Iryupin
- * @version 0.1 dated Oct 14, 2018
+ * @version 0.2 dated Oct 15, 2018
  */
 
 public class Circles {
@@ -15,6 +16,25 @@ public class Circles {
     private Random random;
 
     public Circles() {
+        random = new Random();
+        circles = new LinkedList<>();
+        for (int i = 0; i < 50; i++) {
+            Circle circle;
+            do {
+                int size = random.nextInt(30) + 20;
+                int x = random.nextInt(KatamariDamacy.WIDTH - size);
+                int y = random.nextInt(KatamariDamacy.HEIGHT - size);
+                circle = new Circle(x, y, size, KatamariDamacy.KEY_SPACE, Color.black);
+            } while (isCrossing(circle));
+            circles.add(circle);
+        }
+    }
+
+    public boolean isCrossing(Circle circle) {
+        for (Circle item : circles) {
+            //
+        }
+        return false;
     }
 
     public void paint(Graphics g) {
