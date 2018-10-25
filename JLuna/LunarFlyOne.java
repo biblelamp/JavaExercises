054d45dce24e44a057d8e0fd06d9ec5eb9c3356d
Sergey Iryupin <biblelamp@yandex.ru>
2018-10-25 16:26:22 +0200

    added show methods

--- JLuna/LunarFlyOne.java
+++ JLuna/LunarFlyOne.java
@@ -13,18 +13,35 @@ public class LunarFlyOne {
     int exhaustSpeed = 3660;        // m/s, from the engine
     float accelLimin = 3 * 9.81f;   // 3G, G is earth acceleration of gravity
     int timeCount = -1;
-    float startHeight = 0;
     float startSpeed = 0;
-    int startFuelMass = 0;
+    float startHeight = 0;
+    int startFuelMass = 400;
     int startFlightTime = 0;
 
     // flight variables
-    float height;
+    int fuel;
+    int time;
     float speed;
-    float fuelMass;
-    float flightTime;
+    float height;
+    float acceleration;
+    int fuelMass;
+    int flightTime;
+    
+    private String showConstants() {
+        return String.format("g = %5.3f M = %d c = %d a = %5.3f m = %d",
+            accelOfGravity, dryMass, exhaustSpeed, accelLimin, startFuelMass);
+    }
+
+    private String showVariables() {
+        return String.format("f = %d t = %d v = %5.3f h = %5.3f a = %5.3f F = %d T = %d",
+            fuel, time, speed, height, acceleration, fuelMass, flightTime);
+    }
+
+    private void command() {
+        System.out.println(showVariables());
+    }
 
     public static void main(String[] args) {
-        new LunarFlyOne();
+        new LunarFlyOne().command();
     }
 }
