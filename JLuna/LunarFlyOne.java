f1cb5457a43ba8a5b90ef8e8e7831341abd190ea
Sergey Iryupin <biblelamp@yandex.ru>
2018-10-26 13:14:04 +0200

    added landing

--- JLuna/LunarFlyOne.java
+++ JLuna/LunarFlyOne.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  * based on http://slavav.ru/way_to_earth/
  *
  * @author Sergey Iryupin
- * @version 0.2 dated Oct 26, 2018
+ * @version 0.2.1 dated Oct 26, 2018
  */
 public class LunarFlyOne {
 
@@ -21,12 +21,12 @@ public class LunarFlyOne {
     float startFlightTime = 0;
 
     // flight variables
-    int fuel;
+    float fuel;
     float duration;
     float speed;
     float height;
     float acceleration;
-    int fuelMass;
+    float fuelMass;
     float flightTime;
 
     private String showConstants() {
@@ -35,7 +35,7 @@ public class LunarFlyOne {
     }
 
     private String showVariables() {
-        return String.format("f = %d t = %3.1f v = %5.3f h = %5.3f a = %5.3f F = %d T = %5.3f",
+        return String.format("f = %5.3f t = %5.3f v = %5.3f h = %5.3f a = %5.3f F = %5.3f T = %5.3f",
             fuel, duration, speed, height, acceleration, fuelMass, flightTime);
     }
 
@@ -46,23 +46,51 @@ public class LunarFlyOne {
         flightTime = startFlightTime;
     }
 
-    private void simulate(float reverse, int fuel, float duration) {
+    private float addHeiht(float duration, float speed, float acceleration) {
+        float averageSpeed = (speed + speed + duration * (acceleration - accelOfGravity)) / 2;
+        return duration * averageSpeed;
+    }
+
+    private boolean isAlmostLanded(float height) {
+        return Math.abs(height) < 0.00001;
+    }
+
+    private void simulate(float reverse, float fuel, float duration) {
         if (duration > 0) {
             if (fuelMass > 0) {
 
                 float consumption = fuel / duration;
                 acceleration = reverse * consumption * exhaustSpeed /
                     (dryMass + fuelMass);
-                float averageSpeed = (speed + speed + duration * (acceleration - accelOfGravity)) / 2;
+                if (height + addHeiht(duration, speed, acceleration) < 0) {
+                    float fullAcceleration = acceleration - accelOfGravity;
+                    double d = Math.pow(speed, 2) - 2 * fullAcceleration * height;
+                    double t1 = (-speed - Math.sqrt(d)) / fullAcceleration;
+                    double t2 = (-speed + Math.sqrt(d)) / fullAcceleration;
+                    if (t1 > 0) {
+                        duration = (float) t1;
+                    }
+                    if (t2 > 0 && t2 < duration) {
+                        duration = (float) t2;
+                    }
+                    this.duration = duration;
+                    fuel = consumption * duration;
+                    this.fuel = fuel;
+                    acceleration = reverse * consumption * exhaustSpeed /
+                        (dryMass + fuelMass);
+                }
+                height += addHeiht(duration, speed, acceleration);
                 speed += duration * (acceleration - accelOfGravity);
-                height += duration * averageSpeed;
                 fuelMass -= fuel;
                 flightTime -= duration * timeCount;
-                
-                if (height < 0) {
-                }
 
                 System.out.println(showVariables());
+                if (isAlmostLanded(height)) {
+                    System.out.println("Landing (" + height + ")");
+                    speed = 0;
+                    height = 0;
+                    flightTime = 0;
+                }
             }
         }
     }
