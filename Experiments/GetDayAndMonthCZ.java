90519b67e749ebd20372c2c7afc32f0ceb998572
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-15 16:07:28 +0100

    refactored

--- Experiments/GetDayAndMonthCZ.java
+++ Experiments/GetDayAndMonthCZ.java
@@ -2,22 +2,32 @@ import java.util.Date;
 import java.util.Calendar;
 
 class GetDayAndMonthCZ {
-    final static String[] months = {"ledna", "února", "března", "dubna", "května", "června",
+    final String[] months = {"ledna", "února", "března", "dubna", "května", "června",
         "července", "srpna", "září", "října", "listopadu", "prosince"};
-    static Calendar calendar;
+    Calendar calendar;
 
     public static void main(String[] args) {
-        System.out.println(getDayAndMonthCZ());
-        System.out.println(getDayAndMonthCZ(new Date(2018, 10, 23)));
+        GetDayAndMonthCZ gdm = new GetDayAndMonthCZ();
+
+        System.out.println(gdm.getDayAndMonthCZ());
+
+        gdm.add(1);
+        System.out.println(gdm.getDayAndMonthCZ());
+
+        gdm.add(-2);
+        System.out.println(gdm.getDayAndMonthCZ());
     }
 
-    public static String getDayAndMonthCZ() {
-        return getDayAndMonthCZ(new Date());
-    }
-
-    public static String getDayAndMonthCZ(Date date) {
+    public GetDayAndMonthCZ() {
         calendar = Calendar.getInstance();
-        calendar.setTime(date);
+        calendar.setTime(new Date());
+    }
+
+    public void add(int day) {
+        calendar.add(Calendar.DAY_OF_MONTH, day);
+    }
+
+    public String getDayAndMonthCZ() {
         int day = calendar.get(Calendar.DAY_OF_MONTH);
         int month = calendar.get(Calendar.MONTH);
         return Integer.toString(day) + ". " + months[month];
