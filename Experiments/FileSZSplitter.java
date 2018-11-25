53e1494d1fa49fbb35ec4c136f2e55ebfd60b8b2
Sergey Iryupin <biblelamp@yandex.ru>
2018-11-25 14:33:23 +0100

    changed for windows

--- Experiments/FileSZSplitter.java
+++ Experiments/FileSZSplitter.java
@@ -2,10 +2,14 @@ import java.util.*;
 import java.io.*;
 import java.nio.file.*;
 import java.nio.charset.*;
+import java.lang.reflect.Field;
+
+// for Windows:
+// java -Dfile.encoding=UTF-8 FileSZSplitter
 
 class FileSZSplitter {
-    static final String PATH = "/home/lamp/Downloads/";
-    static final String FILE_NAME = "/home/lamp/Downloads/Slova_zivota_2018_11-12.txt";
+    static final String PATH = "C:/Users/lamp/AndroidExercises/";
+    static final String FILE_NAME = "C:/Users/lamp/AndroidExercises/Slova_zivota_2018_11-12.txt";
     static final String[] DAYS = {"pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota", "neděle"};
     static final String[] MONTHS = {"ledna", "února", "března", "dubna", "května", "června",
         "července", "srpna", "září", "října", "listopadu", "prosince"};
@@ -16,26 +20,22 @@ class FileSZSplitter {
         List<String> lines = Files.readAllLines(Paths.get(FILE_NAME), StandardCharsets.UTF_8);
 
         for(String line: lines) {
-            for (String day : DAYS) {
-                if (line.startsWith(day)) {
+            for (String month : MONTHS) {
+                if (line.indexOf(month) > -1) {
+                    String[] fields = line.split(" ");
                     System.out.println(line);
-                    for (String month : MONTHS) {
-                        if (line.indexOf(month) > -1) {
-                            String[] fields = line.split(" ");
-                            //System.out.println(Arrays.toString(fields));
-                            if (numMonth > -1) {
-                                FileWriter fw = new FileWriter(PATH + Integer.toString(numMonth) + "/" + Integer.toString(numDay) + ".txt");
-                                sb.delete(0, sb.indexOf("\n\n") + 2);
-                                fw.write(sb.toString());
-                                fw.close();
-                                sb.setLength(0);
-                            }
-                            numDay = (int) Float.parseFloat(fields[2]);
-                            numMonth = Arrays.asList(MONTHS).indexOf(month);
-                            File folder = new File(PATH + Integer.toString(numMonth));
-                            folder.mkdirs();
-                        }
+                    //System.out.println(Arrays.toString(fields));
+                    if (numMonth > -1) {
+                        FileWriter fw = new FileWriter(PATH + Integer.toString(numMonth) + "/" + Integer.toString(numDay) + ".txt");
+                        sb.delete(0, sb.indexOf("\n\n") + 2);
+                        fw.write(sb.toString());
+                        fw.close();
+                        sb.setLength(0);
                     }
+                    numDay = (int) Float.parseFloat(fields[2]);
+                    numMonth = Arrays.asList(MONTHS).indexOf(month);
+                    File folder = new File(PATH + Integer.toString(numMonth));
+                    folder.mkdirs();
                 }
             }
             sb.append(line.trim() + "\n");
