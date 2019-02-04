0e7ff3d49944370f7285c092fee8b20a1b042556
Sergey Iryupin <biblelamp@yandex.ru>
2019-02-04 13:37:04 +0100

    init commit

---
+++ TestTasks/about-fun.com/tic-tac-toe/src/main/java/cz/javageek/tictactoe/TicTacToeApplication.java
@@ -0,0 +1,14 @@
+package cz.javageek.tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToeApplication.class, args);
+    }
+
+}
+
