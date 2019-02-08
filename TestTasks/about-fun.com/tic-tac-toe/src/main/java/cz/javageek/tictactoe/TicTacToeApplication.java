5a417695f378af6349d72e3aa71b694e1a437463
Sergey Iryupin <biblelamp@yandex.ru>
2019-02-08 20:50:41 +0100

    added useful links

--- TestTasks/about-fun.com/tic-tac-toe/src/main/java/cz/javageek/tictactoe/TicTacToeApplication.java
+++ TestTasks/about-fun.com/tic-tac-toe/src/main/java/cz/javageek/tictactoe/TicTacToeApplication.java
@@ -3,6 +3,12 @@ package cz.javageek.tictactoe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Useful links:
+ * http://www.springboottutorial.com/spring-boot-and-h2-in-memory-database
+ * https://o7planning.org/ru/11893/integrating-spring-boot-jpa-and-h2-database
+ */
+
 @SpringBootApplication
 public class TicTacToeApplication {
 
