76565432ffd75adaaacdc4e93067cd58c673c9ab
Sergey Iryupin <biblelamp@yandex.ru>
2019-02-10 22:47:58 +0100

    added a bit

--- TestTasks/about-fun.com/tic-tac-toe/src/main/java/cz/javageek/tictactoe/controller/GameController.java
+++ TestTasks/about-fun.com/tic-tac-toe/src/main/java/cz/javageek/tictactoe/controller/GameController.java
@@ -1,5 +1,8 @@
 package cz.javageek.tictactoe.controller;
 
+import cz.javageek.tictactoe.domain.Match;
+import cz.javageek.tictactoe.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,10 +10,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GameController {
 
+    @Autowired
+    MatchRepository matchRepository;
+
     @GetMapping(path="/JoinBattle")
     public @ResponseBody String joinBattle() {
 
-        return "JoinBattle";
+        Match match = new Match();
+
+        if (matchRepository.count() == 0) {
+            match.setField(".........");
+            match.setNext("x");
+            match.setStatus("W");
+            matchRepository.save(match);
+
+            return "Your mark is 'x'. Waiting for a partner";
+        } else {
+            match = matchRepository.getOne(1);
+
+            if (match.getStatus().equals("W")) {
+                match.setStatus("R");
+                matchRepository.save(match);
+
+                return "Your mark is 'o'. Waiting for the action of a partner";
+            }
+        }
+
+        return "JoinBattle: " + match;
     }
 
     @GetMapping(path="/PlaceMarker")
