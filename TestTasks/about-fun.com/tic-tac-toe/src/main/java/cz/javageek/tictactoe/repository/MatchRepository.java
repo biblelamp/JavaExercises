a8815284292703f4b16ed90443525089b59f3195
Sergey Iryupin <biblelamp@yandex.ru>
2019-02-10 18:13:38 +0100

    added repository

--- TestTasks/about-fun.com/tic-tac-toe/src/main/java/cz/javageek/tictactoe/repository/MatchRepository.java
+++ TestTasks/about-fun.com/tic-tac-toe/src/main/java/cz/javageek/tictactoe/repository/MatchRepository.java
@@ -3,5 +3,6 @@ package cz.javageek.tictactoe.repository;
 import cz.javageek.tictactoe.domain.Match;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MatchRepository extends CrudRepository<Match, Long> {
+public interface MatchRepository extends CrudRepository<Match, Integer> {
+    Match getOne(Integer id);
 }
