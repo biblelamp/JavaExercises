99c1f641dcadb013d5096d17248a63738db39f87
Sergey Iryupin <biblelamp@yandex.ru>
2019-06-04 10:46:27 +0200

    exercises with data and time

---
+++ Experiments/DateAndTime.java
@@ -0,0 +1,23 @@
+import java.sql.Date;
+import java.time.Instant;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.DatatypeConfigurationException;
+
+class DateSQLTest {
+
+    public static void main(String[] args) throws DatatypeConfigurationException {
+        Date d1 = Date.valueOf("1805-01-01");
+        //System.out.println(d1 + 1);
+        System.out.println(String.format("%tY", d1));
+
+        //Date currentDate = new Date();
+        //String isoDate = currentDate.toISOString();
+        //System.out.println(currentDate, isoDate);
+
+        Instant yourInstant = Instant.now();
+        String dateTimeString = yourInstant.toString();
+        XMLGregorianCalendar date2 = DatatypeFactory.newInstance().newXMLGregorianCalendar(dateTimeString);
+        System.out.println(date2);
+    }
+}
