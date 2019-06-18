69276e3b90b272b4935ed6f9eb5f80fd04730c75
Sergey Iryupin <biblelamp@yandex.ru>
2019-06-18 16:27:42 +0200

    example of BorderLayout

--- Java 1/swing/HelloSwingLayout.java
+++ Java 1/swing/HelloSwingLayout.java
@@ -1,21 +1,26 @@
 import javax.swing.JFrame;
 import java.awt.Button;
+import java.awt.BorderLayout;
 
 class HelloSwingLayout extends JFrame {
     public static void main(String[] args) {
-        new HelloSwing();
+        new HelloSwingLayout();
     }
 
-    HelloSwing() {
+    HelloSwingLayout() {
         setTitle("Hello Swing!");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500, 250);
         setLocationRelativeTo(null);
 
-        //Button btnExit = new Button("Exit");
-        //btnExit.addActionListener(e -> System.exit(0));
+        Button btnExit = new Button("SOUTH [Exit]");
+        btnExit.addActionListener(e -> System.exit(0));
 
-        add(btnExit);
+        add(new Button("NORTH"), BorderLayout.NORTH);
+        add(new Button("WEST"), BorderLayout.WEST);
+        add(new Button("EAST"), BorderLayout.EAST);
+        add(new Button("CENTER"), BorderLayout.CENTER);
+        add(btnExit, BorderLayout.SOUTH);
         setVisible(true);
     }
 }
