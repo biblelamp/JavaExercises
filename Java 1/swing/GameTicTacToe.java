a88c200972026ffd478e2f8b177d520cd65e4eaf
Sergey Iryupin <biblelamp@yandex.ru>
2019-06-23 09:59:44 +0200

    improved a bit

--- Java 1/swing/GameTicTacToe.java
+++ Java 1/swing/GameTicTacToe.java
@@ -17,12 +17,12 @@ class GameTicTacToe extends JFrame {
     final int WINDOW_SIZE = 300;
     final int WINDOW_DX = 7;
     final int WINDOW_DY = 55;
-    final int FIELD_SIZE = 3;
-    final int CELL_SIZE = WINDOW_SIZE / FIELD_SIZE;
-    char[][] field = new char[FIELD_SIZE][FIELD_SIZE];
-    final char PLAYER_DOT = 'x';
-    final char AI_DOT = 'o';
-    final char EMPTY_DOT = '.';
+    final int MAP_SIZE = 3;
+    final int CELL_SIZE = WINDOW_SIZE / MAP_SIZE;
+    char[][] map = new char[MAP_SIZE][MAP_SIZE];
+    final char DOT_X = 'x';
+    final char DOT_O = 'o';
+    final char DOT_EMPTY = '.';
     Canvas canvas;
     Random rand = new Random();
     boolean gameOver;
@@ -46,23 +46,23 @@ class GameTicTacToe extends JFrame {
                 int x = e.getX() / CELL_SIZE;
                 int y = e.getY() / CELL_SIZE;
                 if (isCellEmpty(x, y) && !gameOver) {
-                    field[x][y] = PLAYER_DOT;
+                    map[x][y] = DOT_X;
                     canvas.repaint();
-                    if (checkWin(PLAYER_DOT)) {
+                    if (checkWin(DOT_X)) {
                         System.out.println("YOU WON!");
                         gameOver = true;
                     } else {
-                        if (isFieldFull()) {
+                        if (isMapFull()) {
                             System.out.println("Sorry, draft...");
                             gameOver = true;
                         } else {
                             aiTurn();
                             canvas.repaint();
-                            if (checkWin(AI_DOT)) {
+                            if (checkWin(DOT_O)) {
                                 System.out.println("COMPUTER WON!");
                                 gameOver = true;
                             } else {
-                                if (isFieldFull()) {
+                                if (isMapFull()) {
                                     System.out.println("Sorry, draft...");
                                     gameOver = true;
                                 }
@@ -78,7 +78,7 @@ class GameTicTacToe extends JFrame {
         JButton start = new JButton("New game");
         start.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                initField();
+                initMap();
                 canvas.repaint();
             }
         });
@@ -94,52 +94,48 @@ class GameTicTacToe extends JFrame {
         add(BorderLayout.CENTER, canvas);
         add(BorderLayout.SOUTH, bp);
         setVisible(true);
-        initField();
+        initMap();
     }
 
-    void initField() {
-        for (int i = 0; i < FIELD_SIZE; i++)
-            for (int j = 0; j < FIELD_SIZE; j++)
-                field[i][j] = EMPTY_DOT;
+    void initMap() {
+        for (int i = 0; i < MAP_SIZE; i++)
+            for (int j = 0; j < MAP_SIZE; j++)
+                map[i][j] = DOT_EMPTY;
         gameOver = false;
     }
 
     boolean isCellEmpty(int x, int y) {
-        if (field[x][y] == EMPTY_DOT) return true;
-        return false;
+        return map[x][y] == DOT_EMPTY);
     }
 
     void aiTurn() {
         int x, y;
         do {
-            x = rand.nextInt(FIELD_SIZE);
-            y = rand.nextInt(FIELD_SIZE);
+            x = rand.nextInt(MAP_SIZE);
+            y = rand.nextInt(MAP_SIZE);
         } while (!isCellEmpty(x, y));
-        field[x][y] = AI_DOT;
+        map[x][y] = DOT_O;
     }
 
-    boolean isFieldFull() {
-        for (int i = 0; i < FIELD_SIZE; i++) {
-            for (int j = 0; j < FIELD_SIZE; j++) {
-                if (field[i][j] == EMPTY_DOT) return false;
+    boolean isMapFull() {
+        for (int i = 0; i < MAP_SIZE; i++) {
+            for (int j = 0; j < MAP_SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
             }
         }
         return true;
     }
 
-
     boolean checkWin(char dot) {
-        // check horizontals
-        if (field[0][0] == dot && field[0][1] == dot && field[0][2] == dot) return true;
-        if (field[1][0] == dot && field[1][1] == dot && field[1][2] == dot) return true;
-        if (field[2][0] == dot && field[2][1] == dot && field[2][2] == dot) return true;
-        // check verticals
-        if (field[0][0] == dot && field[1][0] == dot && field[2][0] == dot) return true;
-        if (field[0][1] == dot && field[1][1] == dot && field[2][1] == dot) return true;
-        if (field[0][2] == dot && field[1][2] == dot && field[2][2] == dot) return true;
+        // check horizontals and verticals
+        for (int i = 0; i < MAP_SIZE; i++)
+            if ((map[i][0] == dot && map[i][1] == dot && map[i][2] == dot) ||
+                (map[0][i] == dot && map[1][i] == dot && map[2][i] == dot))
+                return true;
         // check diagonals
-        if (field[0][0] == dot && field[1][1] == dot && field[2][2] == dot) return true;
-        if (field[2][0] == dot && field[1][1] == dot && field[0][2] == dot) return true;
+        if ((map[0][0] == dot && map[1][1] == dot && map[2][2] == dot) ||
+            (map[2][0] == dot && map[1][1] == dot && map[0][2] == dot))
+            return true;
         return false;
     }
 
@@ -147,20 +143,21 @@ class GameTicTacToe extends JFrame {
         @Override
         public void paint(Graphics g) {
             super.paint(g);
-            for (int i = 0; i < FIELD_SIZE - 1; i++) {
+            g.setColor(Color.lightGray);
+            for (int i = 0; i < MAP_SIZE - 1; i++) {
                 g.drawLine(0, (i + 1)*CELL_SIZE, WINDOW_SIZE, (i + 1)*CELL_SIZE);
                 g.drawLine((i + 1)*CELL_SIZE, 0, (i + 1)*CELL_SIZE, WINDOW_SIZE);
             }
-            Graphics2D g2 = (Graphics2D) g; // use Graphics2D
-            g2.setStroke(new BasicStroke(5)); // 
-            for (int y = 0; y < FIELD_SIZE; y++)
-                for (int x = 0; x < FIELD_SIZE; x++) {
-                    if (field[x][y] == PLAYER_DOT) {
+            Graphics2D g2 = (Graphics2D) g;   // use Graphics2D
+            g2.setStroke(new BasicStroke(5)); // set line width
+            for (int y = 0; y < MAP_SIZE; y++)
+                for (int x = 0; x < MAP_SIZE; x++) {
+                    if (map[x][y] == DOT_X) {
                         g.setColor(Color.blue);
                         g2.draw(new Line2D.Float(x*CELL_SIZE+CELL_SIZE/4, y*CELL_SIZE+CELL_SIZE/4, (x+1)*CELL_SIZE-CELL_SIZE/4, (y+1)*CELL_SIZE-CELL_SIZE/4));
                         g2.draw(new Line2D.Float(x*CELL_SIZE+CELL_SIZE/4, (y+1)*CELL_SIZE-CELL_SIZE/4, (x+1)*CELL_SIZE-CELL_SIZE/4, y*CELL_SIZE+CELL_SIZE/4));
                     }
-                    if (field[x][y] == AI_DOT) {
+                    if (map[x][y] == DOT_O) {
                         g.setColor(Color.red);
                         g2.draw(new Ellipse2D.Float(x*CELL_SIZE+CELL_SIZE/4, y*CELL_SIZE+CELL_SIZE/4, CELL_SIZE/2, CELL_SIZE/2));
                     }
