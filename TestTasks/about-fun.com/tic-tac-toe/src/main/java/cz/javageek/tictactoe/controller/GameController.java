58f75e042c5cd08d169ee031c310397652e3511d
Sergey Iryupin <biblelamp@yandex.ru>
2019-07-03 23:00:41 +0200

    changed a bit

--- TestTasks/about-fun.com/tic-tac-toe/src/main/java/cz/javageek/tictactoe/controller/GameController.java
+++ TestTasks/about-fun.com/tic-tac-toe/src/main/java/cz/javageek/tictactoe/controller/GameController.java
@@ -1,10 +1,15 @@
 package cz.javageek.tictactoe.controller;
 
+// @see https://www.baeldung.com/spring-new-requestmapping-shortcuts
+
 import cz.javageek.tictactoe.domain.Match;
 import cz.javageek.tictactoe.repository.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -24,7 +29,7 @@ public class GameController {
             match.setStatus("W");
             matchRepository.save(match);
 
-            return "Your mark is 'x'. Waiting for a partner";
+            return "Your mark is 'x'. Waiting for a partner...";
         } else {
             match = matchRepository.getOne(1);
 
@@ -32,16 +37,22 @@ public class GameController {
                 match.setStatus("R");
                 matchRepository.save(match);
 
-                return "Your mark is 'o'. Waiting for the action of a partner";
+                return "Your mark is 'o'. Waiting for the action of a partner...";
             }
         }
 
         return "Join: " + match;
     }
 
-    @GetMapping(path="/action")
-    public @ResponseBody String action() {
+    @GetMapping(path="/action/{id}")
+    public @ResponseBody String action(@PathVariable String id) {
 
-        return "Action";
+        return "Action: id=" + id;
+    }
+
+    @GetMapping(path="/")
+    public @ResponseBody String status() {
+
+        return "Status: " + matchRepository.getOne(1);
     }
 }
