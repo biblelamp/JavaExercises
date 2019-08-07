91334c71d8e7da5c57ccd13ae109e9b3fcabd084
Sergey Iryupin <biblelamp@yandex.ru>
2019-08-07 16:03:33 +0200

    init commit

---
+++ ToBeginners/QuickStart/SimpleCannon.java
@@ -0,0 +1,13 @@
+
+class SimpleCannon {
+    public static void main(String[] args) {
+        float g = 9.81f;  // acceleration of gravity, m/c2
+        int speed = 150;  // starting speed of cannon shell, m/c
+        int angle;        // shot angle, degrees
+
+        for (angle = 0; angle < 90; angle += 5) {
+            double l = speed*speed * Math.sin(Math.toRadians(angle*2)) / (2 * g);
+            System.out.println(angle + ": " + l);
+        }
+    }
+}
