5c8dfa7c666b4d68b8e9e56c364cc1077b479448
Sergey Iryupin <biblelamp@yandex.ru>
2019-08-14 15:50:53 +0200

    refactored a bit

--- ToBeginners/QuickStart/SimpleCannon.java
+++ ToBeginners/QuickStart/SimpleCannon.java
@@ -1,13 +1,16 @@
-
 class SimpleCannon {
     public static void main(String[] args) {
         float g = 9.81f;  // acceleration of gravity, m/c2
         int speed = 150;  // starting speed of cannon shell, m/c
         int angle;        // shot angle, degrees
+                          // distance [199 .. 1146] m
 
         for (angle = 0; angle < 90; angle += 5) {
-            double l = Math.pow(speed, 2) * Math.sin(Math.toRadians(angle*2)) / (2 * g);
-            System.out.println(angle + ": " + l);
+            System.out.println(angle + ": " + getDistance(speed, angle, g));
         }
+    }
+
+    static double getDistance(int speed, int angle, float g) {
+        return Math.pow(speed, 2) * Math.sin(Math.toRadians(angle*2)) / (2 * g);
     }
 }
