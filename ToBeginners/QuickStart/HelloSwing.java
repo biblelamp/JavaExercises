798a0c7559eb11678712707fe496c593409737ef
Sergey Iryupin <biblelamp@yandex.ru>
2019-08-19 14:35:22 +0200

    hello swing

---
+++ ToBeginners/QuickStart/HelloSwing.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+class HelloSwing extends JFrame {
+    public static void main(String[] args) {
+        new HelloSwing();
+    }
+
+    HelloSwing() {
+        setTitle("Hello Swing");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
