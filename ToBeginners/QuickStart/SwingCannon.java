f9b5ded8d7850e50c17215def1d16f67261a3d47
Sergey Iryupin <biblelamp@yandex.ru>
2019-08-20 11:24:48 +0200

    init commit

---
+++ ToBeginners/QuickStart/SwingCannon.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+class SwingCannon extends JFrame {
+    public static void main(String[] args) {
+        new SwingCannon();
+    }
+
+    SwingCannon() {
+        setTitle("Cannon");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 400);
+
+        JTextArea text = new JTextArea();
+        JScrollPane scroll = new JScrollPane(text);
+        JTextField command = new JTextField();
+        JButton fire = new JButton("Fire");
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(command);
+        panel.add(fire);
+
+        add(BorderLayout.CENTER, scroll);
+        add(panel, BorderLayout.SOUTH);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
