b698d551d2217f7fa1db47f8f1b03afecb0323db
Sergey Iryupin <biblelamp@yandex.ru>
2019-08-30 21:25:47 +0200

    changed

--- ToBeginners/QuickStart/DrawCannon.java
+++ ToBeginners/QuickStart/DrawCannon.java
@@ -12,14 +12,15 @@ class DrawCannon extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500, 400);
 
-        JTextArea text = new JTextArea();
-        JScrollPane scroll = new JScrollPane(text);
+        Canvas canvas = new Canvas();
+        canvas.setBackground(Color.white);
+
         JTextField command = new JTextField();
         JButton fire = new JButton("Fire");
         fire.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                text.append(calculateShot(command.getText()));
+                //text.append(calculateShot(command.getText()));
             }
         });
 
@@ -28,28 +29,18 @@ class DrawCannon extends JFrame {
         panel.add(command);
         panel.add(fire);
 
-        add(BorderLayout.CENTER, scroll);
+        add(canvas, BorderLayout.CENTER);
         add(panel, BorderLayout.SOUTH);
 
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
-    String calculateShot(String data) {
-        float g = 9.81f;             // acceleration of gravity, m/c2
-        int speed = 150;             // starting speed of cannon shell, m/c
-        int damageRadius = 5;        // damage radius
-        double distanceTarget = 850; // distance to target
-
-        int angle = Integer.parseInt(data);
-        double distance = Math.pow(speed, 2) * Math.sin(Math.toRadians(angle * 2)) / (2 * g);
-        double difference = distanceTarget - distance;
-
-        if (Math.abs(difference) < damageRadius) {
-            return "Target hit";
-        } else {
-            String result = (difference > 0)? "Under" : "Over";
-            return String.format("%d: %sshoot %.2f\n", angle, result, Math.abs(difference));
+    class Canvas extends JPanel { // for painting
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            // 
         }
     }
 
