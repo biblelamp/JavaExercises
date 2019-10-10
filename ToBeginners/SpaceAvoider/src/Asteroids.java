e724ce119ef72aa8aa957732c858ee25cffbaf50
Sergey Iryupin <biblelamp@yandex.ru>
2019-10-10 19:52:41 +0200

    added isCrossing method

--- ToBeginners/SpaceAvoider/src/Asteroids.java
+++ ToBeginners/SpaceAvoider/src/Asteroids.java
@@ -7,21 +7,36 @@ class Asteroids {
     private List<Asteroid> asteroids;
 
     public Asteroids(int amount) {
+        Asteroid asteroid;
         Random rand = new Random();
-        asteroids = new ArrayList<>(amount);
+        asteroids = new ArrayList<>();
         for (int i = 0; i < amount; i++) {
-            asteroids.add(new Asteroid(
-                rand.nextInt(SpaceAvoider.WINDOW_WIDTH),
-                rand.nextInt(SpaceAvoider.WINDOW_HEIGHT - 100),
-                rand.nextInt(20) + 20)
-            );
+            System.out.println(i);
+            do {
+                asteroid = new Asteroid(
+                    rand.nextInt(SpaceAvoider.WINDOW_WIDTH),
+                    rand.nextInt(SpaceAvoider.WINDOW_HEIGHT - 100),
+                    rand.nextInt(20) + 20);
+            } while (isCrossing(asteroid));
+            asteroids.add(asteroid);
         }
     }
 
     boolean isCrossing(Asteroid asteroid) {
-        for (Asteroid asteroid : asteroids) {
-            // https://ru.stackoverflow.com/questions/758529/%D0%9F%D0%B5%D1%80%D0%B5%D1%81%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B4%D0%B2%D1%83%D1%85-%D0%BF%D1%80%D1%8F%D0%BC%D0%BE%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA%D0%BE%D0%B2-c
+        for (Asteroid item : asteroids) {
+            int left = Math.min(asteroid.x, item.x);
+            int right = Math.max(asteroid.x + asteroid.d, item.x + item.d);
+            int top = Math.max(asteroid.y + asteroid.d, item.y + item.d);
+            int bottom = Math.min(asteroid.y, item.y);
+            int width = right - left;
+            int height = top - bottom;
+            System.out.println(width + ":" + height);
+            if ((width > 0) && (height > 0)) {
+                return true;
+            }
+            /* https://ru.stackoverflow.com/questions/758529/%D0%9F%D0%B5%D1%80%D0%B5%D1%81%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-%D0%B4%D0%B2%D1%83%D1%85-%D0%BF%D1%80%D1%8F%D0%BC%D0%BE%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA%D0%BE%D0%B2-c */
         }
+        return false;
     }
 
     public void paint(Graphics g) {
