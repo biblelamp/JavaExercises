f060cab5eda73711bd8e49afa3670e6cdd39a6d0
Sergey Iryupin <biblelamp@yandex.ru>
2019-11-01 10:13:15 +0100

    tictactoe starting pattern

---
+++ ToBeginners/QuickStart/examples/TicTacToe.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class TicTacToe {
+    final char SIGN_X = 'x';
+    final char SIGN_O = 'o';
+    final char SIGN_EMPTY = '.';
+    char[][] table;
+    Random random;
+    Scanner scanner;
+
+    public static void main(String[] args) {
+        new TicTacToe().game();
+    }
+
+    TicTacToe() {
+        // конструктор: инициализация полей
+    }
+
+    void game() {
+        // игровая логика
+    }
+
+    // дополнительные методы
+}
