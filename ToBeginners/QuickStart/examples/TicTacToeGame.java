ade98c1d5fba723b16ffa004ad39c8f652568bf7
Sergey Iryupin <biblelamp@yandex.ru>
2019-11-02 18:53:25 +0100

    init commit

---
+++ ToBeginners/QuickStart/examples/TicTacToeGame.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class TicTacToeGame {
+    final char SIGN_X = 'x';
+    final char SIGN_O = 'o';
+    final char SIGN_EMPTY = '.';
+    char[][] table;
+    Random random;
+    Scanner scanner;
+
+    TicTacToe() {
+        random = new Random();
+        scanner = new Scanner(System.in);
+        table = new char[3][3];
+    }
+
+    void game() {
+        initTable();
+        while (true) {
+            turnHuman();
+            if (checkWin(SIGN_X)) {
+                System.out.println("YOU WIN!");
+                break;
+            }
+            if (isTableFull()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+            turnAI();
+            printTable();
+            if (checkWin(SIGN_O)) {
+                System.out.println("AI WIN!");
+                break;
+            }
+            if (isTableFull()) {
+                System.out.println("Sorry, DRAW!");
+                break;
+            }
+        }
+        System.out.println("GAME OVER.");
+        printTable();
+    }
+
+    void initTable() {
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
+                table[row][col] = SIGN_EMPTY;
+    }
+
+}
