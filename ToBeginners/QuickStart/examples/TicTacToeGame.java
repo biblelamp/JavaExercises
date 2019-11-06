ae860e70321b500af945e78fba36190770780a07
Sergey Iryupin <biblelamp@yandex.ru>
2019-11-06 12:54:18 +0100

    finished code

--- ToBeginners/QuickStart/examples/TicTacToeGame.java
+++ ToBeginners/QuickStart/examples/TicTacToeGame.java
@@ -68,15 +68,31 @@ class TicTacToeGame {
     }
 
     void turnAI() {
-        
+        int x, y;
+        do {
+            x = random.nextInt(3);
+            y = random.nextInt(3);
+        } while (!isCellValid(x, y));
+        table[y][x] = SIGN_O;
     }
 
     boolean checkWin(char dot) {
+        for (int i = 0; i < 3; i++)
+            if ((table[i][0] == dot && table[i][1] == dot && table[i][2] == dot) ||
+                (table[0][i] == dot && table[1][i] == dot && table[2][i] == dot))
+                return true;
+        if ((table[0][0] == dot && table[1][1] == dot && table[2][2] == dot) ||
+            (table[2][0] == dot && table[1][1] == dot && table[0][2] == dot))
+            return true;
         return false;
     }
 
     boolean isTableFull() {
-        return false;
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
+                if (table[row][col] == SIGN_EMPTY)
+                    return false;
+        return true;
     }
 
     boolean isCellValid(int x, int y) {
