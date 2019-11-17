cd5834b1f57085070b79dd23e8010006f09181a0
Sergey Iryupin <biblelamp@yandex.ru>
2019-11-17 20:01:10 +0100

    init commit

---
+++ ToBeginners/PersonalEducation/GameWithKettle.java
@@ -0,0 +1,9 @@
+class GameWithKettle {
+    public static void main(String[] args) {
+        int waterInKettle = 0;
+        if (waterInKettle == 0) {
+            System.out.println("Pour water into the kettle");
+        }
+        System.out.println("Put the kettle on the stove");
+    }
+}
