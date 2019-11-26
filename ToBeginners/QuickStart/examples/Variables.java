825abe3a4c5eab46823c07810ed8d561aad6d7a2
Sergey Iryupin <biblelamp@yandex.ru>
2019-11-26 13:54:42 +0100

    cnahged a bit

--- ToBeginners/QuickStart/examples/Variables.java
+++ ToBeginners/QuickStart/examples/Variables.java
@@ -1,8 +1,8 @@
 class Variables {
     public static void main(String[] args) {
-        int first;
-        int second = 10;
-        first = second * 3 + 5;
+        int first = 10;
+        float second;
+        second = first / 4f + 5;
         System.out.println(first);
         System.out.println(second);
     }
