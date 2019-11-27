80f7d69076342417e2f93524e306db66d9e0e44f
Sergey Iryupin <biblelamp@yandex.ru>
2019-11-27 09:47:03 +0100

    cnahged a bit

--- ToBeginners/QuickStart/examples/Kettle.java
+++ ToBeginners/QuickStart/examples/Kettle.java
@@ -1,8 +1,8 @@
 class Kettle {
     public static void main(String[] args) {
-        boolean isEmpty = true;
+        int water = 0;
         System.out.println("Take a kettle");
-        if (isEmpty) {
+        if (water == 0) {
             System.out.println("Pour water into the kettle");
         }
         System.out.println("Put the kettle on fire");
