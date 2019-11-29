b8c4379134eb17eb77a1eb79be89881137da8a77
Sergey Iryupin <biblelamp@yandex.ru>
2019-11-29 08:15:39 +0100

    added class

---
+++ ToBeginners/QuickStart/examples/Stairs.java
@@ -0,0 +1,7 @@
+class Stairs {
+    public static void main(String[] args) {
+        for (int i = 0; i < 25; i++) {
+            System.out.println("Go down a stair step #" + i);
+        }
+    }
+}
