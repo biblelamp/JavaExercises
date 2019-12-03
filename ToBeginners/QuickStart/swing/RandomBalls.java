544440de8cfb5cab61d64d873b242da7b417c59e
Sergey Iryupin <biblelamp@yandex.ru>
2019-12-03 12:34:54 +0100

    init commit

---
+++ ToBeginners/QuickStart/swing/RandomBalls.java
@@ -0,0 +1,54 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.util.Random;
+
+public class RandomBalls extends JFrame {
+
+    final String TITLE_OF_PROGRAM = "Random balls";
+    final int WINDOW_WIDTH = 650;
+    final int WINDOW_HEIGHT = 650;
+    final Color[] COLORS = {Color.red, Color.green, Color.blue};
+
+    Random random;
+
+    public static void main(String[] args) {
+        new RandomBalls();
+    }
+
+    public RandomBalls() {
+        setTitle(TITLE_OF_PROGRAM);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        random = new Random();
+
+        Canvas canvas = new Canvas();
+        canvas.setBackground(Color.white);
+        canvas.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+
+        add(canvas);
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    class Canvas extends JPanel {
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            for (int i = 0; i < 100; i++) {
+                int d = random.nextInt(20) + 60;
+                int x = random.nextInt(WINDOW_WIDTH - d);
+                int y = random.nextInt(WINDOW_HEIGHT - d);
+                Color color = COLORS[random.nextInt(COLORS.length)];
+                g.setColor(color);
+                g.fillOval(x, y, d, d);
+                g.setColor(Color.black);
+                g.drawOval(x, y, d, d);
+            }
+        }
+    }
+}
