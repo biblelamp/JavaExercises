45753e9d880d424f4ba21f2a8b78d40402302405
Sergey Iryupin <biblelamp@yandex.ru>
2019-12-04 17:55:16 +0100

    added a bit

--- ToBeginners/QuickStart/swing/Food.java
+++ ToBeginners/QuickStart/swing/Food.java
@@ -1,7 +1,11 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.List;
+import java.util.ArrayList;
 
 class Food {
+
+    List<Ball> food;
 
     void paint(Graphics g) {
     }
