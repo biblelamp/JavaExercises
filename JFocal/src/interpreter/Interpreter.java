3b79b5bf41d75462e395d85d12c73c0104567672
Sergey Iryupin <biblelamp@yandex.ru>
2020-03-24 15:29:38 +0100

    new class for program lines

--- JFocal/src/interpreter/Interpreter.java
+++ JFocal/src/interpreter/Interpreter.java
@@ -1,6 +1,7 @@
 package interpreter;
 
 import calculate.Calculate;
+import program.ProgramLines;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -10,7 +11,7 @@ import java.util.TreeMap;
 
 public class Interpreter {
 
-    private final static String WELCOME = "JFocal, version 0.06";
+    private final static String WELCOME = "JFocal, version 0.07";
     private final static String PROMT = "* ";
 
     private final static String T = "T";
@@ -28,19 +29,16 @@ public class Interpreter {
     private final static String COMMAND_NOT_RECOGNIZED = "Error: Command '%s' not recognized\n";
     private final static String NOT_ENOUGH_PARAMETERS = "Error: Not enough parameters command '%s'\n";
     private final static String OPERATION_NOT_RECOGNIZED = "Error: Operation '%s' not recognized\n";
-    private final static String ERROR_WRITING_FILE = "Error writing file '%s'\n";
-    private final static String ERROR_READING_FILE = "Error reading file '%s'\n";
-    private final static String BAD_LINE_NUMBER = "Error: Bad line number %s\n";
     private final static String UNPAIRED_QUOTES = "Error: Unpaired quotes %s\n";
     public final static String ERROR_NUMBER_FORMAT = "Error number format %s\n";
 
     private Scanner scanner;
-    private Map<Float, String> program;
+    private ProgramLines program;
     private boolean quit;
 
     public Interpreter() {
         scanner = new Scanner(System.in);
-        program = new TreeMap<>();
+        program = new ProgramLines();
         quit = false;
     }
 
@@ -74,47 +72,6 @@ public class Interpreter {
         }
     }
 
-    private void addLineToProgram(String numLine, String line) {
-        program.put(Float.valueOf(numLine), line.substring(line.indexOf(' ') + 1));
-    }
-
-    private void writeLineProgram() {
-        for (Float key : program.keySet()) {
-            System.out.printf("%02d.%02d %s\n", key.intValue(), (int)(key * 100 % 100), program.get(key));
-        }
-    }
-
-    private void outputProgram(String fileName) {
-        try (BufferedWriter writer = new BufferedWriter(
-                new OutputStreamWriter(
-                        new FileOutputStream(fileName), StandardCharsets.UTF_8))){
-            for (Float key : program.keySet()) {
-                writer.write(String.format("%02d.%02d %s\n",
-                        key.intValue(), (int)(key * 100 % 100), program.get(key)));
-            }
-        } catch (IOException e) {
-            System.out.printf(ERROR_WRITING_FILE, fileName);
-        }
-    }
-
-    private void inputProgram(String fileName) {
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(
-                    new FileInputStream(fileName), StandardCharsets.UTF_8))){
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] tokens = line.split(" ");
-                if (isValidLineNumber(tokens[0])) {
-                    addLineToProgram(tokens[0], line);
-                } else {
-                    System.out.printf(BAD_LINE_NUMBER, tokens[0]);
-                }
-            }
-        } catch (IOException e) {
-            System.out.printf(ERROR_READING_FILE, fileName);
-        }
-    }
-
     private void readWriteProgram(String[] tokens) {
         if (tokens.length < 3) {
             System.out.printf(NOT_ENOUGH_PARAMETERS, tokens[0]);
@@ -124,25 +81,21 @@ public class Interpreter {
         switch (operation) {
             case "I":
             case "INPUT":
-                inputProgram(tokens[2]);
+                program.input(tokens[2]);
                 break;
             case "O":
             case "OUTPUT":
-                outputProgram(tokens[2]);
+                program.output(tokens[2]);
                 break;
             default:
                 System.out.printf(OPERATION_NOT_RECOGNIZED, tokens[1]);
         }
     }
 
-    private boolean isValidLineNumber(String numLine) {
-        return numLine.matches("\\d\\d?\\.\\d\\d?");
-    }
-
     private void processLine(String line) {
         String[] tokens = line.split(" ");
-        if (isValidLineNumber(tokens[0])) {
-            addLineToProgram(tokens[0], line);
+        if (program.isValidLineNumber(tokens[0])) {
+            program.add(tokens[0], line);
         } else {
             String cmd = tokens[0].toUpperCase();
             switch (cmd) {
@@ -156,7 +109,7 @@ public class Interpreter {
                     break;
                 case E:
                 case ERASE:
-                    program.clear();
+                    program.erase();
                     break;
                 case O:
                 case OPEN:
@@ -164,7 +117,7 @@ public class Interpreter {
                     break;
                 case W:
                 case WRITE:
-                    writeLineProgram();
+                    program.write();
                     break;
                 default:
                     System.out.printf(COMMAND_NOT_RECOGNIZED, tokens[0]);
---
+++ JFocal/src/program/ProgramLines.java
@@ -0,0 +1,69 @@
+package program;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ProgramLines {
+
+    private final static String ERROR_WRITING_FILE = "Error writing file '%s'\n";
+    private final static String ERROR_READING_FILE = "Error reading file '%s'\n";
+    private final static String BAD_LINE_NUMBER = "Error: Bad line number %s\n";
+
+    private Map<Float, String> programLines;
+
+    public ProgramLines() {
+        programLines = new TreeMap<>();;
+    }
+
+    public void add(String numLine, String line) {
+        programLines.put(Float.valueOf(numLine), line.substring(line.indexOf(' ') + 1));
+    }
+
+    public void write() {
+        for (Float key : programLines.keySet()) {
+            System.out.printf("%02d.%02d %s\n", key.intValue(), (int) (key * 100 % 100), programLines.get(key));
+        }
+    }
+
+    public void erase() {
+        programLines.clear();
+    }
+
+    public void input(String fileName) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(fileName), StandardCharsets.UTF_8))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(" ");
+                if (isValidLineNumber(tokens[0])) {
+                    add(tokens[0], line);
+                } else {
+                    System.out.printf(BAD_LINE_NUMBER, tokens[0]);
+                }
+            }
+        } catch (IOException e) {
+            System.out.printf(ERROR_READING_FILE, fileName);
+        }
+    }
+
+    public void output(String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream(fileName), StandardCharsets.UTF_8))){
+            for (Float key : programLines.keySet()) {
+                writer.write(String.format("%02d.%02d %s\n",
+                        key.intValue(), (int)(key * 100 % 100), programLines.get(key)));
+            }
+        } catch (IOException e) {
+            System.out.printf(ERROR_WRITING_FILE, fileName);
+        }
+    }
+
+    public boolean isValidLineNumber(String numLine) {
+        return numLine.matches("\\d\\d?\\.\\d\\d?");
+    }
+
+}
