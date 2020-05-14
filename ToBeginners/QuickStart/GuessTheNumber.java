b91e4471f954683e1c057da2789aa2d712c631f8
Sergey Iryupin <biblelamp@yandex.ru>
2020-05-14 13:51:49 +0200

    improved a bit

--- ToBeginners/QuickStart/GuessTheNumber.java
+++ ToBeginners/QuickStart/GuessTheNumber.java
@@ -5,14 +5,14 @@ class GuessTheNumber {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
+        final int limit = 3;
         do {
-            int limit = 3;
             int count = 0;
             int guess = -1;
             int number = random.nextInt(10);
             while (count < limit && guess != number) {
-                System.out.print(
-                    "Guess [" + (limit - count) + " attempts] the number (0..9): ");
+                System.out.printf(
+                    "Guess [%d attempts] the number (0..9): ", limit-count);
                 guess = sc.nextInt();
                 if (number != guess) {
                     System.out.println("Your number is " +
