3ee734302c7ce02c083cf60241651338435ffa3a
Sergey Iryupin <biblelamp@yandex.ru>
2020-05-29 19:12:55 +0200

    example with lambda

---
+++ Experiments/LambdaExample.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.util.stream.*;
+
+class LambdaExample {
+
+    public static void main(String[] args) {
+        List<Artist> allArtists = Arrays.asList(
+            new Artist("Leonid Sobinov", "Moscow"),
+            new Artist("John Lennon", "London")
+        );
+
+        List<Artist> london = 
+            allArtists.stream()
+                .filter(artist -> artist.isFrom("London"))
+                .collect(Collectors.toList());
+        System.out.println(london);
+
+        List<String> collected = 
+            Stream.of("a", "b", "hello")
+                .map(string -> string.toUpperCase())
+                .collect(Collectors.toList());
+        System.out.println(collected);
+    }
+
+    static class Artist {
+        String name;
+        String city;
+
+        Artist(String name, String city) {
+            this.name = name;
+            this.city = city;
+        }
+
+        boolean isFrom(String city) {
+            return this.city.equals(city);
+        }
+
+        String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("{%s, %s}", name, city);
+        }
+    }
+}
