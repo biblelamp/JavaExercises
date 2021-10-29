adcb9e6fe81b789a15856be1132148c9f9be7860
Sergey Iryupin <biblelamp@yandex.ru>
2021-10-29 17:20:26 +0200

    next version

--- ToBeginners/GameSnake/src/Snake.java
+++ ToBeginners/GameSnake/src/Snake.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.util.LinkedList;
 
@@ -6,32 +7,49 @@ import java.util.LinkedList;
  *  A class that implements a snake
  *
  * @author Sergey Iryupin
- * @version 0.3.3 dated Dec 28, 2018
+ * @version 0.3.5 dated Oct 26, 2019
  */
 
 public class Snake {
     private LinkedList<Cell> snake;
     private int direction;
-    private GameSnake gameSnake;
+    private Food food;
+    private Poison poison;
 
-    public Snake(int x, int y, int length, int direction, GameSnake gameSnake) {
+    public Snake(int x, int y, int length, int direction) {
         snake = new LinkedList<>();
-        for (int i = 0; i < length; i++)
-            snake.add(new Cell(x - i, y, gameSnake.CELL_SIZE, gameSnake.SNAKE_COLOR));
+        for (int i = 0; i < length; i++) {
+            snake.add(new Cell(x - i, y, GameSnake.CELL_SIZE, GameSnake.SNAKE_COLOR));
+        }
         this.direction = direction;
-        this.gameSnake = gameSnake;
+    }
+
+    public void setFood(Food food) {
+        this.food = food;
+    }
+
+    public void setPoison(Poison poison) {
+        this.poison = poison;
+    }
+
+    public int size() {
+        return snake.size();
     }
 
     public void setDirection(int direction) {
-        if ((direction >= GameSnake.KEY_LEFT) && (direction <= GameSnake.KEY_DOWN))
-            if (Math.abs(this.direction - direction) != 2)
+        if ((direction >= GameSnake.KEY_LEFT) && (direction <= GameSnake.KEY_DOWN)) {
+            if (Math.abs(this.direction - direction) != 2) {
                 this.direction = direction;
+            }
+        }
     }
 
     public boolean isInSnake(int x, int y) {
-        for (Cell cell : snake)
-            if ((cell.getX() == x) && (cell.getY() == y))
+        for (Cell cell : snake) {
+            if ((cell.getX() == x) && (cell.getY() == y)) {
                 return true;
+            }
+        }
         return false;
     }
 
@@ -41,36 +59,37 @@ public class Snake {
         switch (direction) {
             case GameSnake.KEY_LEFT: x--;
                 if (x < 0)
-                    x = gameSnake.CANVAS_WIDTH - 1;
+                    x = GameSnake.CANVAS_WIDTH - 1;
                 break;
             case GameSnake.KEY_RIGHT: x++;
-                if (x == gameSnake.CANVAS_WIDTH)
+                if (x == GameSnake.CANVAS_WIDTH)
                     x = 0;
                 break;
             case GameSnake.KEY_UP: y--;
                 if (y < 0)
-                    y = gameSnake.CANVAS_HEIGHT - 1;
+                    y = GameSnake.CANVAS_HEIGHT - 1;
                 break;
             case GameSnake.KEY_DOWN: y++;
-                if (y == gameSnake.CANVAS_HEIGHT)
+                if (y == GameSnake.CANVAS_HEIGHT)
                     y = 0;
                 break;
         }
-        if (isInSnake(x, y) ||                     // snake shouldn't cross itself
-                gameSnake.poison.isPoison(x, y)) { // and eat poison
-            gameSnake.gameOver = true;
+        if (isInSnake(x, y) ||           // if the snake crosses itself
+                poison.isPoison(x, y)) { // or if it eats poison
+            GameSnake.gameOver = true;
             return;
         }
-        snake.addFirst(new Cell(x, y, gameSnake.CELL_SIZE, gameSnake.SNAKE_COLOR)); // new head of snake
-        if (gameSnake.food.isFood(x, y)) {
-            gameSnake.food.eat();
-            gameSnake.setTitle(gameSnake.TITLE_OF_PROGRAM + " : " + snake.size());
-        } else
+        snake.addFirst(new Cell(x, y, GameSnake.CELL_SIZE, GameSnake.SNAKE_COLOR)); // new head of snake
+        if (food.isFood(x, y)) {
+            food.eat();
+        } else {
             snake.removeLast();
+        }
     }
 
     public void paint(Graphics2D g) {
-        for (Cell cell : snake)
+        for (Cell cell : snake) {
             cell.paint(g);
+        }
     }
 }
