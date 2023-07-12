48741a20e02c7ff2303848b9193ef21218d68f43
Sergey Iryupin <biblelamp@yandex.ru>
2023-07-12 08:17:17 +0200

    hello crud update

--- BerlinAIT/HelloCRUD/src/main/java/spring/controller/EventController.java
+++ BerlinAIT/HelloCRUD/src/main/java/spring/controller/EventController.java
@@ -3,10 +3,7 @@ package spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import spring.domain.Event;
 import spring.service.EventService;
 
@@ -31,7 +28,6 @@ public class EventController {
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public String addEvent(Model model, @ModelAttribute("event") Event event) {
-
         String name = event.getName();
         String city = event.getCity();
 
@@ -44,4 +40,34 @@ public class EventController {
         model.addAttribute("errorMessage", "Event Name & City is required!");
         return "add";
     }
+
+    @GetMapping("/update/{id}")
+    public String updateEventForm(Model model, @PathVariable Integer id) {
+        Event event = eventService.get(id);
+        model.addAttribute("events", eventService.findAll());
+        model.addAttribute("event", event);
+        return "update";
+    }
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public String updateEvent(Model model, @ModelAttribute("event") Event event, @PathVariable Integer id) {
+        String name = event.getName();
+        String city = event.getCity();
+
+        if (name != null && name.length() > 0 && city != null && city.length() > 0) {
+            Event newEvent = new Event(name, city);
+            eventService.update(id, newEvent);
+
+            return "redirect:/";
+        }
+        model.addAttribute("errorMessage", "Event Name & City is required!");
+        return "update";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public String delete(Integer id) {
+        eventService.remove(id);
+        return "redirect:/";
+    }
+
 }
--- BerlinAIT/HelloCRUD/src/main/java/spring/service/EventService.java
+++ BerlinAIT/HelloCRUD/src/main/java/spring/service/EventService.java
@@ -8,8 +8,7 @@ import java.util.List;
 
 @Service
 public class EventService {
-
-    static final ArrayList<Event> events = new ArrayList<Event>(){{
+    static final List<Event> events = new ArrayList<Event>(){{
         add(new Event("Opera", "London"));
         add(new Event("Violin concert", "Prague"));
         add(new Event("Jazz concert", "Berlin"));
@@ -22,5 +21,19 @@ public class EventService {
 
     public void add(Event event) {
         events.add(event);
+    }
+
+    public Event get(Integer id) {
+        return events.get(id);
+    }
+
+    public void update(Integer id, Event event) {
+        Event updEvent = events.get(id);
+        updEvent.setName(event.getName());
+        updEvent.setCity(event.getCity());
+    }
+
+    public void remove(int id) {
+        events.remove(id);
     }
 }
