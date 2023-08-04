a2160d09116b07febebceaf36d7a765640cc348c
Sergey Iryupin <biblelamp@yandex.ru>
2023-08-04 05:43:59 +0200

    regexp simple example

--- Experiments/RegexpSimpleTest.java
+++ Experiments/RegexpSimpleTest.java
@@ -1,7 +1,11 @@
 class RegexpSimpleTest {
     public static void main(String[] args) {
-        String exp = "\\d-\\d";
-        String str = "2012-2013";
+        String exp = "\\d+-\\d+";
+        String str = "2013-2014";
         System.out.println(str.matches(exp));
+
+        String pattern = "^(https?|elza-node)://.*";
+        String url = "https://bugzilla.lightcomp.cz/show_bug.cgi?id=8597";
+        System.out.println(url.matches(pattern));
     }
 }
