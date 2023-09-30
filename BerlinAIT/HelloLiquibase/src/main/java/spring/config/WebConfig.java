7e36ea6d847bc8f3731380aac9a95f944fae0d41
Sergey Iryupin <biblelamp@yandex.ru>
2023-09-30 21:15:44 +0200

    allow all http methods

--- BerlinAIT/HelloLiquibase/src/main/java/spring/config/WebConfig.java
+++ BerlinAIT/HelloLiquibase/src/main/java/spring/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
 
