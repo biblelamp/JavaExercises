b3e1caee3bae21614e744b4a328c3fe37d9d705e
Sergey Iryupin <biblelamp@yandex.ru>
2023-11-13 16:00:21 +0100

    hello Jsoup

--- BerlinAIT/HelloJsoup/src/main/java/web/crawler/Application.java
+++ BerlinAIT/HelloJsoup/src/main/java/web/crawler/Application.java
@@ -9,23 +9,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+/*
+ * Parsing HTML in Java with Jsoup https://www.baeldung.com/java-with-jsoup
+ */
 public class Application {
+
+    private static final String URL_CLASS = "Link-module-root"; // Link-module-isInBlockTitle";
+    private static final String TITLE_CLASS = "BlockTitle-module-first";
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.parse(new File("C:/temp/e-kladensko/06-10-23.html"));
-        Elements news = doc.getElementsByClass("wrapperk2");
+        Document doc = Jsoup.parse(new File("C:/temp/meduza.io/index-10-11-2023.html"));
+        Elements news = doc.getElementsByClass(URL_CLASS);
         for (Element element : news) {
-            // find date
-            Elements datehit = element.getElementsByClass("datehit");
-            String dateText = datehit.text();
-            List<String> items = List.of(dateText.split(" "));
-            System.out.println(items.get(0));
+            // find url
+            String url = element.select("a").attr("href");
+            System.out.println(url);
 
             // find title
-            Elements url = element.getElementsByClass("latestItemTitle");
-            System.out.println(url.text());
-            break;
-
-            // find url
+            Elements title = element.getElementsByClass(TITLE_CLASS);
+            String dateText = title.text();
+            System.out.println(dateText);
         }
     }
 }
