f047f40fdcad3785588a1ea6bdc345bdc8df27c4
Sergey Iryupin <biblelamp@yandex.ru>
2024-06-01 10:08:12 +0200

    java basic lesson #7

---
+++ BerlinTelran/JavaBasic/JavaBasic7Hw.java
@@ -0,0 +1,15 @@
+/**
+ * Java Basic. Home work #7
+ *
+ * @author Sergey
+ * @todo 26.9.2022
+ * @date 28.9.2022
+ */
+import java.math.BigInteger;
+
+class JavaBasic7Hw {
+    public static void main(String[] args) {
+        BigInteger a = (BigInteger) Math.pow(2, 95);
+        System.out.println(a);
+    }
+}
